a79b3d64690a8271c64044afb409cf896044375f
Johan T. Halseth <johanth@gmail.com>
2018-10-08 15:56:25 +0200

    htlcswitch/link test: add test for permanent failure

--- htlcswitch/link_test.go
+++ htlcswitch/link_test.go
@@ -5387,6 +5387,11 @@ func TestChannelLinkFail(t *testing.T) {
 		// force close the channel in response to the actions performed
 		// during the linkTest.
 		shouldForceClose bool
+
+		// permanentFailure indicates whether we expect the link to
+		// consider the failure permanent in response to the actions
+		// performed during the linkTest.
+		permanentFailure bool
 	}{
 		{
 			// Test that we don't force close if syncing states
@@ -5401,6 +5406,7 @@ func TestChannelLinkFail(t *testing.T) {
 			func(t *testing.T, c *channelLink, _ *lnwallet.LightningChannel) {
 				// Should fail at startup.
 			},
+			false,
 			false,
 		},
 		{
@@ -5418,6 +5424,7 @@ func TestChannelLinkFail(t *testing.T) {
 				// Should fail at startup.
 			},
 			false,
+			false,
 		},
 		{
 			// Test that we force close the channel if we receive
@@ -5434,6 +5441,7 @@ func TestChannelLinkFail(t *testing.T) {
 				c.HandleChannelUpdate(htlcSettle)
 			},
 			true,
+			false,
 		},
 		{
 			// Test that we force close the channel if we receive
@@ -5471,6 +5479,7 @@ func TestChannelLinkFail(t *testing.T) {
 				c.HandleChannelUpdate(commitSig)
 			},
 			true,
+			false,
 		},
 		{
 			// Test that we force close the channel if we receive
@@ -5509,6 +5518,19 @@ func TestChannelLinkFail(t *testing.T) {
 
 				c.HandleChannelUpdate(commitSig)
 			},
+			true,
+			false,
+		},
+		{
+			// Test that we consider the failure permanent if we
+			// receive a link error from the remote.
+			func(c *channelLink) {
+			},
+			func(t *testing.T, c *channelLink, remoteChannel *lnwallet.LightningChannel) {
+				err := &lnwire.Error{}
+				c.HandleChannelUpdate(err)
+			},
+			false,
 			true,
 		},
 	}
@@ -5558,6 +5580,12 @@ func TestChannelLinkFail(t *testing.T) {
 			t.Fatalf("%d) Expected Alice to force close(%v), "+
 				"instead got(%v)", i, test.shouldForceClose,
 				linkErr.ForceClose)
+		}
+
+		if test.permanentFailure != linkErr.PermanentFailure {
+			t.Fatalf("%d) Expected Alice set permanent failure(%v), "+
+				"instead got(%v)", i, test.permanentFailure,
+				linkErr.PermanentFailure)
 		}
 
 		// Clean up before starting next test case.
