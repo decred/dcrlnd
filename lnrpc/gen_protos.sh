b72820eef634a3010c3ff1ec566f3325f9bea555
Johan T. Halseth <johanth@gmail.com>
2021-02-09 15:12:21 +0100

    lnrpc: add StateService

--- lnrpc/gen_protos.sh
+++ lnrpc/gen_protos.sh
@@ -21,7 +21,7 @@ generate() {
 
     echo "Generating root gRPC server protos"
 
-    PROTOS="rpc.proto walletunlocker.proto **/*.proto"
+    PROTOS="rpc.proto walletunlocker.proto stateservice.proto **/*.proto"
 
     # For each of the sub-servers, we then generate their protos, but a restricted
     # set as they don't yet require REST proxies, or swagger docs.
