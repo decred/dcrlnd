573da461bf151bb7ea1dc373115b157eb46539ec
Elliott Jin <elliott.jin@gmail.com>
2021-02-16 14:06:54 -0800

    itest: add test for new UpdateChanStatus RPC

--- lntest/itest/lnd_test_list_on_test.go
+++ lntest/itest/lnd_test_list_on_test.go
@@ -93,6 +93,10 @@ var allTestCases = []*testCase{
 		test: testListChannels,
 	},
 	{
+		name: "update channel status",
+		test: testUpdateChanStatus,
+	},
+	{
 		name: "list outgoing payments",
 		test: testListPayments,
 	},
