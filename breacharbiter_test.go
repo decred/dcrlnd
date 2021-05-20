37b3e005f27a5eda5ea26decf0823c0ef23c36d4
Johan T. Halseth <johanth@gmail.com>
2021-05-20 09:08:03 +0200

    breacharbiter_test: select on quit chan on publication
    Since publication would deadlock on publishing the tx in case the test
    had failed we also select on the brar quit channel.

--- breacharbiter_test.go
+++ breacharbiter_test.go
@@ -1606,7 +1606,12 @@ func testBreachSpends(t *testing.T, test breachTest) {
 		publMtx.Lock()
 		err := publErr
 		publMtx.Unlock()
-		publTx <- tx
+
+		select {
+		case publTx <- tx:
+		case <-brar.quit:
+			return fmt.Errorf("brar quit")
+		}
 
 		return err
 	}
@@ -1819,7 +1824,11 @@ func TestBreachDelayedJusticeConfirmation(t *testing.T) {
 
 	// Make PublishTransaction always return succeed.
 	brar.cfg.PublishTransaction = func(tx *wire.MsgTx, _ string) error {
-		publTx <- tx
+		select {
+		case publTx <- tx:
+		case <-brar.quit:
+			return fmt.Errorf("brar quit")
+		}
 		return nil
 	}
 
