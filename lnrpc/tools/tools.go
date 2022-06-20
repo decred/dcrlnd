20a84228852fb684b13fdcb040af4786d0ea98ae
Matheus Degiovani <opensource@matheusd.com>
2022-06-20 10:41:26 -0300

    lnrpc: Add missing entry to go.sum
    This adds an imported package and updates the go.sum file to avoid
    errors when attempting to rebuild the lnrpc bindings.

--- lnrpc/tools/tools.go
+++ lnrpc/tools/tools.go
@@ -2,6 +2,7 @@ package tools
 
 import (
 	_ "github.com/golang/protobuf/proto"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway/descriptor"
 	_ "github.com/grpc-ecosystem/grpc-gateway/utilities"
 	_ "github.com/matheusd/google-protobuf-protos/dummy"
 )
