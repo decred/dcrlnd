lightningnetwork/lnd

lnwallet: expose optional account filter for several WalletController methods

lnrpc/walletrpc/walletkit_server.go
@@ -321,7 +321,9 @@ func (w *WalletKit) ListUnspent(ctx context.Context,
 	// be shown available to us.
 	var utxos []*lnwallet.Utxo
 	err = w.cfg.CoinSelectionLocker.WithCoinSelectLock(func() error {
-		utxos, err = w.cfg.Wallet.ListUnspentWitness(minConfs, maxConfs)
+		utxos, err = w.cfg.Wallet.ListUnspentWitness(
+			minConfs, maxConfs, "",
+		)
 		return err
 	})
 	if err != nil {
@@ -854,9 +856,10 @@ func (w *WalletKit) ListSweeps(ctx context.Context,
 	// Some of our sweeps could have been replaced by fee, or dropped out
 	// of the mempool. Here, we lookup our wallet transactions so that we
 	// can match our list of sweeps against the list of transactions that
-	// the wallet is still tracking.
+	// the wallet is still tracking. Sweeps are currently always swept to
+	// the default wallet account.
 	transactions, err := w.cfg.Wallet.ListTransactionDetails(
-		0, btcwallet.UnconfirmedHeight,
+		0, btcwallet.UnconfirmedHeight, lnwallet.DefaultAccountName,
 	)
 	if err != nil {
 		return nil, err
@@ -1057,7 +1060,7 @@ func (w *WalletKit) FundPsbt(_ context.Context,
 		if len(packet.UnsignedTx.TxIn) > 0 {
 			// Get a list of all unspent witness outputs.
 			utxos, err := w.cfg.Wallet.ListUnspentWitness(
-				defaultMinConf, defaultMaxConf,
+				defaultMinConf, defaultMaxConf, "",
 			)
 			if err != nil {
 				return err

lnwallet/interface.go
@@ -159,12 +159,14 @@ type WalletController interface {
 	// ConfirmedBalance returns the sum of all the wallet's unspent outputs
 	// that have at least confs confirmations. If confs is set to zero,
 	// then all unspent outputs, including those currently in the mempool
-	// will be included in the final sum.
+	// will be included in the final sum. The account parameter serves as a
+	// filter to retrieve the balance for a specific account. When empty,
+	// the confirmed balance of all wallet accounts is returned.
 	//
 	// NOTE: Only witness outputs should be included in the computation of
 	// the total spendable balance of the wallet. We require this as only
 	// witness inputs can be used for funding channels.
-	ConfirmedBalance(confs int32) (btcutil.Amount, error)
+	ConfirmedBalance(confs int32, accountFilter string) (btcutil.Amount, error)
 
 	// NewAddress returns the next external or internal address for the
 	// wallet dictated by the value of the `change` parameter. If change is
@@ -221,10 +223,13 @@ type WalletController interface {
 	// needs in order to be returned by this method. Passing -1 as
 	// 'minconfirms' indicates that even unconfirmed outputs should be
 	// returned. Using MaxInt32 as 'maxconfirms' implies returning all
-	// outputs with at least 'minconfirms'.
+	// outputs with at least 'minconfirms'. The account parameter serves as
+	// a filter to retrieve the unspent outputs for a specific account.
+	// When empty, the unspent outputs of all wallet accounts are returned.
 	//
 	// NOTE: This method requires the global coin selection lock to be held.
-	ListUnspentWitness(minconfirms, maxconfirms int32) ([]*Utxo, error)
+	ListUnspentWitness(minconfirms, maxconfirms int32,
+		accountFilter string) ([]*Utxo, error)
 
 	// ListTransactionDetails returns a list of all transactions which are
 	// relevant to the wallet over [startHeight;endHeight]. If start height
@@ -232,9 +237,11 @@ type WalletController interface {
 	// reverse order. To include unconfirmed transactions, endHeight should
 	// be set to the special value -1. This will return transactions from
 	// the tip of the chain until the start height (inclusive) and
-	// unconfirmed transactions.
-	ListTransactionDetails(startHeight,
-		endHeight int32) ([]*TransactionDetail, error)
+	// unconfirmed transactions. The account parameter serves as a filter to
+	// retrieve the transactions relevant to a specific account. When
+	// empty, transactions of all wallet accounts are returned.
+	ListTransactionDetails(startHeight, endHeight int32,
+		accountFilter string) ([]*TransactionDetail, error)
 
 	// LockOutpoint marks an outpoint as locked meaning it will no longer
 	// be deemed as eligible for coin selection. Locking outputs are

lnwallet/wallet.go
@@ -396,14 +396,33 @@ func (l *LightningWallet) Shutdown() error {
 	return nil
 }
 
-// ConfirmedBalance returns the current confirmed balance of the wallet. This
-// methods wraps the interal WalletController method so we're able to properly
-// hold the coin select mutex while we compute the balance.
-func (l *LightningWallet) ConfirmedBalance(confs int32) (btcutil.Amount, error) {
+// ConfirmedBalance returns the current confirmed balance of a wallet account.
+// This methods wraps the internal WalletController method so we're able to
+// properly hold the coin select mutex while we compute the balance.
+func (l *LightningWallet) ConfirmedBalance(confs int32,
+	account string) (btcutil.Amount, error) {
+
 	l.coinSelectMtx.Lock()
 	defer l.coinSelectMtx.Unlock()
 
-	return l.WalletController.ConfirmedBalance(confs)
+	return l.WalletController.ConfirmedBalance(confs, account)
+}
+
+// ListUnspentWitnessFromDefaultAccount returns all unspent outputs from the
+// default wallet account which are version 0 witness programs. The 'minConfs'
+// and 'maxConfs' parameters indicate the minimum and maximum number of
+// confirmations an output needs in order to be returned by this method. Passing
+// -1 as 'minConfs' indicates that even unconfirmed outputs should be returned.
+// Using MaxInt32 as 'maxConfs' implies returning all outputs with at least
+// 'minConfs'.
+//
+// NOTE: This method requires the global coin selection lock to be held.
+func (l *LightningWallet) ListUnspentWitnessFromDefaultAccount(
+	minConfs, maxConfs int32) ([]*Utxo, error) {
+
+	return l.WalletController.ListUnspentWitness(
+		minConfs, maxConfs, DefaultAccountName,
+	)
 }
 
 // LockedOutpoints returns a list of all currently locked outpoint.
@@ -922,8 +941,12 @@ func (l *LightningWallet) currentNumAnchorChans() (int, error) {
 func (l *LightningWallet) CheckReservedValue(in []wire.OutPoint,
 	out []*wire.TxOut, numAnchorChans int) (btcutil.Amount, error) {
 
-	// Get all unspent coins in the wallet.
-	witnessOutputs, err := l.ListUnspentWitness(0, math.MaxInt32)
+	// Get all unspent coins in the wallet. We only care about those part of
+	// the wallet's default account as we know we can readily sign for those
+	// at any time.
+	witnessOutputs, err := l.ListUnspentWitnessFromDefaultAccount(
+		0, math.MaxInt32,
+	)
 	if err != nil {
 		return 0, err
 	}
@@ -2022,7 +2045,9 @@ func NewCoinSource(w *LightningWallet) *CoinSource {
 func (c *CoinSource) ListCoins(minConfs int32,
 	maxConfs int32) ([]chanfunding.Coin, error) {
 
-	utxos, err := c.wallet.ListUnspentWitness(minConfs, maxConfs)
+	utxos, err := c.wallet.ListUnspentWitnessFromDefaultAccount(
+		minConfs, maxConfs,
+	)
 	if err != nil {
 		return nil, err
 	}

pilot.go
@@ -12,6 +12,7 @@ import (
 	"github.com/lightningnetwork/lnd/chainreg"
 	"github.com/lightningnetwork/lnd/funding"
 	"github.com/lightningnetwork/lnd/lncfg"
+	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/tor"
 )
@@ -180,7 +181,9 @@ func initAutoPilot(svr *server, cfg *lncfg.AutoPilot,
 			netParams:     netParams,
 		},
 		WalletBalance: func() (btcutil.Amount, error) {
-			return svr.cc.Wallet.ConfirmedBalance(cfg.MinConfs)
+			return svr.cc.Wallet.ConfirmedBalance(
+				cfg.MinConfs, lnwallet.DefaultAccountName,
+			)
 		},
 		Graph:       autopilot.ChannelGraphFromDatabase(svr.localChanDB.ChannelGraph()),
 		Constraints: atplConstraints,

rpcserver.go
@@ -1023,7 +1023,7 @@ func (r *rpcServer) ListUnspent(ctx context.Context,
 	var utxos []*lnwallet.Utxo
 	err = r.server.cc.Wallet.WithCoinSelectLock(func() error {
 		utxos, err = r.server.cc.Wallet.ListUnspentWitness(
-			minConfs, maxConfs,
+			minConfs, maxConfs, "",
 		)
 		return err
 	})
@@ -1191,7 +1191,7 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 		sweepTxPkg, err := sweep.CraftSweepAllTx(
 			feePerKw, lnwallet.DefaultDustLimit(),
 			uint32(bestHeight), nil, targetAddr, wallet,
-			wallet.WalletController, wallet.WalletController,
+			wallet, wallet.WalletController,
 			r.server.cc.FeeEstimator, r.server.cc.Signer,
 		)
 		if err != nil {
@@ -1243,7 +1243,7 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 			sweepTxPkg, err = sweep.CraftSweepAllTx(
 				feePerKw, lnwallet.DefaultDustLimit(),
 				uint32(bestHeight), outputs, targetAddr, wallet,
-				wallet.WalletController, wallet.WalletController,
+				wallet, wallet.WalletController,
 				r.server.cc.FeeEstimator, r.server.cc.Signer,
 			)
 			if err != nil {
@@ -2763,7 +2763,9 @@ func (r *rpcServer) WalletBalance(ctx context.Context,
 	in *lnrpc.WalletBalanceRequest) (*lnrpc.WalletBalanceResponse, error) {
 
 	// Get total balance, from txs that have >= 0 confirmations.
-	totalBal, err := r.server.cc.Wallet.ConfirmedBalance(0)
+	totalBal, err := r.server.cc.Wallet.ConfirmedBalance(
+		0, lnwallet.DefaultAccountName,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -2771,7 +2773,9 @@ func (r *rpcServer) WalletBalance(ctx context.Context,
 	// Get confirmed balance, from txs that have >= 1 confirmations.
 	// TODO(halseth): get both unconfirmed and confirmed balance in one
 	// call, as this is racy.
-	confirmedBal, err := r.server.cc.Wallet.ConfirmedBalance(1)
+	confirmedBal, err := r.server.cc.Wallet.ConfirmedBalance(
+		1, lnwallet.DefaultAccountName,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -4963,7 +4967,7 @@ func (r *rpcServer) GetTransactions(ctx context.Context,
 	}
 
 	transactions, err := r.server.cc.Wallet.ListTransactionDetails(
-		req.StartHeight, endHeight,
+		req.StartHeight, endHeight, "",
 	)
 	if err != nil {
 		return nil, err

decred/dcrlnd

lnrpc/walletrpc/walletkit_server.go
@@ -267,7 +267,9 @@ func (w *WalletKit) ListUnspent(ctx context.Context,
 	// be shown available to us.
 	var utxos []*lnwallet.Utxo
 	err = w.cfg.CoinSelectionLocker.WithCoinSelectLock(func() error {
-		utxos, err = w.cfg.Wallet.ListUnspentWitness(minConfs, maxConfs)
+		utxos, err = w.cfg.Wallet.ListUnspentWitness(
+			minConfs, maxConfs, "",
+		)
 		return err
 	})
 	if err != nil {
@@ -773,7 +775,7 @@ func (w *WalletKit) ListSweeps(ctx context.Context,
 	// If the caller does want full transaction lookups, query our wallet
 	// for all transactions, including unconfirmed transactions.
 	transactions, err := w.cfg.Wallet.ListTransactionDetails(
-		0, dcrwallet.UnconfirmedHeight,
+		0, dcrwallet.UnconfirmedHeight, lnwallet.DefaultAccountName,
 	)
 	if err != nil {
 		return nil, err

lnwallet/interface.go
@@ -166,12 +166,14 @@ type WalletController interface {
 	// ConfirmedBalance returns the sum of all the wallet's unspent outputs
 	// that have at least confs confirmations. If confs is set to zero,
 	// then all unspent outputs, including those currently in the mempool
-	// will be included in the final sum.
+	// will be included in the final sum. The account parameter serves as a
+	// filter to retrieve the balance for a specific account. When empty,
+	// the confirmed balance of all wallet accounts is returned.
 	//
 	// NOTE: Only witness outputs should be included in the computation of
 	// the total spendable balance of the wallet. We require this as only
 	// witness inputs can be used for funding channels.
-	ConfirmedBalance(confs int32) (dcrutil.Amount, error)
+	ConfirmedBalance(confs int32, accountFilter string) (dcrutil.Amount, error)
 
 	// NewAddress returns the next external or internal address for the
 	// wallet dictated by the value of the `change` parameter. If change is
@@ -228,10 +230,13 @@ type WalletController interface {
 	// needs in order to be returned by this method. Passing -1 as
 	// 'minconfirms' indicates that even unconfirmed outputs should be
 	// returned. Using MaxInt32 as 'maxconfirms' implies returning all
-	// outputs with at least 'minconfirms'.
+	// outputs with at least 'minconfirms'. The account parameter serves as
+	// a filter to retrieve the unspent outputs for a specific account.
+	// When empty, the unspent outputs of all wallet accounts are returned.
 	//
 	// NOTE: This method requires the global coin selection lock to be held.
-	ListUnspentWitness(minconfirms, maxconfirms int32) ([]*Utxo, error)
+	ListUnspentWitness(minconfirms, maxconfirms int32,
+		accountFilter string) ([]*Utxo, error)
 
 	// ListTransactionDetails returns a list of all transactions which are
 	// relevant to the wallet over [startHeight;endHeight]. If start height
@@ -239,9 +244,11 @@ type WalletController interface {
 	// reverse order. To include unconfirmed transactions, endHeight should
 	// be set to the special value -1. This will return transactions from
 	// the tip of the chain until the start height (inclusive) and
-	// unconfirmed transactions.
-	ListTransactionDetails(startHeight,
-		endHeight int32) ([]*TransactionDetail, error)
+	// unconfirmed transactions. The account parameter serves as a filter to
+	// retrieve the transactions relevant to a specific account. When
+	// empty, transactions of all wallet accounts are returned.
+	ListTransactionDetails(startHeight, endHeight int32,
+		accountFilter string) ([]*TransactionDetail, error)
 
 	// LockOutpoint marks an outpoint as locked meaning it will no longer
 	// be deemed as eligible for coin selection. Locking outputs are

lnwallet/wallet.go
@@ -376,14 +376,33 @@ func (l *LightningWallet) Shutdown() error {
 	return nil
 }
 
-// ConfirmedBalance returns the current confirmed balance of the wallet. This
-// methods wraps the interal WalletController method so we're able to properly
-// hold the coin select mutex while we compute the balance.
-func (l *LightningWallet) ConfirmedBalance(confs int32) (dcrutil.Amount, error) {
+// ConfirmedBalance returns the current confirmed balance of a wallet account.
+// This methods wraps the internal WalletController method so we're able to
+// properly hold the coin select mutex while we compute the balance.
+func (l *LightningWallet) ConfirmedBalance(confs int32,
+	account string) (dcrutil.Amount, error) {
+
 	l.coinSelectMtx.Lock()
 	defer l.coinSelectMtx.Unlock()
 
-	return l.WalletController.ConfirmedBalance(confs)
+	return l.WalletController.ConfirmedBalance(confs, account)
+}
+
+// ListUnspentWitnessFromDefaultAccount returns all unspent outputs from the
+// default wallet account which are version 0 witness programs. The 'minConfs'
+// and 'maxConfs' parameters indicate the minimum and maximum number of
+// confirmations an output needs in order to be returned by this method. Passing
+// -1 as 'minConfs' indicates that even unconfirmed outputs should be returned.
+// Using MaxInt32 as 'maxConfs' implies returning all outputs with at least
+// 'minConfs'.
+//
+// NOTE: This method requires the global coin selection lock to be held.
+func (l *LightningWallet) ListUnspentWitnessFromDefaultAccount(
+	minConfs, maxConfs int32) ([]*Utxo, error) {
+
+	return l.WalletController.ListUnspentWitness(
+		minConfs, maxConfs, DefaultAccountName,
+	)
 }
 
 // LockedOutpoints returns a list of all currently locked outpoint.
@@ -1789,7 +1808,9 @@ func NewCoinSource(w *LightningWallet) *CoinSource {
 func (c *CoinSource) ListCoins(minConfs int32,
 	maxConfs int32) ([]chanfunding.Coin, error) {
 
-	utxos, err := c.wallet.ListUnspentWitness(minConfs, maxConfs)
+	utxos, err := c.wallet.ListUnspentWitnessFromDefaultAccount(
+		minConfs, maxConfs,
+	)
 	if err != nil {
 		return nil, err
 	}

pilot.go
@@ -10,6 +10,7 @@ import (
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/autopilot"
 	"github.com/decred/dcrlnd/lncfg"
+	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/tor"
 )
@@ -183,7 +184,9 @@ func initAutoPilot(svr *server, cfg *lncfg.AutoPilot,
 			chanMinHtlcIn: c.MinHTLCIn,
 		},
 		WalletBalance: func() (dcrutil.Amount, error) {
-			return svr.cc.wallet.ConfirmedBalance(cfg.MinConfs)
+			return svr.cc.wallet.ConfirmedBalance(
+				cfg.MinConfs, lnwallet.DefaultAccountName,
+			)
 		},
 		Graph:       autopilot.ChannelGraphFromDatabase(svr.localChanDB.ChannelGraph()),
 		Constraints: atplConstraints,

rpcserver.go
@@ -1109,7 +1109,7 @@ func (r *rpcServer) ListUnspent(ctx context.Context,
 	var utxos []*lnwallet.Utxo
 	err = r.server.cc.wallet.WithCoinSelectLock(func() error {
 		utxos, err = r.server.cc.wallet.ListUnspentWitness(
-			minConfs, maxConfs,
+			minConfs, maxConfs, "",
 		)
 		return err
 	})
@@ -1258,7 +1258,7 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 		// safe manner, so no need to worry about locking.
 		sweepTxPkg, err := sweep.CraftSweepAllTx(
 			feePerKB, uint32(bestHeight), targetAddr, wallet,
-			wallet.WalletController, wallet.WalletController,
+			wallet, wallet.WalletController,
 			r.server.cc.feeEstimator, r.server.cc.signer,
 			activeNetParams.Params,
 		)
@@ -2786,7 +2786,9 @@ func (r *rpcServer) WalletBalance(ctx context.Context,
 	in *lnrpc.WalletBalanceRequest) (*lnrpc.WalletBalanceResponse, error) {
 
 	// Get total balance, from txs that have >= 0 confirmations.
-	totalBal, err := r.server.cc.wallet.ConfirmedBalance(0)
+	totalBal, err := r.server.cc.wallet.ConfirmedBalance(
+		0, lnwallet.DefaultAccountName,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -2794,7 +2796,9 @@ func (r *rpcServer) WalletBalance(ctx context.Context,
 	// Get confirmed balance, from txs that have >= 1 confirmations.
 	// TODO(halseth): get both unconfirmed and confirmed balance in one
 	// call, as this is racy.
-	confirmedBal, err := r.server.cc.wallet.ConfirmedBalance(1)
+	confirmedBal, err := r.server.cc.wallet.ConfirmedBalance(
+		1, lnwallet.DefaultAccountName,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -5113,7 +5117,7 @@ func (r *rpcServer) GetTransactions(ctx context.Context,
 	}
 
 	transactions, err := r.server.cc.wallet.ListTransactionDetails(
-		req.StartHeight, endHeight,
+		req.StartHeight, endHeight, "",
 	)
 	if err != nil {
 		return nil, err