lightningnetwork/lnd

rpc: expose account parameter for several wallet RPCs

lnrpc/walletrpc/walletkit_server.go
@@ -322,7 +322,7 @@ func (w *WalletKit) ListUnspent(ctx context.Context,
 	var utxos []*lnwallet.Utxo
 	err = w.cfg.CoinSelectionLocker.WithCoinSelectLock(func() error {
 		utxos, err = w.cfg.Wallet.ListUnspentWitness(
-			minConfs, maxConfs, "",
+			minConfs, maxConfs, req.Account,
 		)
 		return err
 	})
@@ -489,8 +489,13 @@ func (w *WalletKit) DeriveKey(ctx context.Context,
 func (w *WalletKit) NextAddr(ctx context.Context,
 	req *AddrRequest) (*AddrResponse, error) {
 
+	account := lnwallet.DefaultAccountName
+	if req.Account != "" {
+		account = req.Account
+	}
+
 	addr, err := w.cfg.Wallet.NewAddress(
-		lnwallet.WitnessPubKey, false, lnwallet.DefaultAccountName,
+		lnwallet.WitnessPubKey, false, account,
 	)
 	if err != nil {
 		return nil, err
@@ -1055,12 +1060,19 @@ func (w *WalletKit) FundPsbt(_ context.Context,
 	// UTXOs.
 	changeIndex := int32(-1)
 	err = w.cfg.CoinSelectionLocker.WithCoinSelectLock(func() error {
+		// We'll assume the PSBT will be funded by the default account
+		// unless otherwise specified.
+		account := lnwallet.DefaultAccountName
+		if req.Account != "" {
+			account = req.Account
+		}
+
 		// In case the user did specify inputs, we need to make sure
 		// they are known to us, still unspent and not yet locked.
 		if len(packet.UnsignedTx.TxIn) > 0 {
 			// Get a list of all unspent witness outputs.
 			utxos, err := w.cfg.Wallet.ListUnspentWitness(
-				defaultMinConf, defaultMaxConf, "",
+				defaultMinConf, defaultMaxConf, account,
 			)
 			if err != nil {
 				return err
@@ -1079,7 +1091,7 @@ func (w *WalletKit) FundPsbt(_ context.Context,
 		// lock any coins but might still change the wallet DB by
 		// generating a new change address.
 		changeIndex, err = w.cfg.Wallet.FundPsbt(
-			packet, feeSatPerKW, lnwallet.DefaultAccountName,
+			packet, feeSatPerKW, account,
 		)
 		if err != nil {
 			return fmt.Errorf("wallet couldn't fund PSBT: %v", err)
@@ -1155,6 +1167,13 @@ func marshallLeases(locks []*wtxmgr.LockedOutput) []*UtxoLease {
 func (w *WalletKit) FinalizePsbt(_ context.Context,
 	req *FinalizePsbtRequest) (*FinalizePsbtResponse, error) {
 
+	// We'll assume the PSBT was funded by the default account unless
+	// otherwise specified.
+	account := lnwallet.DefaultAccountName
+	if req.Account != "" {
+		account = req.Account
+	}
+
 	// Parse the funded PSBT. No additional checks are required at this
 	// level as the wallet will perform all of them.
 	packet, err := psbt.NewFromRawBytes(
@@ -1167,7 +1186,7 @@ func (w *WalletKit) FinalizePsbt(_ context.Context,
 	// Let the wallet do the heavy lifting. This will sign all inputs that
 	// we have the UTXO for. If some inputs can't be signed and don't have
 	// witness data attached, this will fail.
-	err = w.cfg.Wallet.FinalizePsbt(packet)
+	err = w.cfg.Wallet.FinalizePsbt(packet, account)
 	if err != nil {
 		return nil, fmt.Errorf("error finalizing PSBT: %v", err)
 	}

rpcserver.go
@@ -1023,7 +1023,7 @@ func (r *rpcServer) ListUnspent(ctx context.Context,
 	var utxos []*lnwallet.Utxo
 	err = r.server.cc.Wallet.WithCoinSelectLock(func() error {
 		utxos, err = r.server.cc.Wallet.ListUnspentWitness(
-			minConfs, maxConfs, "",
+			minConfs, maxConfs, in.Account,
 		)
 		return err
 	})
@@ -1373,6 +1373,12 @@ func (r *rpcServer) SendMany(ctx context.Context,
 func (r *rpcServer) NewAddress(ctx context.Context,
 	in *lnrpc.NewAddressRequest) (*lnrpc.NewAddressResponse, error) {
 
+	// Always use the default wallet account unless one was specified.
+	account := lnwallet.DefaultAccountName
+	if in.Account != "" {
+		account = in.Account
+	}
+
 	// Translate the gRPC proto address type to the wallet controller's
 	// available address types.
 	var (
@@ -1382,7 +1388,7 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 	switch in.Type {
 	case lnrpc.AddressType_WITNESS_PUBKEY_HASH:
 		addr, err = r.server.cc.Wallet.NewAddress(
-			lnwallet.WitnessPubKey, false, lnwallet.DefaultAccountName,
+			lnwallet.WitnessPubKey, false, account,
 		)
 		if err != nil {
 			return nil, err
@@ -1390,7 +1396,7 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 
 	case lnrpc.AddressType_NESTED_PUBKEY_HASH:
 		addr, err = r.server.cc.Wallet.NewAddress(
-			lnwallet.NestedWitnessPubKey, false, lnwallet.DefaultAccountName,
+			lnwallet.NestedWitnessPubKey, false, account,
 		)
 		if err != nil {
 			return nil, err
@@ -1398,7 +1404,7 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 
 	case lnrpc.AddressType_UNUSED_WITNESS_PUBKEY_HASH:
 		addr, err = r.server.cc.Wallet.LastUnusedAddress(
-			lnwallet.WitnessPubKey, lnwallet.DefaultAccountName,
+			lnwallet.WitnessPubKey, account,
 		)
 		if err != nil {
 			return nil, err
@@ -1406,14 +1412,15 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 
 	case lnrpc.AddressType_UNUSED_NESTED_PUBKEY_HASH:
 		addr, err = r.server.cc.Wallet.LastUnusedAddress(
-			lnwallet.NestedWitnessPubKey, lnwallet.DefaultAccountName,
+			lnwallet.NestedWitnessPubKey, account,
 		)
 		if err != nil {
 			return nil, err
 		}
 	}
 
-	rpcsLog.Debugf("[newaddress] type=%v addr=%v", in.Type, addr.String())
+	rpcsLog.Debugf("[newaddress] account=%v type=%v addr=%v", account,
+		in.Type, addr.String())
 	return &lnrpc.NewAddressResponse{Address: addr.String()}, nil
 }
 
@@ -4967,7 +4974,7 @@ func (r *rpcServer) GetTransactions(ctx context.Context,
 	}
 
 	transactions, err := r.server.cc.Wallet.ListTransactionDetails(
-		req.StartHeight, endHeight, "",
+		req.StartHeight, endHeight, req.Account,
 	)
 	if err != nil {
 		return nil, err

decred/dcrlnd

lnrpc/walletrpc/walletkit_server.go
@@ -268,7 +268,7 @@ func (w *WalletKit) ListUnspent(ctx context.Context,
 	var utxos []*lnwallet.Utxo
 	err = w.cfg.CoinSelectionLocker.WithCoinSelectLock(func() error {
 		utxos, err = w.cfg.Wallet.ListUnspentWitness(
-			minConfs, maxConfs, "",
+			minConfs, maxConfs, req.Account,
 		)
 		return err
 	})
@@ -407,8 +407,13 @@ func (w *WalletKit) DeriveKey(ctx context.Context,
 func (w *WalletKit) NextAddr(ctx context.Context,
 	req *AddrRequest) (*AddrResponse, error) {
 
+	account := lnwallet.DefaultAccountName
+	if req.Account != "" {
+		account = req.Account
+	}
+
 	addr, err := w.cfg.Wallet.NewAddress(
-		lnwallet.PubKeyHash, false, lnwallet.DefaultAccountName,
+		lnwallet.PubKeyHash, false, account,
 	)
 	if err != nil {
 		return nil, err

rpcserver.go
@@ -1109,7 +1109,7 @@ func (r *rpcServer) ListUnspent(ctx context.Context,
 	var utxos []*lnwallet.Utxo
 	err = r.server.cc.wallet.WithCoinSelectLock(func() error {
 		utxos, err = r.server.cc.wallet.ListUnspentWitness(
-			minConfs, maxConfs, "",
+			minConfs, maxConfs, in.Account,
 		)
 		return err
 	})
@@ -1369,6 +1369,12 @@ func (r *rpcServer) SendMany(ctx context.Context,
 func (r *rpcServer) NewAddress(ctx context.Context,
 	in *lnrpc.NewAddressRequest) (*lnrpc.NewAddressResponse, error) {
 
+	// Always use the default wallet account unless one was specified.
+	account := lnwallet.DefaultAccountName
+	if in.Account != "" {
+		account = in.Account
+	}
+
 	// Translate the gRPC proto address type to the wallet controller's
 	// available address types.
 	var (
@@ -1378,14 +1384,14 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 	switch in.Type {
 	case lnrpc.AddressType_PUBKEY_HASH:
 		addr, err = r.server.cc.wallet.NewAddress(
-			lnwallet.PubKeyHash, false, lnwallet.DefaultAccountName,
+			lnwallet.PubKeyHash, false, account,
 		)
 		if err != nil {
 			return nil, err
 		}
 	case lnrpc.AddressType_UNUSED_PUBKEY_HASH:
 		addr, err = r.server.cc.wallet.LastUnusedAddress(
-			lnwallet.PubKeyHash, lnwallet.DefaultAccountName,
+			lnwallet.PubKeyHash, account,
 		)
 		if err != nil {
 			return nil, err
@@ -1395,7 +1401,8 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 		return nil, fmt.Errorf("unsupported address type %s", in.Type)
 	}
 
-	rpcsLog.Debugf("[newaddress] type=%v addr=%v", in.Type, addr.String())
+	rpcsLog.Debugf("[newaddress] account=%v type=%v addr=%v", account,
+		in.Type, addr.String())
 	return &lnrpc.NewAddressResponse{Address: addr.String()}, nil
 }
 
@@ -5117,7 +5124,7 @@ func (r *rpcServer) GetTransactions(ctx context.Context,
 	}
 
 	transactions, err := r.server.cc.wallet.ListTransactionDetails(
-		req.StartHeight, endHeight, "",
+		req.StartHeight, endHeight, req.Account,
 	)
 	if err != nil {
 		return nil, err