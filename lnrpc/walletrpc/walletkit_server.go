lightningnetwork/lnd

lnwallet: expose required account parameter for WalletController methods

lnrpc/walletrpc/walletkit_server.go
@@ -487,7 +487,9 @@ func (w *WalletKit) DeriveKey(ctx context.Context,
 func (w *WalletKit) NextAddr(ctx context.Context,
 	req *AddrRequest) (*AddrResponse, error) {
 
-	addr, err := w.cfg.Wallet.NewAddress(lnwallet.WitnessPubKey, false)
+	addr, err := w.cfg.Wallet.NewAddress(
+		lnwallet.WitnessPubKey, false, lnwallet.DefaultAccountName,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -1073,7 +1075,9 @@ func (w *WalletKit) FundPsbt(_ context.Context,
 		// We can now ask the wallet to fund the TX. This will not yet
 		// lock any coins but might still change the wallet DB by
 		// generating a new change address.
-		changeIndex, err = w.cfg.Wallet.FundPsbt(packet, feeSatPerKW)
+		changeIndex, err = w.cfg.Wallet.FundPsbt(
+			packet, feeSatPerKW, lnwallet.DefaultAccountName,
+		)
 		if err != nil {
 			return fmt.Errorf("wallet couldn't fund PSBT: %v", err)
 		}

lnwallet/interface.go
@@ -17,6 +17,12 @@ import (
 	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 )
 
+const (
+	// DefaultAccountName is the name for the default account used to manage
+	// on-chain funds within the wallet.
+	DefaultAccountName = "default"
+)
+
 // AddressType is an enum-like type which denotes the possible address types
 // WalletController supports.
 type AddressType uint8
@@ -165,8 +171,10 @@ type WalletController interface {
 	// true, then an internal address should be used, otherwise an external
 	// address should be returned. The type of address returned is dictated
 	// by the wallet's capabilities, and may be of type: p2sh, p2wkh,
-	// p2wsh, etc.
-	NewAddress(addrType AddressType, change bool) (btcutil.Address, error)
+	// p2wsh, etc. The account parameter must be non-empty as it determines
+	// which account the address should be generated from.
+	NewAddress(addrType AddressType, change bool,
+		account string) (btcutil.Address, error)
 
 	// LastUnusedAddress returns the last *unused* address known by the
 	// wallet. An address is unused if it hasn't received any payments.
@@ -174,7 +182,10 @@ type WalletController interface {
 	// "freshest" address without having to worry about "address inflation"
 	// caused by continual refreshing. Similar to NewAddress it can derive
 	// a specified address type. By default, this is a non-change address.
-	LastUnusedAddress(addrType AddressType) (btcutil.Address, error)
+	// The account parameter must be non-empty as it determines which
+	// account the address should be generated from.
+	LastUnusedAddress(addrType AddressType,
+		account string) (btcutil.Address, error)
 
 	// IsOurAddress checks if the passed address belongs to this wallet
 	IsOurAddress(a btcutil.Address) bool
@@ -286,28 +297,29 @@ type WalletController interface {
 	// is returned.
 	//
 	// NOTE: If the packet doesn't contain any inputs, coin selection is
-	// performed automatically. If the packet does contain any inputs, it is
-	// assumed that full coin selection happened externally and no
-	// additional inputs are added. If the specified inputs aren't enough to
-	// fund the outputs with the given fee rate, an error is returned.
-	// No lock lease is acquired for any of the selected/validated inputs.
-	// It is in the caller's responsibility to lock the inputs before
-	// handing them out.
-	FundPsbt(packet *psbt.Packet, feeRate chainfee.SatPerKWeight) (int32,
-		error)
+	// performed automatically. The account parameter must be non-empty as
+	// it determines which set of coins are eligible for coin selection. If
+	// the packet does contain any inputs, it is assumed that full coin
+	// selection happened externally and no additional inputs are added. If
+	// the specified inputs aren't enough to fund the outputs with the given
+	// fee rate, an error is returned. No lock lease is acquired for any of
+	// the selected/validated inputs. It is in the caller's responsibility
+	// to lock the inputs before handing them out.
+	FundPsbt(packet *psbt.Packet, feeRate chainfee.SatPerKWeight,
+		account string) (int32, error)
 
 	// FinalizePsbt expects a partial transaction with all inputs and
 	// outputs fully declared and tries to sign all inputs that belong to
-	// the wallet. Lnd must be the last signer of the transaction. That
-	// means, if there are any unsigned non-witness inputs or inputs without
-	// UTXO information attached or inputs without witness data that do not
-	// belong to lnd's wallet, this method will fail. If no error is
-	// returned, the PSBT is ready to be extracted and the final TX within
-	// to be broadcast.
+	// the specified account. Lnd must be the last signer of the
+	// transaction. That means, if there are any unsigned non-witness inputs
+	// or inputs without UTXO information attached or inputs without witness
+	// data that do not belong to lnd's wallet, this method will fail. If no
+	// error is returned, the PSBT is ready to be extracted and the final TX
+	// within to be broadcast.
 	//
 	// NOTE: This method does NOT publish the transaction after it's been
 	// finalized successfully.
-	FinalizePsbt(packet *psbt.Packet) error
+	FinalizePsbt(packet *psbt.Packet, account string) error
 
 	// SubscribeTransactions returns a TransactionSubscription client which
 	// is capable of receiving async notifications as new transactions

lnwallet/wallet.go
@@ -722,7 +722,9 @@ func (l *LightningWallet) handleFundingReserveRequest(req *InitFundingReserveMsg
 			SubtractFees: req.SubtractFees,
 			FeeRate:      req.FundingFeePerKw,
 			ChangeAddr: func() (btcutil.Address, error) {
-				return l.NewAddress(WitnessPubKey, true)
+				return l.NewAddress(
+					WitnessPubKey, true, DefaultAccountName,
+				)
 			},
 		}
 		fundingIntent, err = req.ChanFunder.ProvisionChannel(

peer/brontide.go
@@ -2043,7 +2043,7 @@ func (p *Brontide) ChannelSnapshots() []*channeldb.ChannelSnapshot {
 // the case of a cooperative channel close negotiation.
 func (p *Brontide) genDeliveryScript() ([]byte, error) {
 	deliveryAddr, err := p.cfg.Wallet.NewAddress(
-		lnwallet.WitnessPubKey, false,
+		lnwallet.WitnessPubKey, false, lnwallet.DefaultAccountName,
 	)
 	if err != nil {
 		return nil, err
@@ -2333,10 +2333,14 @@ func (p *Brontide) fetchActiveChanCloser(chanID lnwire.ChannelID) (
 				"channel w/ active htlcs")
 		}
 
-		// We'll create a valid closing state machine in order to respond to the
-		// initiated cooperative channel closure. First, we set the delivery
-		// script that our funds will be paid out to. If an upfront shutdown script
-		// was set, we will use it. Otherwise, we get a fresh delivery script.
+		// We'll create a valid closing state machine in order to
+		// respond to the initiated cooperative channel closure. First,
+		// we set the delivery script that our funds will be paid out
+		// to. If an upfront shutdown script was set, we will use it.
+		// Otherwise, we get a fresh delivery script.
+		//
+		// TODO: Expose option to allow upfront shutdown script from
+		// watch-only accounts.
 		deliveryScript := channel.LocalUpfrontShutdownScript()
 		if len(deliveryScript) == 0 {
 			var err error

rpcserver.go
@@ -1225,6 +1225,7 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 			// allowing us to pass the reserved value check.
 			changeAddr, err := r.server.cc.Wallet.NewAddress(
 				lnwallet.WitnessPubKey, true,
+				lnwallet.DefaultAccountName,
 			)
 			if err != nil {
 				return nil, err
@@ -1381,7 +1382,7 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 	switch in.Type {
 	case lnrpc.AddressType_WITNESS_PUBKEY_HASH:
 		addr, err = r.server.cc.Wallet.NewAddress(
-			lnwallet.WitnessPubKey, false,
+			lnwallet.WitnessPubKey, false, lnwallet.DefaultAccountName,
 		)
 		if err != nil {
 			return nil, err
@@ -1389,7 +1390,7 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 
 	case lnrpc.AddressType_NESTED_PUBKEY_HASH:
 		addr, err = r.server.cc.Wallet.NewAddress(
-			lnwallet.NestedWitnessPubKey, false,
+			lnwallet.NestedWitnessPubKey, false, lnwallet.DefaultAccountName,
 		)
 		if err != nil {
 			return nil, err
@@ -1397,7 +1398,7 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 
 	case lnrpc.AddressType_UNUSED_WITNESS_PUBKEY_HASH:
 		addr, err = r.server.cc.Wallet.LastUnusedAddress(
-			lnwallet.WitnessPubKey,
+			lnwallet.WitnessPubKey, lnwallet.DefaultAccountName,
 		)
 		if err != nil {
 			return nil, err
@@ -1405,7 +1406,7 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 
 	case lnrpc.AddressType_UNUSED_NESTED_PUBKEY_HASH:
 		addr, err = r.server.cc.Wallet.LastUnusedAddress(
-			lnwallet.NestedWitnessPubKey,
+			lnwallet.NestedWitnessPubKey, lnwallet.DefaultAccountName,
 		)
 		if err != nil {
 			return nil, err

server.go
@@ -3809,7 +3809,9 @@ func newSweepPkScriptGen(
 	wallet lnwallet.WalletController) func() ([]byte, error) {
 
 	return func() ([]byte, error) {
-		sweepAddr, err := wallet.NewAddress(lnwallet.WitnessPubKey, false)
+		sweepAddr, err := wallet.NewAddress(
+			lnwallet.WitnessPubKey, false, lnwallet.DefaultAccountName,
+		)
 		if err != nil {
 			return nil, err
 		}

decred/dcrlnd

lnrpc/walletrpc/walletkit_server.go
@@ -405,7 +405,9 @@ func (w *WalletKit) DeriveKey(ctx context.Context,
 func (w *WalletKit) NextAddr(ctx context.Context,
 	req *AddrRequest) (*AddrResponse, error) {
 
-	addr, err := w.cfg.Wallet.NewAddress(lnwallet.PubKeyHash, false)
+	addr, err := w.cfg.Wallet.NewAddress(
+		lnwallet.PubKeyHash, false, lnwallet.DefaultAccountName,
+	)
 	if err != nil {
 		return nil, err
 	}

lnwallet/interface.go
@@ -16,6 +16,12 @@ import (
 	"github.com/decred/dcrlnd/lnwallet/chainfee"
 )
 
+const (
+	// DefaultAccountName is the name for the default account used to manage
+	// on-chain funds within the wallet.
+	DefaultAccountName = "default"
+)
+
 // AddressType is an enum-like type which denotes the possible address types
 // WalletController supports.
 type AddressType uint8
@@ -171,9 +177,11 @@ type WalletController interface {
 	// wallet dictated by the value of the `change` parameter. If change is
 	// true, then an internal address should be used, otherwise an external
 	// address should be returned. The type of address returned is dictated
-	// by the wallet's capabilities, and may be of type: p2sh, p2pkh,
-	// etc.
-	NewAddress(addrType AddressType, change bool) (stdaddr.Address, error)
+	// by the wallet's capabilities, and may be of type: p2sh, p2wkh,
+	// p2wsh, etc. The account parameter must be non-empty as it determines
+	// which account the address should be generated from.
+	NewAddress(addrType AddressType, change bool,
+		account string) (stdaddr.Address, error)
 
 	// LastUnusedAddress returns the last *unused* address known by the
 	// wallet. An address is unused if it hasn't received any payments.
@@ -181,7 +189,10 @@ type WalletController interface {
 	// "freshest" address without having to worry about "address inflation"
 	// caused by continual refreshing. Similar to NewAddress it can derive
 	// a specified address type. By default, this is a non-change address.
-	LastUnusedAddress(addrType AddressType) (stdaddr.Address, error)
+	// The account parameter must be non-empty as it determines which
+	// account the address should be generated from.
+	LastUnusedAddress(addrType AddressType,
+		account string) (stdaddr.Address, error)
 
 	// IsOurAddress checks if the passed address belongs to this wallet
 	IsOurAddress(a stdaddr.Address) bool

lnwallet/wallet.go
@@ -659,7 +659,9 @@ func (l *LightningWallet) handleFundingReserveRequest(req *InitFundingReserveMsg
 			SubtractFees: req.SubtractFees,
 			FeeRate:      req.FundingFeePerKB,
 			ChangeAddr: func() (stdaddr.Address, error) {
-				return l.NewAddress(PubKeyHash, true)
+				return l.NewAddress(
+					PubKeyHash, true, DefaultAccountName,
+				)
 			},
 		}
 		fundingIntent, err = req.ChanFunder.ProvisionChannel(

peer/brontide.go
@@ -1843,7 +1843,7 @@ func (p *Brontide) ChannelSnapshots() []*channeldb.ChannelSnapshot {
 // the case of a cooperative channel close negotiation.
 func (p *Brontide) genDeliveryScript() ([]byte, error) {
 	deliveryAddr, err := p.cfg.Wallet.NewAddress(
-		lnwallet.PubKeyHash, false,
+		lnwallet.PubKeyHash, false, lnwallet.DefaultAccountName,
 	)
 	if err != nil {
 		return nil, err
@@ -2131,10 +2131,14 @@ func (p *Brontide) fetchActiveChanCloser(chanID lnwire.ChannelID) (
 				"channel w/ active htlcs")
 		}
 
-		// We'll create a valid closing state machine in order to respond to the
-		// initiated cooperative channel closure. First, we set the delivery
-		// script that our funds will be paid out to. If an upfront shutdown script
-		// was set, we will use it. Otherwise, we get a fresh delivery script.
+		// We'll create a valid closing state machine in order to
+		// respond to the initiated cooperative channel closure. First,
+		// we set the delivery script that our funds will be paid out
+		// to. If an upfront shutdown script was set, we will use it.
+		// Otherwise, we get a fresh delivery script.
+		//
+		// TODO: Expose option to allow upfront shutdown script from
+		// watch-only accounts.
 		deliveryScript := channel.LocalUpfrontShutdownScript()
 		if len(deliveryScript) == 0 {
 			var err error

rpcserver.go
@@ -1378,14 +1378,14 @@ func (r *rpcServer) NewAddress(ctx context.Context,
 	switch in.Type {
 	case lnrpc.AddressType_PUBKEY_HASH:
 		addr, err = r.server.cc.wallet.NewAddress(
-			lnwallet.PubKeyHash, false,
+			lnwallet.PubKeyHash, false, lnwallet.DefaultAccountName,
 		)
 		if err != nil {
 			return nil, err
 		}
 	case lnrpc.AddressType_UNUSED_PUBKEY_HASH:
 		addr, err = r.server.cc.wallet.LastUnusedAddress(
-			lnwallet.PubKeyHash,
+			lnwallet.PubKeyHash, lnwallet.DefaultAccountName,
 		)
 		if err != nil {
 			return nil, err

server.go
@@ -3706,7 +3706,9 @@ func newSweepPkScriptGen(
 	wallet lnwallet.WalletController) func() ([]byte, error) {
 
 	return func() ([]byte, error) {
-		sweepAddr, err := wallet.NewAddress(lnwallet.PubKeyHash, false)
+		sweepAddr, err := wallet.NewAddress(
+			lnwallet.PubKeyHash, false, lnwallet.DefaultAccountName,
+		)
 		if err != nil {
 			return nil, err
 		}