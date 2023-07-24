halseth/lnd

sweep: pass dustLimit to CreateSweepTx

rpcserver.go
@@ -1254,7 +1254,8 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 		// single transaction. This will be generated in a concurrent
 		// safe manner, so no need to worry about locking.
 		sweepTxPkg, err := sweep.CraftSweepAllTx(
-			feePerKw, uint32(bestHeight), targetAddr, wallet,
+			feePerKw, lnwallet.DefaultDustLimit(),
+			uint32(bestHeight), targetAddr, wallet,
 			wallet.WalletController, wallet.WalletController,
 			r.server.cc.FeeEstimator, r.server.cc.Signer,
 		)

sweep/sweeper.go
@@ -1169,7 +1169,7 @@ func (s *UtxoSweeper) sweep(inputs inputSet, feeRate chainfee.SatPerKWeight,
 	// Create sweep tx.
 	tx, err := createSweepTx(
 		inputs, s.currentOutputScript, uint32(currentHeight), feeRate,
-		s.cfg.Signer,
+		dustLimit(s.relayFeeRate), s.cfg.Signer,
 	)
 	if err != nil {
 		return fmt.Errorf("create sweep tx: %v", err)
@@ -1459,7 +1459,8 @@ func (s *UtxoSweeper) CreateSweepTx(inputs []input.Input, feePref FeePreference,
 	}
 
 	return createSweepTx(
-		inputs, pkScript, currentBlockHeight, feePerKw, s.cfg.Signer,
+		inputs, pkScript, currentBlockHeight, feePerKw,
+		dustLimit(s.relayFeeRate), s.cfg.Signer,
 	)
 }
 

sweep/tx_input_set.go
@@ -145,14 +145,18 @@ type txInputSet struct {
 	wallet Wallet
 }
 
+func dustLimit(relayFee chainfee.SatPerKWeight) btcutil.Amount {
+	return txrules.GetDustThreshold(
+		input.P2WPKHSize,
+		btcutil.Amount(relayFee.FeePerKVByte()),
+	)
+}
+
 // newTxInputSet constructs a new, empty input set.
 func newTxInputSet(wallet Wallet, feePerKW,
 	relayFee chainfee.SatPerKWeight, maxInputs int) *txInputSet {
 
-	dustLimit := txrules.GetDustThreshold(
-		input.P2WPKHSize,
-		btcutil.Amount(relayFee.FeePerKVByte()),
-	)
+	dustLimit := dustLimit(relayFee)
 
 	state := txInputSetState{
 		feeRate: feePerKW,

sweep/walletsweep.go
@@ -153,10 +153,10 @@ type WalletSweepPackage struct {
 // by the delivery address. The sweep transaction will be crafted with the
 // target fee rate, and will use the utxoSource and outpointLocker as sources
 // for wallet funds.
-func CraftSweepAllTx(feeRate chainfee.SatPerKWeight, blockHeight uint32,
-	deliveryAddr btcutil.Address, coinSelectLocker CoinSelectionLocker,
-	utxoSource UtxoSource, outpointLocker OutpointLocker,
-	feeEstimator chainfee.Estimator,
+func CraftSweepAllTx(feeRate chainfee.SatPerKWeight, dustLimit btcutil.Amount,
+	blockHeight uint32, deliveryAddr btcutil.Address,
+	coinSelectLocker CoinSelectionLocker, utxoSource UtxoSource,
+	outpointLocker OutpointLocker, feeEstimator chainfee.Estimator,
 	signer input.Signer) (*WalletSweepPackage, error) {
 
 	// TODO(roasbeef): turn off ATPL as well when available?
@@ -273,7 +273,8 @@ func CraftSweepAllTx(feeRate chainfee.SatPerKWeight, blockHeight uint32,
 	// Finally, we'll ask the sweeper to craft a sweep transaction which
 	// respects our fee preference and targets all the UTXOs of the wallet.
 	sweepTx, err := createSweepTx(
-		inputsToSweep, deliveryPkScript, blockHeight, feeRate, signer,
+		inputsToSweep, deliveryPkScript, blockHeight, feeRate,
+		dustLimit, signer,
 	)
 	if err != nil {
 		unlockOutputs()

decred/dcrlnd

rpcserver.go
@@ -1275,7 +1275,8 @@ func (r *rpcServer) SendCoins(ctx context.Context,
 		// single transaction. This will be generated in a concurrent
 		// safe manner, so no need to worry about locking.
 		sweepTxPkg, err := sweep.CraftSweepAllTx(
-			feePerKB, uint32(bestHeight), targetAddr, wallet,
+			feePerKB, lnwallet.DefaultDustLimit(),
+			uint32(bestHeight), targetAddr, wallet,
 			wallet, wallet.WalletController,
 			r.server.cc.FeeEstimator, r.server.cc.Signer,
 			r.cfg.ActiveNetParams.Params,

sweep/sweeper.go
@@ -1213,7 +1213,7 @@ func (s *UtxoSweeper) sweep(inputs inputSet, feeRate chainfee.AtomPerKByte,
 	// Create sweep tx.
 	tx, err := createSweepTx(
 		inputs, s.currentOutputScript, uint32(currentHeight), feeRate,
-		s.cfg.Signer, s.cfg.NetParams,
+		dustLimit(s.relayFeeRate), s.cfg.Signer, s.cfg.NetParams,
 	)
 	if err != nil {
 		return fmt.Errorf("create sweep tx: %v", err)
@@ -1503,7 +1503,8 @@ func (s *UtxoSweeper) CreateSweepTx(inputs []input.Input, feePref FeePreference,
 	}
 
 	return createSweepTx(
-		inputs, pkScript, currentBlockHeight, feePerKB, s.cfg.Signer, s.cfg.NetParams,
+		inputs, pkScript, currentBlockHeight, feePerKB,
+		dustLimit(s.relayFeeRate), s.cfg.Signer, s.cfg.NetParams,
 	)
 }
 

sweep/tx_input_set.go
@@ -82,13 +82,15 @@ type txInputSet struct {
 	wallet Wallet
 }
 
+func dustLimit(relayFee chainfee.AtomPerKByte) dcrutil.Amount {
+	return lnwallet.DustThresholdForRelayFee(relayFee)
+}
+
 // newTxInputSet constructs a new, empty input set.
 func newTxInputSet(wallet Wallet, feePerKB,
 	relayFee chainfee.AtomPerKByte, maxInputs int) *txInputSet {
 
-	// Calculate dust limit based on the P2PKH output script of the sweep
-	// txes.
-	dustLimit := lnwallet.DustThresholdForRelayFee(relayFee)
+	dustLimit := dustLimit(relayFee)
 
 	state := txInputSetState{
 		sizeEstimate: newSizeEstimator(feePerKB),

sweep/walletsweep.go
@@ -5,6 +5,7 @@ import (
 	"math"
 
 	"github.com/decred/dcrd/chaincfg/v3"
+	"github.com/decred/dcrd/dcrutil/v4"
 	"github.com/decred/dcrd/txscript/v4"
 	"github.com/decred/dcrd/txscript/v4/stdaddr"
 	"github.com/decred/dcrd/txscript/v4/stdscript"
@@ -158,10 +159,10 @@ type WalletSweepPackage struct {
 // by the delivery address. The sweep transaction will be crafted with the
 // target fee rate, and will use the utxoSource and outpointLocker as sources
 // for wallet funds.
-func CraftSweepAllTx(feeRate chainfee.AtomPerKByte, blockHeight uint32,
-	deliveryAddr stdaddr.Address, coinSelectLocker CoinSelectionLocker,
-	utxoSource UtxoSource, outpointLocker OutpointLocker,
-	feeEstimator chainfee.Estimator,
+func CraftSweepAllTx(feeRate chainfee.AtomPerKByte, dustLimit dcrutil.Amount,
+	blockHeight uint32, deliveryAddr stdaddr.Address,
+	coinSelectLocker CoinSelectionLocker, utxoSource UtxoSource,
+	outpointLocker OutpointLocker, feeEstimator chainfee.Estimator,
 	signer input.Signer, netParams *chaincfg.Params) (*WalletSweepPackage, error) {
 
 	// TODO(roasbeef): turn off ATPL as well when available?
@@ -276,8 +277,8 @@ func CraftSweepAllTx(feeRate chainfee.AtomPerKByte, blockHeight uint32,
 	// Finally, we'll ask the sweeper to craft a sweep transaction which
 	// respects our fee preference and targets all the UTXOs of the wallet.
 	sweepTx, err := createSweepTx(
-		inputsToSweep, deliveryPkScript, blockHeight, feeRate, signer,
-		netParams,
+		inputsToSweep, deliveryPkScript, blockHeight, feeRate,
+		dustLimit, signer, netParams,
 	)
 	if err != nil {
 		unlockOutputs()