lightningnetwork/lnd

kvdb: add timeout options for bbolt (#4787)
* mod: bump btcwallet version to accept db timeout

* btcwallet: add DBTimeOut in config

* kvdb: add database timeout option for bbolt

This commit adds a DBTimeout option in bbolt config. The relevant
functions walletdb.Open/Create are updated to use this config. In
addition, the bolt compacter also applies the new timeout option.

* channeldb: add DBTimeout in db options

This commit adds the DBTimeout option for channeldb. A new unit
test file is created to test the default options. In addition,
the params used in kvdb.Create inside channeldb_test is updated
with a DefaultDBTimeout value.

* contractcourt+routing: use DBTimeout in kvdb

This commit touches multiple test files in contractcourt and routing.
The call of function kvdb.Create and kvdb.Open are now updated with
the new param DBTimeout, using the default value kvdb.DefaultDBTimeout.

* lncfg: add DBTimeout option in db config

The DBTimeout option is added to db config. A new unit test is
added to check the default DB config is created as expected.

* migration: add DBTimeout param in kvdb.Create/kvdb.Open

* keychain: update tests to use DBTimeout param

* htlcswitch+chainreg: add DBTimeout option

* macaroons: support DBTimeout config in creation

This commit adds the DBTimeout during the creation of macaroons.db.
The usage of kvdb.Create and kvdb.Open in its tests are updated with
a timeout value using kvdb.DefaultDBTimeout.

* walletunlocker: add dbTimeout option in UnlockerService

This commit adds a new param, dbTimeout, during the creation of
UnlockerService. This param is then passed to wallet.NewLoader
inside various service calls, specifying a timeout value to be
used when opening the bbolt. In addition, the macaroonService
is also called with this dbTimeout param.

* watchtower/wtdb: add dbTimeout param during creation

This commit adds the dbTimeout param for the creation of both
watchtower.db and

lncfg/db_test.go
@@ -1,0 +1,24 @@
+package lncfg_test
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
+	"github.com/lightningnetwork/lnd/lncfg"
+	"github.com/stretchr/testify/require"
+)
+
+// TestDBDefaultConfig tests that the default DB config is created as expected.
+func TestDBDefaultConfig(t *testing.T) {
+	defaultConfig := lncfg.DefaultDB()
+
+	require.Equal(t, lncfg.BoltBackend, defaultConfig.Backend)
+	require.Equal(
+		t, kvdb.DefaultBoltAutoCompactMinAge,
+		defaultConfig.Bolt.AutoCompactMinAge,
+	)
+	require.Equal(t, kvdb.DefaultDBTimeout, defaultConfig.Bolt.DBTimeout)
+	// Implicitly, the following fields are default to false.
+	require.False(t, defaultConfig.Bolt.AutoCompact)
+	require.False(t, defaultConfig.Bolt.SyncFreelist)
+}

macaroons/service_test.go
@@ -40,6 +40,7 @@ func setupTestRootKeyStorage(t *testing.T) string {
 	}
 	db, err := kvdb.Create(
 		kvdb.BoltBackendName, path.Join(tempDir, "macaroons.db"), true,
+		kvdb.DefaultDBTimeout,
 	)
 	if err != nil {
 		t.Fatalf("Error opening store DB: %v", err)
@@ -67,7 +68,8 @@ func TestNewService(t *testing.T) {
 	// Second, create the new service instance, unlock it and pass in a
 	// checker that we expect it to add to the bakery.
 	service, err := macaroons.NewService(
-		tempDir, "lnd", false, macaroons.IPLockChecker,
+		tempDir, "lnd", false, kvdb.DefaultDBTimeout,
+		macaroons.IPLockChecker,
 	)
 	if err != nil {
 		t.Fatalf("Error creating new service: %v", err)
@@ -118,7 +120,8 @@ func TestValidateMacaroon(t *testing.T) {
 	tempDir := setupTestRootKeyStorage(t)
 	defer os.RemoveAll(tempDir)
 	service, err := macaroons.NewService(
-		tempDir, "lnd", false, macaroons.IPLockChecker,
+		tempDir, "lnd", false, kvdb.DefaultDBTimeout,
+		macaroons.IPLockChecker,
 	)
 	if err != nil {
 		t.Fatalf("Error creating new service: %v", err)
@@ -178,7 +181,8 @@ func TestListMacaroonIDs(t *testing.T) {
 	// Second, create the new service instance, unlock it and pass in a
 	// checker that we expect it to add to the bakery.
 	service, err := macaroons.NewService(
-		tempDir, "lnd", false, macaroons.IPLockChecker,
+		tempDir, "lnd", false, kvdb.DefaultDBTimeout,
+		macaroons.IPLockChecker,
 	)
 	require.NoError(t, err, "Error creating new service")
 	defer service.Close()
@@ -210,7 +214,8 @@ func TestDeleteMacaroonID(t *testing.T) {
 	// Second, create the new service instance, unlock it and pass in a
 	// checker that we expect it to add to the bakery.
 	service, err := macaroons.NewService(
-		tempDir, "lnd", false, macaroons.IPLockChecker,
+		tempDir, "lnd", false, kvdb.DefaultDBTimeout,
+		macaroons.IPLockChecker,
 	)
 	require.NoError(t, err, "Error creating new service")
 	defer service.Close()

decred/dcrlnd

lncfg/db_test.go
@@ -1,0 +1,24 @@
+package lncfg_test
+
+import (
+	"testing"
+
+	"github.com/decred/dcrlnd/channeldb/kvdb"
+	"github.com/decred/dcrlnd/lncfg"
+	"github.com/stretchr/testify/require"
+)
+
+// TestDBDefaultConfig tests that the default DB config is created as expected.
+func TestDBDefaultConfig(t *testing.T) {
+	defaultConfig := lncfg.DefaultDB()
+
+	require.Equal(t, lncfg.BoltBackend, defaultConfig.Backend)
+	require.Equal(
+		t, kvdb.DefaultBoltAutoCompactMinAge,
+		defaultConfig.Bolt.AutoCompactMinAge,
+	)
+	require.Equal(t, kvdb.DefaultDBTimeout, defaultConfig.Bolt.DBTimeout)
+	// Implicitly, the following fields are default to false.
+	require.False(t, defaultConfig.Bolt.AutoCompact)
+	require.False(t, defaultConfig.Bolt.SyncFreelist)
+}

macaroons/service_test.go
@@ -39,7 +39,8 @@ func setupTestRootKeyStorage(t *testing.T) string {
 		t.Fatalf("Error creating temp dir: %v", err)
 	}
 	db, err := kvdb.Create(
-		kvdb.BoltBackendName, path.Join(tempDir, "macaroons.db"), true, kvdb.DefaultDBTimeout,
+		kvdb.BoltBackendName, path.Join(tempDir, "macaroons.db"), true,
+		kvdb.DefaultDBTimeout,
 	)
 	if err != nil {
 		t.Fatalf("Error opening store DB: %v", err)
@@ -67,7 +68,8 @@ func TestNewService(t *testing.T) {
 	// Second, create the new service instance, unlock it and pass in a
 	// checker that we expect it to add to the bakery.
 	service, err := macaroons.NewService(
-		tempDir, "dcrlnd", false, macaroons.IPLockChecker,
+		tempDir, "dcrlnd", false, kvdb.DefaultDBTimeout,
+		macaroons.IPLockChecker,
 	)
 	if err != nil {
 		t.Fatalf("Error creating new service: %v", err)
@@ -118,7 +120,8 @@ func TestValidateMacaroon(t *testing.T) {
 	tempDir := setupTestRootKeyStorage(t)
 	defer os.RemoveAll(tempDir)
 	service, err := macaroons.NewService(
-		tempDir, "dcrlnd", false, macaroons.IPLockChecker,
+		tempDir, "dcrlnd", false, kvdb.DefaultDBTimeout,
+		macaroons.IPLockChecker,
 	)
 	if err != nil {
 		t.Fatalf("Error creating new service: %v", err)
@@ -178,7 +181,8 @@ func TestListMacaroonIDs(t *testing.T) {
 	// Second, create the new service instance, unlock it and pass in a
 	// checker that we expect it to add to the bakery.
 	service, err := macaroons.NewService(
-		tempDir, "dcrlnd", false, macaroons.IPLockChecker,
+		tempDir, "dcrlnd", false, kvdb.DefaultDBTimeout,
+		macaroons.IPLockChecker,
 	)
 	require.NoError(t, err, "Error creating new service")
 	defer service.Close()
@@ -210,7 +214,8 @@ func TestDeleteMacaroonID(t *testing.T) {
 	// Second, create the new service instance, unlock it and pass in a
 	// checker that we expect it to add to the bakery.
 	service, err := macaroons.NewService(
-		tempDir, "dcrlnd", false, macaroons.IPLockChecker,
+		tempDir, "dcrlnd", false, kvdb.DefaultDBTimeout,
+		macaroons.IPLockChecker,
 	)
 	require.NoError(t, err, "Error creating new service")
 	defer service.Close()