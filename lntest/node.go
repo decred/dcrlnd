lightningnetwork/lnd

itest: save temp miner's logs

lntest/node.go
@@ -19,6 +19,8 @@ import (
 
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/integration/rpctest"
+	"github.com/btcsuite/btcd/rpcclient"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/go-errors/errors"
@@ -349,6 +351,59 @@ func newNode(cfg NodeConfig) (*HarnessNode, error) {
 		closedChans:  make(map[wire.OutPoint]struct{}),
 		closeClients: make(map[wire.OutPoint][]chan struct{}),
 	}, nil
+}
+
+// NewMiner creates a new miner using btcd backend. The logDir specifies the
+// miner node's log dir. When tests finished, during clean up, its logs are
+// copied to a file specified as logFilename.
+func NewMiner(logDir, logFilename string, netParams *chaincfg.Params,
+	handler *rpcclient.NotificationHandlers) (*rpctest.Harness,
+	func() error, error) {
+
+	args := []string{
+		"--rejectnonstd",
+		"--txindex",
+		"--nowinservice",
+		"--nobanning",
+		"--debuglevel=debug",
+		"--logdir=" + logDir,
+		"--trickleinterval=100ms",
+	}
+
+	miner, err := rpctest.New(netParams, handler, args)
+	if err != nil {
+		return nil, nil, fmt.Errorf(
+			"unable to create mining node: %v", err,
+		)
+	}
+
+	cleanUp := func() error {
+		if err := miner.TearDown(); err != nil {
+			return fmt.Errorf(
+				"failed to tear down miner, got error: %s", err,
+			)
+		}
+
+		// After shutting down the miner, we'll make a copy of the log
+		// file before deleting the temporary log dir.
+		logFile := fmt.Sprintf(
+			"%s/%s/btcd.log", logDir, netParams.Name,
+		)
+		copyPath := fmt.Sprintf("./%s", logFilename)
+		err := CopyFile(copyPath, logFile)
+		if err != nil {
+			return fmt.Errorf("unable to copy file: %v", err)
+		}
+
+		if err = os.RemoveAll(logDir); err != nil {
+			return fmt.Errorf(
+				"cannot remove dir %s: %v", logDir, err,
+			)
+		}
+		return nil
+	}
+
+	return miner, cleanUp, nil
 }
 
 // DBPath returns the filepath to the channeldb database file for this node.

decred/dcrlnd

lntest/node.go
@@ -17,6 +17,7 @@ import (
 	"strconv"
 	"sync"
 	"sync/atomic"
+	"testing"
 	"time"
 
 	pb "decred.org/dcrwallet/v3/rpc/walletrpc"
@@ -24,9 +25,11 @@ import (
 	"github.com/decred/dcrd/chaincfg/v3"
 	"github.com/decred/dcrd/dcrutil/v4"
 	"github.com/decred/dcrd/hdkeychain/v3"
+	"github.com/decred/dcrd/rpcclient/v8"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/aezeed"
 	"github.com/decred/dcrlnd/chanbackup"
+	"github.com/decred/dcrlnd/internal/testutils"
 	"github.com/decred/dcrlnd/lnrpc"
 	"github.com/decred/dcrlnd/lnrpc/invoicesrpc"
 	"github.com/decred/dcrlnd/lnrpc/routerrpc"
@@ -36,6 +39,7 @@ import (
 	"github.com/decred/dcrlnd/lnrpc/wtclientrpc"
 	"github.com/decred/dcrlnd/lntest/wait"
 	"github.com/decred/dcrlnd/macaroons"
+	rpctest "github.com/decred/dcrtest/dcrdtest"
 	"github.com/go-errors/errors"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/backoff"
@@ -404,6 +408,64 @@ func newNode(cfg NodeConfig) (*HarnessNode, error) {
 		closedChans:  make(map[wire.OutPoint]struct{}),
 		closeClients: make(map[wire.OutPoint][]chan struct{}),
 	}, nil
+}
+
+// NewMiner creates a new miner using btcd backend. The logDir specifies the
+// miner node's log dir. When tests finished, during clean up, its logs are
+// copied to a file specified as logFilename.
+func NewMiner(t *testing.T, logDir, logFilename string, netParams *chaincfg.Params,
+	handler *rpcclient.NotificationHandlers) (*rpctest.Harness,
+	func() error, error) {
+
+	args := []string{
+		"--rejectnonstd",
+		"--txindex",
+		"--nobanning",
+		"--debuglevel=debug",
+		"--logdir=" + logDir,
+		"--maxorphantx=0",
+		"--rpcmaxclients=100",
+		"--rpcmaxwebsockets=100",
+		"--rpcmaxconcurrentreqs=100",
+		"--logsize=100M",
+		"--maxsameip=200",
+	}
+
+	miner, err := testutils.NewSetupRPCTest(t, 5, netParams, handler,
+		args, false, 0)
+	if err != nil {
+		return nil, nil, fmt.Errorf(
+			"unable to create mining node: %v", err,
+		)
+	}
+
+	cleanUp := func() error {
+		if err := miner.TearDown(); err != nil {
+			return fmt.Errorf(
+				"failed to tear down miner, got error: %s", err,
+			)
+		}
+
+		// After shutting down the miner, we'll make a copy of the log
+		// file before deleting the temporary log dir.
+		logFile := fmt.Sprintf(
+			"%s/%s/dcrd.log", logDir, netParams.Name,
+		)
+		copyPath := fmt.Sprintf("./%s", logFilename)
+		err := CopyFile(copyPath, logFile)
+		if err != nil {
+			return fmt.Errorf("unable to copy file: %v", err)
+		}
+
+		if err = os.RemoveAll(logDir); err != nil {
+			return fmt.Errorf(
+				"cannot remove dir %s: %v", logDir, err,
+			)
+		}
+		return nil
+	}
+
+	return miner, cleanUp, nil
 }
 
 // DBPath returns the filepath to the channeldb database file for this node.