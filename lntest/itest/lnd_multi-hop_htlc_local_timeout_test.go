lightningnetwork/lnd

itest: increase htlc amt for local force close tests
Since the tests set a quite high fee rate before the node goes to chain,
the HTLCs wouldn't be economical to sweep at this fee rate.

Pre sweeper handling of the second-level transactions this was not a
problem, since the fees were set when the second-levels were created,
before the fee estimate was increased.

lntest/itest/lnd_multi-hop_htlc_local_timeout_test.go
@@ -43,7 +43,7 @@ func testMultiHopHtlcLocalTimeout(net *lntest.NetworkHarness, t *harnessTest,
 	// while the second will be a proper fully valued HTLC.
 	const (
 		dustHtlcAmt    = btcutil.Amount(100)
-		htlcAmt        = btcutil.Amount(30000)
+		htlcAmt        = btcutil.Amount(300_000)
 		finalCltvDelta = 40
 	)
 

lntest/itest/lnd_multi-hop_local_force_close_on_chain_htlc_timeout_test.go
@@ -36,7 +36,7 @@ func testMultiHopLocalForceCloseOnChainHtlcTimeout(net *lntest.NetworkHarness,
 	// opens up the base for out tests.
 	const (
 		finalCltvDelta = 40
-		htlcAmt        = btcutil.Amount(30000)
+		htlcAmt        = btcutil.Amount(300_000)
 	)
 	ctx, cancel := context.WithCancel(ctxb)
 	defer cancel()

decred/dcrlnd

lntest/itest/lnd_multi-hop_htlc_local_timeout_test.go
@@ -43,7 +43,7 @@ func testMultiHopHtlcLocalTimeout(net *lntest.NetworkHarness, t *harnessTest,
 	// while the second will be a proper fully valued HTLC.
 	const (
 		dustHtlcAmt    = dcrutil.Amount(100)
-		htlcAmt        = dcrutil.Amount(30000)
+		htlcAmt        = dcrutil.Amount(300_00)
 		finalCltvDelta = 40
 	)
 

lntest/itest/lnd_multi-hop_local_force_close_on_chain_htlc_timeout_test.go
@@ -36,7 +36,7 @@ func testMultiHopLocalForceCloseOnChainHtlcTimeout(net *lntest.NetworkHarness,
 	// opens up the base for out tests.
 	const (
 		finalCltvDelta = 40
-		htlcAmt        = dcrutil.Amount(30000)
+		htlcAmt        = dcrutil.Amount(300_000)
 	)
 	ctx, cancel := context.WithCancel(ctxb)
 	defer cancel()