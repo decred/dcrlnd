lightningnetwork/lnd

etcd: add support for namespaces
This commit adds support for etcd namespaces. This is useful when
using a shared etcd database where separate users have access to
separate namespaces.

channeldb/kvdb/etcd/db.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/btcsuite/btcwallet/walletdb"
 	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/clientv3/namespace"
 	"github.com/coreos/etcd/pkg/transport"
 )
 
@@ -153,6 +154,9 @@ type BackendConfig struct {
 	// name spaces.
 	Prefix string
 
+	// Namespace is the etcd namespace that we'll use for all keys.
+	Namespace string
+
 	// CollectCommitStats indicates wheter to commit commit stats.
 	CollectCommitStats bool
 }
@@ -184,10 +188,14 @@ func newEtcdBackend(config BackendConfig) (*db, error) {
 		TLS:                tlsConfig,
 		MaxCallSendMsgSize: 16384*1024 - 1,
 	})
-
 	if err != nil {
 		return nil, err
 	}
+
+	// Apply the namespace.
+	cli.KV = namespace.NewKV(cli.KV, config.Namespace)
+	cli.Watcher = namespace.NewWatcher(cli.Watcher, config.Namespace)
+	cli.Lease = namespace.NewLease(cli.Lease, config.Namespace)
 
 	backend := &db{
 		cli:     cli,

channeldb/kvdb/etcd/fixture_test.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/clientv3/namespace"
 )
 
 const (
@@ -59,6 +60,11 @@ func NewEtcdTestFixture(t *testing.T) *EtcdTestFixture {
 		t.Fatalf("unable to create etcd test fixture: %v", err)
 	}
 
+	// Apply the default namespace (since that's what we use in tests).
+	cli.KV = namespace.NewKV(cli.KV, defaultNamespace)
+	cli.Watcher = namespace.NewWatcher(cli.Watcher, defaultNamespace)
+	cli.Lease = namespace.NewLease(cli.Lease, defaultNamespace)
+
 	return &EtcdTestFixture{
 		t:      t,
 		cli:    cli,
@@ -88,7 +94,7 @@ func (f *EtcdTestFixture) Get(key string) string {
 
 	resp, err := f.cli.Get(ctx, key)
 	if err != nil {
-		f.t.Fatalf("etcd test fixture failed to put: %v", err)
+		f.t.Fatalf("etcd test fixture failed to get: %v", err)
 	}
 
 	if len(resp.Kvs) > 0 {
@@ -103,9 +109,9 @@ func (f *EtcdTestFixture) Dump() map[string]string {
 	ctx, cancel := context.WithTimeout(context.TODO(), testEtcdTimeout)
 	defer cancel()
 
-	resp, err := f.cli.Get(ctx, "", clientv3.WithPrefix())
+	resp, err := f.cli.Get(ctx, "\x00", clientv3.WithFromKey())
 	if err != nil {
-		f.t.Fatalf("etcd test fixture failed to put: %v", err)
+		f.t.Fatalf("etcd test fixture failed to get: %v", err)
 	}
 
 	result := make(map[string]string)

decred/dcrlnd

channeldb/kvdb/etcd/db.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/btcsuite/btcwallet/walletdb"
 	"github.com/matheusd/etcd/clientv3"
+	"github.com/matheusd/etcd/clientv3/namespace"
 	"github.com/matheusd/etcd/pkg/transport"
 )
 
@@ -154,6 +155,9 @@ type BackendConfig struct {
 	// name spaces.
 	Prefix string
 
+	// Namespace is the etcd namespace that we'll use for all keys.
+	Namespace string
+
 	// CollectCommitStats indicates wheter to commit commit stats.
 	CollectCommitStats bool
 }
@@ -185,10 +189,14 @@ func newEtcdBackend(config BackendConfig) (*db, error) {
 		TLS:                tlsConfig,
 		MaxCallSendMsgSize: 16384*1024 - 1,
 	})
-
 	if err != nil {
 		return nil, err
 	}
+
+	// Apply the namespace.
+	cli.KV = namespace.NewKV(cli.KV, config.Namespace)
+	cli.Watcher = namespace.NewWatcher(cli.Watcher, config.Namespace)
+	cli.Lease = namespace.NewLease(cli.Lease, config.Namespace)
 
 	backend := &db{
 		cli:     cli,

channeldb/kvdb/etcd/fixture_test.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/matheusd/etcd/clientv3"
+	"github.com/matheusd/etcd/clientv3/namespace"
 )
 
 const (
@@ -60,6 +61,11 @@ func NewEtcdTestFixture(t *testing.T) *EtcdTestFixture {
 		t.Fatalf("unable to create etcd test fixture: %v", err)
 	}
 
+	// Apply the default namespace (since that's what we use in tests).
+	cli.KV = namespace.NewKV(cli.KV, defaultNamespace)
+	cli.Watcher = namespace.NewWatcher(cli.Watcher, defaultNamespace)
+	cli.Lease = namespace.NewLease(cli.Lease, defaultNamespace)
+
 	return &EtcdTestFixture{
 		t:      t,
 		cli:    cli,
@@ -89,7 +95,7 @@ func (f *EtcdTestFixture) Get(key string) string {
 
 	resp, err := f.cli.Get(ctx, key)
 	if err != nil {
-		f.t.Fatalf("etcd test fixture failed to put: %v", err)
+		f.t.Fatalf("etcd test fixture failed to get: %v", err)
 	}
 
 	if len(resp.Kvs) > 0 {
@@ -104,9 +110,9 @@ func (f *EtcdTestFixture) Dump() map[string]string {
 	ctx, cancel := context.WithTimeout(context.TODO(), testEtcdTimeout)
 	defer cancel()
 
-	resp, err := f.cli.Get(ctx, "", clientv3.WithPrefix())
+	resp, err := f.cli.Get(ctx, "\x00", clientv3.WithFromKey())
 	if err != nil {
-		f.t.Fatalf("etcd test fixture failed to put: %v", err)
+		f.t.Fatalf("etcd test fixture failed to get: %v", err)
 	}
 
 	result := make(map[string]string)