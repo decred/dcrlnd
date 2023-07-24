gitlikeagirl/lnd

multi: add channel open parameters to channel acceptor
Add more fields to channel acceptor response so that users can have more
fine grained control over their incoming channels. With our chained
acceptor, it is possible that we get inconsistent responses from
multiple chained acceptors. We create a conjugate repsponse from all the
set fields in our various responses, but fail if we get different, non-
zero responses from our various acceptors. Separate merge functions are
used per type so that we avoid unexpected outcomes comparing interfaces
(panic on comparing types that aren't comparable), with casting used
where applicable to avoid code duplication.

chanacceptor/interface.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/lnwire"
 )
 
@@ -28,11 +29,39 @@ type ChannelAcceptRequest struct {
 }
 
 // ChannelAcceptResponse is a struct containing the response to a request to
-// open an inbound channel.
+// open an inbound channel. Note that fields added to this struct must be added
+// to the mergeResponse function to allow combining of responses from different
+// acceptors.
 type ChannelAcceptResponse struct {
 	// ChanAcceptError the error returned by the channel acceptor. If the
 	// channel was accepted, this value will be nil.
 	ChanAcceptError
+
+	// UpfrontShutdown is the address that we will set as our upfront
+	// shutdown address if the peer supports the feature bit.
+	UpfrontShutdown lnwire.DeliveryAddress
+
+	// CSVDelay is the csv delay we require for the remote peer.
+	CSVDelay uint16
+
+	// Reserve is the amount that require the remote peer hold in reserve
+	// on the channel.
+	Reserve btcutil.Amount
+
+	// InFlightTotal is the maximum amount that we allow the remote peer to
+	// hold in outstanding htlcs.
+	InFlightTotal lnwire.MilliSatoshi
+
+	// HtlcLimit is the maximum number of htlcs that we allow the remote
+	// peer to offer us.
+	HtlcLimit uint16
+
+	// MinHtlcIn is the minimum incoming htlc value allowed on the channel.
+	MinHtlcIn lnwire.MilliSatoshi
+
+	// MinAcceptDepth is the minimum depth that the initiator of the
+	// channel should wait before considering the channel open.
+	MinAcceptDepth uint16
 }
 
 // NewChannelAcceptResponse is a constructor for a channel accept response,
@@ -40,13 +69,25 @@ type ChannelAcceptResponse struct {
 // a rejection) so that the error will be whitelisted and delivered to the
 // initiating peer. Accepted channels simply return a response containing a nil
 // error.
-func NewChannelAcceptResponse(accept bool,
-	acceptErr error) *ChannelAcceptResponse {
+func NewChannelAcceptResponse(accept bool, acceptErr error,
+	upfrontShutdown lnwire.DeliveryAddress, csvDelay, htlcLimit,
+	minDepth uint16, reserve btcutil.Amount, inFlight,
+	minHtlcIn lnwire.MilliSatoshi) *ChannelAcceptResponse {
+
+	resp := &ChannelAcceptResponse{
+		UpfrontShutdown: upfrontShutdown,
+		CSVDelay:        csvDelay,
+		Reserve:         reserve,
+		InFlightTotal:   inFlight,
+		HtlcLimit:       htlcLimit,
+		MinHtlcIn:       minHtlcIn,
+		MinAcceptDepth:  minDepth,
+	}
 
 	// If we want to accept the channel, we return a response with a nil
 	// error.
 	if accept {
-		return &ChannelAcceptResponse{}
+		return resp
 	}
 
 	// Use a generic error when no custom error is provided.
@@ -54,11 +95,11 @@ func NewChannelAcceptResponse(accept bool,
 		acceptErr = errChannelRejected
 	}
 
-	return &ChannelAcceptResponse{
-		ChanAcceptError: ChanAcceptError{
-			error: acceptErr,
-		},
+	resp.ChanAcceptError = ChanAcceptError{
+		error: acceptErr,
 	}
+
+	return resp
 }
 
 // RejectChannel returns a boolean that indicates whether we should reject the

chanacceptor/merge.go
@@ -1,0 +1,153 @@
+package chanacceptor
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+const (
+	// We use field names in our errors for more readable errors. Create
+	// consts for them here so that we can exactly match in our unit tests.
+	fieldCSV             = "csv delay"
+	fieldHtlcLimit       = "htlc limit"
+	fieldMinDep          = "min depth"
+	fieldReserve         = "reserve"
+	fieldMinIn           = "min htlc in"
+	fieldInFlightTotal   = "in flight total"
+	fieldUpfrontShutdown = "upfront shutdown"
+)
+
+// mergeError returns a merge error for a named field when we get two channel
+// acceptor responses which have different values set.
+func mergeError(name string, current, new interface{}) error {
+	return fmt.Errorf("multiple values set for: %v, %v and %v",
+		name, current, new)
+}
+
+// mergeInt64 merges two int64 values, failing if they have different non-zero
+// values.
+func mergeInt64(name string, current, new int64) (int64, error) {
+	switch {
+	case current == 0:
+		return new, nil
+
+	case new == 0:
+		return current, nil
+
+	case current != new:
+		return 0, mergeError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeMillisatoshi merges two msat values, failing if they have different
+// non-zero values.
+func mergeMillisatoshi(name string, current,
+	new lnwire.MilliSatoshi) (lnwire.MilliSatoshi, error) {
+
+	switch {
+	case current == 0:
+		return new, nil
+
+	case new == 0:
+		return current, nil
+
+	case current != new:
+		return 0, mergeError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeDeliveryAddress merges two delivery address values, failing if they have
+// different non-zero values.
+func mergeDeliveryAddress(name string, current,
+	new lnwire.DeliveryAddress) (lnwire.DeliveryAddress, error) {
+
+	switch {
+	case current == nil:
+		return new, nil
+
+	case new == nil:
+		return current, nil
+
+	case !bytes.Equal(current, new):
+		return nil, mergeError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeResponse takes two channel accept responses, and attempts to merge their
+// fields, failing if any fields conflict (are non-zero and not equal). It
+// returns a new response that has all the merged fields in it.
+func mergeResponse(current, new ChannelAcceptResponse) (ChannelAcceptResponse,
+	error) {
+
+	csv, err := mergeInt64(
+		fieldCSV, int64(current.CSVDelay), int64(new.CSVDelay),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.CSVDelay = uint16(csv)
+
+	htlcLimit, err := mergeInt64(
+		fieldHtlcLimit, int64(current.HtlcLimit),
+		int64(new.HtlcLimit),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.HtlcLimit = uint16(htlcLimit)
+
+	minDepth, err := mergeInt64(
+		fieldMinDep, int64(current.MinAcceptDepth),
+		int64(new.MinAcceptDepth),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.MinAcceptDepth = uint16(minDepth)
+
+	reserve, err := mergeInt64(
+		fieldReserve, int64(current.Reserve), int64(new.Reserve),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.Reserve = btcutil.Amount(reserve)
+
+	// Merge msat values and set directly.
+	current.MinHtlcIn, err = mergeMillisatoshi(
+		fieldMinIn, current.MinHtlcIn, new.MinHtlcIn,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	current.InFlightTotal, err = mergeMillisatoshi(
+		fieldInFlightTotal, current.InFlightTotal,
+		new.InFlightTotal,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	current.UpfrontShutdown, err = mergeDeliveryAddress(
+		fieldUpfrontShutdown, current.UpfrontShutdown,
+		new.UpfrontShutdown,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	return current, nil
+}

chanacceptor/merge_test.go
@@ -1,0 +1,188 @@
+package chanacceptor
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/stretchr/testify/require"
+)
+
+// TestMergeResponse tests merging of channel acceptor responses.
+func TestMergeResponse(t *testing.T) {
+	var (
+		addr1 = lnwire.DeliveryAddress{1}
+		addr2 = lnwire.DeliveryAddress{2}
+
+		populatedResp = ChannelAcceptResponse{
+			UpfrontShutdown: addr1,
+			CSVDelay:        2,
+			Reserve:         3,
+			InFlightTotal:   4,
+			HtlcLimit:       5,
+			MinHtlcIn:       6,
+			MinAcceptDepth:  7,
+		}
+	)
+
+	tests := []struct {
+		name    string
+		current ChannelAcceptResponse
+		new     ChannelAcceptResponse
+		merged  ChannelAcceptResponse
+		err     error
+	}{
+		{
+			name:    "same response",
+			current: populatedResp,
+			new:     populatedResp,
+			merged:  populatedResp,
+			err:     nil,
+		},
+		{
+			name: "different upfront",
+			current: ChannelAcceptResponse{
+				UpfrontShutdown: addr1,
+			},
+			new: ChannelAcceptResponse{
+				UpfrontShutdown: addr2,
+			},
+			err: mergeError(fieldUpfrontShutdown, addr1, addr2),
+		},
+		{
+			name: "different csv",
+			current: ChannelAcceptResponse{
+				CSVDelay: 1,
+			},
+			new: ChannelAcceptResponse{
+				CSVDelay: 2,
+			},
+			err: mergeError(fieldCSV, 1, 2),
+		},
+		{
+			name: "different reserve",
+			current: ChannelAcceptResponse{
+				Reserve: 1,
+			},
+			new: ChannelAcceptResponse{
+				Reserve: 2,
+			},
+			err: mergeError(fieldReserve, 1, 2),
+		},
+		{
+			name: "different in flight",
+			current: ChannelAcceptResponse{
+				InFlightTotal: 1,
+			},
+			new: ChannelAcceptResponse{
+				InFlightTotal: 2,
+			},
+			err: mergeError(
+				fieldInFlightTotal, lnwire.MilliSatoshi(1),
+				lnwire.MilliSatoshi(2),
+			),
+		},
+		{
+			name: "different htlc limit",
+			current: ChannelAcceptResponse{
+				HtlcLimit: 1,
+			},
+			new: ChannelAcceptResponse{
+				HtlcLimit: 2,
+			},
+			err: mergeError(fieldHtlcLimit, 1, 2),
+		},
+		{
+			name: "different min in",
+			current: ChannelAcceptResponse{
+				MinHtlcIn: 1,
+			},
+			new: ChannelAcceptResponse{
+				MinHtlcIn: 2,
+			},
+			err: mergeError(
+				fieldMinIn, lnwire.MilliSatoshi(1),
+				lnwire.MilliSatoshi(2),
+			),
+		},
+		{
+			name: "different depth",
+			current: ChannelAcceptResponse{
+				MinAcceptDepth: 1,
+			},
+			new: ChannelAcceptResponse{
+				MinAcceptDepth: 2,
+			},
+			err: mergeError(fieldMinDep, 1, 2),
+		},
+		{
+			name: "merge all values",
+			current: ChannelAcceptResponse{
+				UpfrontShutdown: lnwire.DeliveryAddress{1},
+				CSVDelay:        1,
+				Reserve:         0,
+				InFlightTotal:   3,
+				HtlcLimit:       0,
+				MinHtlcIn:       5,
+				MinAcceptDepth:  0,
+			},
+			new: ChannelAcceptResponse{
+				UpfrontShutdown: nil,
+				CSVDelay:        0,
+				Reserve:         2,
+				InFlightTotal:   0,
+				HtlcLimit:       4,
+				MinHtlcIn:       0,
+				MinAcceptDepth:  6,
+			},
+			merged: ChannelAcceptResponse{
+				UpfrontShutdown: lnwire.DeliveryAddress{1},
+				CSVDelay:        1,
+				Reserve:         2,
+				InFlightTotal:   3,
+				HtlcLimit:       4,
+				MinHtlcIn:       5,
+				MinAcceptDepth:  6,
+			},
+			err: nil,
+		},
+		{
+			// Test the case where fields have the same non-zero
+			// value, and the case where only response value is
+			// non-zero.
+			name: "empty and identical",
+			current: ChannelAcceptResponse{
+				CSVDelay:      1,
+				Reserve:       2,
+				InFlightTotal: 0,
+			},
+			new: ChannelAcceptResponse{
+				CSVDelay:      0,
+				Reserve:       2,
+				InFlightTotal: 3,
+			},
+			merged: ChannelAcceptResponse{
+				CSVDelay:      1,
+				Reserve:       2,
+				InFlightTotal: 3,
+			},
+			err: nil,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.name, func(t *testing.T) {
+			resp, err := mergeResponse(test.current, test.new)
+			require.Equal(t, test.err, err)
+
+			// If we expect an error, exit early rather than compare
+			// our result.
+			if test.err != nil {
+				return
+			}
+
+			require.Equal(t, test.merged, resp)
+		})
+	}
+}

chanacceptor/rpcacceptor_test.go
@@ -5,20 +5,34 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/lightningnetwork/lnd/lnwallet/chancloser"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/stretchr/testify/require"
 )
 
 // TestValidateAcceptorResponse test validation of acceptor responses.
 func TestValidateAcceptorResponse(t *testing.T) {
-	customError := errors.New("custom error")
+	var (
+		customError = errors.New("custom error")
+		validAddr   = "bcrt1qwrmq9uca0t3dy9t9wtuq5tm4405r7tfzyqn9pp"
+		addr, _     = chancloser.ParseUpfrontShutdownAddress(
+			validAddr, &chaincfg.TestNet3Params,
+		)
+	)
 
 	tests := []struct {
 		name        string
+		dustLimit   btcutil.Amount
 		response    lnrpc.ChannelAcceptResponse
 		accept      bool
 		acceptorErr error
 		error       error
+		shutdown    lnwire.DeliveryAddress
 	}{
 		{
 			name: "accepted with error",
@@ -43,11 +57,13 @@ func TestValidateAcceptorResponse(t *testing.T) {
 		{
 			name: "accepted",
 			response: lnrpc.ChannelAcceptResponse{
-				Accept: true,
+				Accept:          true,
+				UpfrontShutdown: validAddr,
 			},
 			accept:      true,
 			acceptorErr: nil,
 			error:       nil,
+			shutdown:    addr,
 		},
 		{
 			name: "rejected with error",
@@ -68,18 +84,46 @@ func TestValidateAcceptorResponse(t *testing.T) {
 			acceptorErr: errChannelRejected,
 			error:       nil,
 		},
+		{
+			name: "invalid upfront shutdown",
+			response: lnrpc.ChannelAcceptResponse{
+				Accept:          true,
+				UpfrontShutdown: "invalid addr",
+			},
+			accept:      false,
+			acceptorErr: errChannelRejected,
+			error:       errInvalidUpfrontShutdown,
+		},
+		{
+			name:      "reserve too low",
+			dustLimit: 100,
+			response: lnrpc.ChannelAcceptResponse{
+				Accept:     true,
+				ReserveSat: 10,
+			},
+			accept:      false,
+			acceptorErr: errChannelRejected,
+			error:       errInsufficientReserve,
+		},
 	}
 
 	for _, test := range tests {
 		test := test
 
 		t.Run(test.name, func(t *testing.T) {
-			accept, acceptErr, err := validateAcceptorResponse(
-				test.response,
+			// Create an acceptor, everything can be nil because
+			// we just need the params.
+			acceptor := NewRPCAcceptor(
+				nil, nil, 0, &chaincfg.TestNet3Params, nil,
+			)
+
+			accept, acceptErr, shutdown, err := acceptor.validateAcceptorResponse(
+				test.dustLimit, test.response,
 			)
 			require.Equal(t, test.accept, accept)
 			require.Equal(t, test.acceptorErr, acceptErr)
 			require.Equal(t, test.error, err)
+			require.Equal(t, test.shutdown, shutdown)
 		})
 	}
 }

decred/dcrlnd

chanacceptor/interface.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/decred/dcrd/dcrutil/v4"
 	"github.com/decred/dcrlnd/lnwire"
 )
 
@@ -28,11 +29,39 @@ type ChannelAcceptRequest struct {
 }
 
 // ChannelAcceptResponse is a struct containing the response to a request to
-// open an inbound channel.
+// open an inbound channel. Note that fields added to this struct must be added
+// to the mergeResponse function to allow combining of responses from different
+// acceptors.
 type ChannelAcceptResponse struct {
 	// ChanAcceptError the error returned by the channel acceptor. If the
 	// channel was accepted, this value will be nil.
 	ChanAcceptError
+
+	// UpfrontShutdown is the address that we will set as our upfront
+	// shutdown address.
+	UpfrontShutdown lnwire.DeliveryAddress
+
+	// CSVDelay is the csv delay we require for the remote peer.
+	CSVDelay uint16
+
+	// Reserve is the amount that require the remote peer hold in reserve
+	// on the channel.
+	Reserve dcrutil.Amount
+
+	// InFlightTotal is the maximum amount that we allow the remote peer to
+	// hold in outstanding htlcs.
+	InFlightTotal lnwire.MilliAtom
+
+	// HtlcLimit is the maximum number of htlcs that we allow the remote
+	// peer to offer us.
+	HtlcLimit uint16
+
+	// MinHtlcIn is the minimum incoming htlc value allowed on the channel.
+	MinHtlcIn lnwire.MilliAtom
+
+	// MinAcceptDepth is the minimum depth that the initiator of the
+	// channel should wait before considering the channel open.
+	MinAcceptDepth uint16
 }
 
 // NewChannelAcceptResponse is a constructor for a channel accept response,
@@ -40,13 +69,25 @@ type ChannelAcceptResponse struct {
 // a rejection) so that the error will be whitelisted and delivered to the
 // initiating peer. Accepted channels simply return a response containing a nil
 // error.
-func NewChannelAcceptResponse(accept bool,
-	acceptErr error) *ChannelAcceptResponse {
+func NewChannelAcceptResponse(accept bool, acceptErr error,
+	upfrontShutdown lnwire.DeliveryAddress, csvDelay, htlcLimit,
+	minDepth uint16, reserve dcrutil.Amount, inFlight,
+	minHtlcIn lnwire.MilliAtom) *ChannelAcceptResponse {
+
+	resp := &ChannelAcceptResponse{
+		UpfrontShutdown: upfrontShutdown,
+		CSVDelay:        csvDelay,
+		Reserve:         reserve,
+		InFlightTotal:   inFlight,
+		HtlcLimit:       htlcLimit,
+		MinHtlcIn:       minHtlcIn,
+		MinAcceptDepth:  minDepth,
+	}
 
 	// If we want to accept the channel, we return a response with a nil
 	// error.
 	if accept {
-		return &ChannelAcceptResponse{}
+		return resp
 	}
 
 	// Use a generic error when no custom error is provided.
@@ -54,11 +95,11 @@ func NewChannelAcceptResponse(accept bool,
 		acceptErr = errChannelRejected
 	}
 
-	return &ChannelAcceptResponse{
-		ChanAcceptError: ChanAcceptError{
-			error: acceptErr,
-		},
+	resp.ChanAcceptError = ChanAcceptError{
+		error: acceptErr,
 	}
+
+	return resp
 }
 
 // RejectChannel returns a boolean that indicates whether we should reject the

chanacceptor/merge.go
@@ -1,0 +1,152 @@
+package chanacceptor
+
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/decred/dcrd/dcrutil/v4"
+	"github.com/decred/dcrlnd/lnwire"
+)
+
+const (
+	// We use field names in our errors for more readable errors. Create
+	// consts for them here so that we can exactly match in our unit tests.
+	fieldCSV             = "csv delay"
+	fieldHtlcLimit       = "htlc limit"
+	fieldMinDep          = "min depth"
+	fieldReserve         = "reserve"
+	fieldMinIn           = "min htlc in"
+	fieldInFlightTotal   = "in flight total"
+	fieldUpfrontShutdown = "upfront shutdown"
+)
+
+// fieldMismatchError returns a merge error for a named field when we get two
+// channel acceptor responses which have different values set.
+func fieldMismatchError(name string, current, new interface{}) error {
+	return fmt.Errorf("multiple values set for: %v, %v and %v",
+		name, current, new)
+}
+
+// mergeInt64 merges two int64 values, failing if they have different non-zero
+// values.
+func mergeInt64(name string, current, new int64) (int64, error) {
+	switch {
+	case current == 0:
+		return new, nil
+
+	case new == 0:
+		return current, nil
+
+	case current != new:
+		return 0, fieldMismatchError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeMilliatoms merges two msat values, failing if they have different
+// non-zero values.
+func mergeMilliatoms(name string, current,
+	new lnwire.MilliAtom) (lnwire.MilliAtom, error) {
+
+	switch {
+	case current == 0:
+		return new, nil
+
+	case new == 0:
+		return current, nil
+
+	case current != new:
+		return 0, fieldMismatchError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeDeliveryAddress merges two delivery address values, failing if they have
+// different non-zero values.
+func mergeDeliveryAddress(name string, current,
+	new lnwire.DeliveryAddress) (lnwire.DeliveryAddress, error) {
+
+	switch {
+	case current == nil:
+		return new, nil
+
+	case new == nil:
+		return current, nil
+
+	case !bytes.Equal(current, new):
+		return nil, fieldMismatchError(name, current, new)
+
+	default:
+		return new, nil
+	}
+}
+
+// mergeResponse takes two channel accept responses, and attempts to merge their
+// fields, failing if any fields conflict (are non-zero and not equal). It
+// returns a new response that has all the merged fields in it.
+func mergeResponse(current, new ChannelAcceptResponse) (ChannelAcceptResponse,
+	error) {
+
+	csv, err := mergeInt64(
+		fieldCSV, int64(current.CSVDelay), int64(new.CSVDelay),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.CSVDelay = uint16(csv)
+
+	htlcLimit, err := mergeInt64(
+		fieldHtlcLimit, int64(current.HtlcLimit),
+		int64(new.HtlcLimit),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.HtlcLimit = uint16(htlcLimit)
+
+	minDepth, err := mergeInt64(
+		fieldMinDep, int64(current.MinAcceptDepth),
+		int64(new.MinAcceptDepth),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.MinAcceptDepth = uint16(minDepth)
+
+	reserve, err := mergeInt64(
+		fieldReserve, int64(current.Reserve), int64(new.Reserve),
+	)
+	if err != nil {
+		return current, err
+	}
+	current.Reserve = dcrutil.Amount(reserve)
+
+	current.MinHtlcIn, err = mergeMilliatoms(
+		fieldMinIn, current.MinHtlcIn, new.MinHtlcIn,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	current.InFlightTotal, err = mergeMilliatoms(
+		fieldInFlightTotal, current.InFlightTotal,
+		new.InFlightTotal,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	current.UpfrontShutdown, err = mergeDeliveryAddress(
+		fieldUpfrontShutdown, current.UpfrontShutdown,
+		new.UpfrontShutdown,
+	)
+	if err != nil {
+		return current, err
+	}
+
+	return current, nil
+}

chanacceptor/merge_test.go
@@ -1,0 +1,188 @@
+package chanacceptor
+
+import (
+	"testing"
+
+	"github.com/decred/dcrlnd/lnwire"
+	"github.com/stretchr/testify/require"
+)
+
+// TestMergeResponse tests merging of channel acceptor responses.
+func TestMergeResponse(t *testing.T) {
+	var (
+		addr1 = lnwire.DeliveryAddress{1}
+		addr2 = lnwire.DeliveryAddress{2}
+
+		populatedResp = ChannelAcceptResponse{
+			UpfrontShutdown: addr1,
+			CSVDelay:        2,
+			Reserve:         3,
+			InFlightTotal:   4,
+			HtlcLimit:       5,
+			MinHtlcIn:       6,
+			MinAcceptDepth:  7,
+		}
+	)
+
+	tests := []struct {
+		name    string
+		current ChannelAcceptResponse
+		new     ChannelAcceptResponse
+		merged  ChannelAcceptResponse
+		err     error
+	}{
+		{
+			name:    "same response",
+			current: populatedResp,
+			new:     populatedResp,
+			merged:  populatedResp,
+			err:     nil,
+		},
+		{
+			name: "different upfront",
+			current: ChannelAcceptResponse{
+				UpfrontShutdown: addr1,
+			},
+			new: ChannelAcceptResponse{
+				UpfrontShutdown: addr2,
+			},
+			err: fieldMismatchError(fieldUpfrontShutdown, addr1, addr2),
+		},
+		{
+			name: "different csv",
+			current: ChannelAcceptResponse{
+				CSVDelay: 1,
+			},
+			new: ChannelAcceptResponse{
+				CSVDelay: 2,
+			},
+			err: fieldMismatchError(fieldCSV, 1, 2),
+		},
+		{
+			name: "different reserve",
+			current: ChannelAcceptResponse{
+				Reserve: 1,
+			},
+			new: ChannelAcceptResponse{
+				Reserve: 2,
+			},
+			err: fieldMismatchError(fieldReserve, 1, 2),
+		},
+		{
+			name: "different in flight",
+			current: ChannelAcceptResponse{
+				InFlightTotal: 1,
+			},
+			new: ChannelAcceptResponse{
+				InFlightTotal: 2,
+			},
+			err: fieldMismatchError(
+				fieldInFlightTotal, lnwire.MilliAtom(1),
+				lnwire.MilliAtom(2),
+			),
+		},
+		{
+			name: "different htlc limit",
+			current: ChannelAcceptResponse{
+				HtlcLimit: 1,
+			},
+			new: ChannelAcceptResponse{
+				HtlcLimit: 2,
+			},
+			err: fieldMismatchError(fieldHtlcLimit, 1, 2),
+		},
+		{
+			name: "different min in",
+			current: ChannelAcceptResponse{
+				MinHtlcIn: 1,
+			},
+			new: ChannelAcceptResponse{
+				MinHtlcIn: 2,
+			},
+			err: fieldMismatchError(
+				fieldMinIn, lnwire.MilliAtom(1),
+				lnwire.MilliAtom(2),
+			),
+		},
+		{
+			name: "different depth",
+			current: ChannelAcceptResponse{
+				MinAcceptDepth: 1,
+			},
+			new: ChannelAcceptResponse{
+				MinAcceptDepth: 2,
+			},
+			err: fieldMismatchError(fieldMinDep, 1, 2),
+		},
+		{
+			name: "merge all values",
+			current: ChannelAcceptResponse{
+				UpfrontShutdown: lnwire.DeliveryAddress{1},
+				CSVDelay:        1,
+				Reserve:         0,
+				InFlightTotal:   3,
+				HtlcLimit:       0,
+				MinHtlcIn:       5,
+				MinAcceptDepth:  0,
+			},
+			new: ChannelAcceptResponse{
+				UpfrontShutdown: nil,
+				CSVDelay:        0,
+				Reserve:         2,
+				InFlightTotal:   0,
+				HtlcLimit:       4,
+				MinHtlcIn:       0,
+				MinAcceptDepth:  6,
+			},
+			merged: ChannelAcceptResponse{
+				UpfrontShutdown: lnwire.DeliveryAddress{1},
+				CSVDelay:        1,
+				Reserve:         2,
+				InFlightTotal:   3,
+				HtlcLimit:       4,
+				MinHtlcIn:       5,
+				MinAcceptDepth:  6,
+			},
+			err: nil,
+		},
+		{
+			// Test the case where fields have the same non-zero
+			// value, and the case where only response value is
+			// non-zero.
+			name: "empty and identical",
+			current: ChannelAcceptResponse{
+				CSVDelay:      1,
+				Reserve:       2,
+				InFlightTotal: 0,
+			},
+			new: ChannelAcceptResponse{
+				CSVDelay:      0,
+				Reserve:       2,
+				InFlightTotal: 3,
+			},
+			merged: ChannelAcceptResponse{
+				CSVDelay:      1,
+				Reserve:       2,
+				InFlightTotal: 3,
+			},
+			err: nil,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+
+		t.Run(test.name, func(t *testing.T) {
+			resp, err := mergeResponse(test.current, test.new)
+			require.Equal(t, test.err, err)
+
+			// If we expect an error, exit early rather than compare
+			// our result.
+			if test.err != nil {
+				return
+			}
+
+			require.Equal(t, test.merged, resp)
+		})
+	}
+}

chanacceptor/rpcacceptor_test.go
@@ -5,20 +5,33 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/decred/dcrd/chaincfg/v3"
+	"github.com/decred/dcrd/dcrutil/v4"
+	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnrpc"
+	"github.com/decred/dcrlnd/lnwallet/chancloser"
+	"github.com/decred/dcrlnd/lnwire"
 	"github.com/stretchr/testify/require"
 )
 
 // TestValidateAcceptorResponse test validation of acceptor responses.
 func TestValidateAcceptorResponse(t *testing.T) {
-	customError := errors.New("custom error")
+	var (
+		customError = errors.New("custom error")
+		validAddr   = "TsZwLVNTYxwcT5tgcqMzGUujjPydy5nGMk2"
+		addr, _     = chancloser.ParseUpfrontShutdownAddress(
+			validAddr, chaincfg.TestNet3Params(),
+		)
+	)
 
 	tests := []struct {
 		name        string
+		dustLimit   dcrutil.Amount
 		response    lnrpc.ChannelAcceptResponse
 		accept      bool
 		acceptorErr error
 		error       error
+		shutdown    lnwire.DeliveryAddress
 	}{
 		{
 			name: "accepted with error",
@@ -43,11 +56,13 @@ func TestValidateAcceptorResponse(t *testing.T) {
 		{
 			name: "accepted",
 			response: lnrpc.ChannelAcceptResponse{
-				Accept: true,
+				Accept:          true,
+				UpfrontShutdown: validAddr,
 			},
 			accept:      true,
 			acceptorErr: nil,
 			error:       nil,
+			shutdown:    addr,
 		},
 		{
 			name: "rejected with error",
@@ -68,18 +83,57 @@ func TestValidateAcceptorResponse(t *testing.T) {
 			acceptorErr: errChannelRejected,
 			error:       nil,
 		},
+		{
+			name: "invalid upfront shutdown",
+			response: lnrpc.ChannelAcceptResponse{
+				Accept:          true,
+				UpfrontShutdown: "invalid addr",
+			},
+			accept:      false,
+			acceptorErr: errChannelRejected,
+			error:       errInvalidUpfrontShutdown,
+		},
+		{
+			name:      "reserve too low",
+			dustLimit: 100,
+			response: lnrpc.ChannelAcceptResponse{
+				Accept:       true,
+				ReserveAtoms: 10,
+			},
+			accept:      false,
+			acceptorErr: errChannelRejected,
+			error:       errInsufficientReserve,
+		},
+		{
+			name:      "max htlcs too high",
+			dustLimit: 100,
+			response: lnrpc.ChannelAcceptResponse{
+				Accept:       true,
+				MaxHtlcCount: 1 + input.MaxHTLCNumber/2,
+			},
+			accept:      false,
+			acceptorErr: errChannelRejected,
+			error:       errMaxHtlcTooHigh,
+		},
 	}
 
 	for _, test := range tests {
 		test := test
 
 		t.Run(test.name, func(t *testing.T) {
-			accept, acceptErr, err := validateAcceptorResponse(
-				test.response,
+			// Create an acceptor, everything can be nil because
+			// we just need the params.
+			acceptor := NewRPCAcceptor(
+				nil, nil, 0, chaincfg.TestNet3Params(), nil,
+			)
+
+			accept, acceptErr, shutdown, err := acceptor.validateAcceptorResponse(
+				test.dustLimit, test.response,
 			)
 			require.Equal(t, test.accept, accept)
 			require.Equal(t, test.acceptorErr, acceptErr)
 			require.Equal(t, test.error, err)
+			require.Equal(t, test.shutdown, shutdown)
 		})
 	}
 }