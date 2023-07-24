lightningnetwork/lnd

routing+lnrpc: extend BuildRoute to accept raw payAddr
In this commit, we extend the `BuildRoute` method and RPC on the router
sub-server to accept a raw payment address which will be included as
part of an MPP payload for the finla hop. This change actually also
allows users to craft their own MPP paths using BuildRoute+SendToRoute.
Our primary goal however, was to fix some broken itests since we now
require the payAddr to be present for ALL payments other than key send
payments.

lntest/itest/lnd_forward_interceptor_test.go
@@ -27,6 +27,7 @@ var (
 
 type interceptorTestCase struct {
 	amountMsat        int64
+	payAddr           []byte
 	invoice           *lnrpc.Invoice
 	shouldHold        bool
 	interceptorAction routerrpc.ResolveHoldForwardAction
@@ -105,7 +106,9 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 		defer wg.Done()
 		for _, tc := range testCases {
 			attempt, err := testContext.sendAliceToCarolPayment(
-				context.Background(), tc.invoice.ValueMsat, tc.invoice.RHash)
+				context.Background(), tc.invoice.ValueMsat,
+				tc.invoice.RHash, tc.payAddr,
+			)
 
 			if t.t.Failed() {
 				return
@@ -118,7 +121,8 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 			// For 'fail' interceptor action we make sure the payment failed.
 			case routerrpc.ResolveHoldForwardAction_FAIL:
 				if attempt.Status != lnrpc.HTLCAttempt_FAILED {
-					t.t.Errorf("expected payment to fail, instead got %v", attempt.Status)
+					t.t.Errorf("expected payment to fail, "+
+						"instead got %v", attempt.Status)
 				}
 
 			// For settle and resume we make sure the payment is successful.
@@ -127,7 +131,8 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 
 			case routerrpc.ResolveHoldForwardAction_RESUME:
 				if attempt.Status != lnrpc.HTLCAttempt_SUCCEEDED {
-					t.t.Errorf("expected payment to succeed, instead got %v", attempt.Status)
+					t.t.Errorf("expected payment to "+
+						"succeed, instead got %v", attempt.Status)
 				}
 			}
 		}
@@ -202,7 +207,9 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 				foundPayment.Status != lnrpc.Payment_IN_FLIGHT {
 
 				t.Fatalf("expected to find in flight payment for"+
-					"amount %v, %v", testCase.invoice.ValueMsat, foundPayment.Status)
+					"amount %v, %v",
+					testCase.invoice.ValueMsat,
+					foundPayment.Status)
 			}
 		}
 	}
@@ -294,12 +301,25 @@ func (c *interceptorTestContext) prepareTestCases() (
 		if err != nil {
 			return nil, fmt.Errorf("unable to add invoice: %v", err)
 		}
+
+		// We'll need to also decode the returned invoice so we can
+		// grab the payment address which is now required for ALL
+		// payments.
+		payReq, err := c.carol.DecodePayReq(context.Background(), &lnrpc.PayReqString{
+			PayReq: invoice.PaymentRequest,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("unable to decode invoice: %v", err)
+		}
 		t.invoice = invoice
+		t.payAddr = payReq.PaymentAddr
 	}
 	return cases, nil
 }
 
-func (c *interceptorTestContext) openChannel(from, to *lntest.HarnessNode, chanSize btcutil.Amount) {
+func (c *interceptorTestContext) openChannel(from, to *lntest.HarnessNode,
+	chanSize btcutil.Amount) {
+
 	ctxb := context.Background()
 
 	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
@@ -365,10 +385,14 @@ func (c *interceptorTestContext) waitForChannels() {
 // sendAliceToCarolPayment sends a payment from alice to carol and make an
 // attempt to pay. The lnrpc.HTLCAttempt is returned.
 func (c *interceptorTestContext) sendAliceToCarolPayment(ctx context.Context,
-	amtMsat int64, paymentHash []byte) (*lnrpc.HTLCAttempt, error) {
+	amtMsat int64,
+	paymentHash, paymentAddr []byte) (*lnrpc.HTLCAttempt, error) {
 
 	// Build a route from alice to carol.
-	route, err := c.buildRoute(ctx, amtMsat, []*lntest.HarnessNode{c.bob, c.carol})
+	route, err := c.buildRoute(
+		ctx, amtMsat, []*lntest.HarnessNode{c.bob, c.carol},
+		paymentAddr,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -387,8 +411,8 @@ func (c *interceptorTestContext) sendAliceToCarolPayment(ctx context.Context,
 }
 
 // buildRoute is a helper function to build a route with given hops.
-func (c *interceptorTestContext) buildRoute(ctx context.Context, amtMsat int64, hops []*lntest.HarnessNode) (
-	*lnrpc.Route, error) {
+func (c *interceptorTestContext) buildRoute(ctx context.Context, amtMsat int64,
+	hops []*lntest.HarnessNode, payAddr []byte) (*lnrpc.Route, error) {
 
 	rpcHops := make([][]byte, 0, len(hops))
 	for _, hop := range hops {
@@ -405,6 +429,7 @@ func (c *interceptorTestContext) buildRoute(ctx context.Context, amtMsat int64, 
 		AmtMsat:        amtMsat,
 		FinalCltvDelta: chainreg.DefaultBitcoinTimeLockDelta,
 		HopPubkeys:     rpcHops,
+		PaymentAddr:    payAddr,
 	}
 
 	routeResp, err := c.alice.RouterClient.BuildRoute(ctx, req)

routing/router_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
 
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/clock"
@@ -2921,6 +2922,10 @@ func TestSendToRouteMaxHops(t *testing.T) {
 func TestBuildRoute(t *testing.T) {
 	// Setup a three node network.
 	chanCapSat := btcutil.Amount(100000)
+	paymentAddrFeatures := lnwire.NewFeatureVector(
+		lnwire.NewRawFeatureVector(lnwire.PaymentAddrOptional),
+		lnwire.Features,
+	)
 	testChannels := []*testChannel{
 		// Create two local channels from a. The bandwidth is estimated
 		// in this test as the channel capacity. For building routes, we
@@ -2945,29 +2950,33 @@ func TestBuildRoute(t *testing.T) {
 		// nodes are recommended to keep their channel policies towards
 		// the same peer identical.
 		symmetricTestChannel("b", "c", chanCapSat, &testChannelPolicy{
-			Expiry:  144,
-			FeeRate: 50000,
-			MinHTLC: lnwire.NewMSatFromSatoshis(20),
-			MaxHTLC: lnwire.NewMSatFromSatoshis(120),
+			Expiry:   144,
+			FeeRate:  50000,
+			MinHTLC:  lnwire.NewMSatFromSatoshis(20),
+			MaxHTLC:  lnwire.NewMSatFromSatoshis(120),
+			Features: paymentAddrFeatures,
 		}, 2),
 		symmetricTestChannel("b", "c", chanCapSat, &testChannelPolicy{
-			Expiry:  144,
-			FeeRate: 60000,
-			MinHTLC: lnwire.NewMSatFromSatoshis(20),
-			MaxHTLC: lnwire.NewMSatFromSatoshis(120),
+			Expiry:   144,
+			FeeRate:  60000,
+			MinHTLC:  lnwire.NewMSatFromSatoshis(20),
+			MaxHTLC:  lnwire.NewMSatFromSatoshis(120),
+			Features: paymentAddrFeatures,
 		}, 7),
 
 		symmetricTestChannel("a", "e", chanCapSat, &testChannelPolicy{
-			Expiry:  144,
-			FeeRate: 80000,
-			MinHTLC: lnwire.NewMSatFromSatoshis(5),
-			MaxHTLC: lnwire.NewMSatFromSatoshis(10),
+			Expiry:   144,
+			FeeRate:  80000,
+			MinHTLC:  lnwire.NewMSatFromSatoshis(5),
+			MaxHTLC:  lnwire.NewMSatFromSatoshis(10),
+			Features: paymentAddrFeatures,
 		}, 5),
 		symmetricTestChannel("e", "c", chanCapSat, &testChannelPolicy{
-			Expiry:  144,
-			FeeRate: 100000,
-			MinHTLC: lnwire.NewMSatFromSatoshis(20),
-			MaxHTLC: lnwire.NewMSatFromSatoshis(chanCapSat),
+			Expiry:   144,
+			FeeRate:  100000,
+			MinHTLC:  lnwire.NewMSatFromSatoshis(20),
+			MaxHTLC:  lnwire.NewMSatFromSatoshis(chanCapSat),
+			Features: paymentAddrFeatures,
 		}, 4),
 	}
 
@@ -2987,7 +2996,9 @@ func TestBuildRoute(t *testing.T) {
 	}
 	defer cleanUp()
 
-	checkHops := func(rt *route.Route, expected []uint64) {
+	checkHops := func(rt *route.Route, expected []uint64,
+		payAddr [32]byte) {
+
 		t.Helper()
 
 		if len(rt.Hops) != len(expected) {
@@ -3000,7 +3011,15 @@ func TestBuildRoute(t *testing.T) {
 					expected[i], i, hop.ChannelID)
 			}
 		}
+
+		lastHop := rt.Hops[len(rt.Hops)-1]
+		require.NotNil(t, lastHop.MPP)
+		require.Equal(t, lastHop.MPP.PaymentAddr(), payAddr)
 	}
+
+	var payAddr [32]byte
+	_, err = rand.Read(payAddr[:])
+	require.NoError(t, err)
 
 	// Create hop list from the route node pubkeys.
 	hops := []route.Vertex{
@@ -3010,7 +3029,7 @@ func TestBuildRoute(t *testing.T) {
 
 	// Build the route for the given amount.
 	rt, err := ctx.router.BuildRoute(
-		&amt, hops, nil, 40,
+		&amt, hops, nil, 40, &payAddr,
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -3019,14 +3038,14 @@ func TestBuildRoute(t *testing.T) {
 	// Check that we get the expected route back. The total amount should be
 	// the amount to deliver to hop c (100 sats) plus the max fee for the
 	// connection b->c (6 sats).
-	checkHops(rt, []uint64{1, 7})
+	checkHops(rt, []uint64{1, 7}, payAddr)
 	if rt.TotalAmount != 106000 {
 		t.Fatalf("unexpected total amount %v", rt.TotalAmount)
 	}
 
 	// Build the route for the minimum amount.
 	rt, err = ctx.router.BuildRoute(
-		nil, hops, nil, 40,
+		nil, hops, nil, 40, &payAddr,
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -3036,7 +3055,7 @@ func TestBuildRoute(t *testing.T) {
 	// send from b to c is 20 sats. Hop b charges 1200 msat for the
 	// forwarding. The channel between hop a and b can carry amounts in the
 	// range [5, 100], so 21200 msats is the minimum amount for this route.
-	checkHops(rt, []uint64{1, 7})
+	checkHops(rt, []uint64{1, 7}, payAddr)
 	if rt.TotalAmount != 21200 {
 		t.Fatalf("unexpected total amount %v", rt.TotalAmount)
 	}
@@ -3047,7 +3066,7 @@ func TestBuildRoute(t *testing.T) {
 		ctx.aliases["e"], ctx.aliases["c"],
 	}
 	_, err = ctx.router.BuildRoute(
-		nil, hops, nil, 40,
+		nil, hops, nil, 40, nil,
 	)
 	errNoChannel, ok := err.(ErrNoChannel)
 	if !ok {

decred/dcrlnd

lntest/itest/lnd_forward_interceptor_test.go
@@ -27,6 +27,7 @@ var (
 
 type interceptorTestCase struct {
 	amountMsat        int64
+	payAddr           []byte
 	invoice           *lnrpc.Invoice
 	shouldHold        bool
 	interceptorAction routerrpc.ResolveHoldForwardAction
@@ -105,7 +106,9 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 		defer wg.Done()
 		for _, tc := range testCases {
 			attempt, err := testContext.sendAliceToCarolPayment(
-				context.Background(), tc.invoice.ValueMAtoms, tc.invoice.RHash)
+				context.Background(), tc.invoice.ValueMAtoms,
+				tc.invoice.RHash, tc.payAddr,
+			)
 
 			if t.t.Failed() {
 				return
@@ -118,7 +121,8 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 			// For 'fail' interceptor action we make sure the payment failed.
 			case routerrpc.ResolveHoldForwardAction_FAIL:
 				if attempt.Status != lnrpc.HTLCAttempt_FAILED {
-					t.t.Errorf("expected payment to fail, instead got %v", attempt.Status)
+					t.t.Errorf("expected payment to fail, "+
+						"instead got %v", attempt.Status)
 				}
 
 			// For settle and resume we make sure the payment is successful.
@@ -127,7 +131,8 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 
 			case routerrpc.ResolveHoldForwardAction_RESUME:
 				if attempt.Status != lnrpc.HTLCAttempt_SUCCEEDED {
-					t.t.Errorf("expected payment to succeed, instead got %v", attempt.Status)
+					t.t.Errorf("expected payment to "+
+						"succeed, instead got %v", attempt.Status)
 				}
 			}
 		}
@@ -202,7 +207,9 @@ func testForwardInterceptor(net *lntest.NetworkHarness, t *harnessTest) {
 				foundPayment.Status != lnrpc.Payment_IN_FLIGHT {
 
 				t.Fatalf("expected to find in flight payment for"+
-					"amount %v, %v", testCase.invoice.ValueMAtoms, foundPayment.Status)
+					"amount %v, %v",
+					testCase.invoice.ValueMAtoms,
+					foundPayment.Status)
 			}
 		}
 	}
@@ -294,12 +301,25 @@ func (c *interceptorTestContext) prepareTestCases() (
 		if err != nil {
 			return nil, fmt.Errorf("unable to add invoice: %v", err)
 		}
+
+		// We'll need to also decode the returned invoice so we can
+		// grab the payment address which is now required for ALL
+		// payments.
+		payReq, err := c.carol.DecodePayReq(context.Background(), &lnrpc.PayReqString{
+			PayReq: invoice.PaymentRequest,
+		})
+		if err != nil {
+			return nil, fmt.Errorf("unable to decode invoice: %v", err)
+		}
 		t.invoice = invoice
+		t.payAddr = payReq.PaymentAddr
 	}
 	return cases, nil
 }
 
-func (c *interceptorTestContext) openChannel(from, to *lntest.HarnessNode, chanSize dcrutil.Amount) {
+func (c *interceptorTestContext) openChannel(from, to *lntest.HarnessNode,
+	chanSize dcrutil.Amount) {
+
 	ctxb := context.Background()
 
 	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
@@ -365,10 +385,14 @@ func (c *interceptorTestContext) waitForChannels() {
 // sendAliceToCarolPayment sends a payment from alice to carol and make an
 // attempt to pay. The lnrpc.HTLCAttempt is returned.
 func (c *interceptorTestContext) sendAliceToCarolPayment(ctx context.Context,
-	amtMAtoms int64, paymentHash []byte) (*lnrpc.HTLCAttempt, error) {
+	amtMAtoms int64,
+	paymentHash, paymentAddr []byte) (*lnrpc.HTLCAttempt, error) {
 
 	// Build a route from alice to carol.
-	route, err := c.buildRoute(ctx, amtMAtoms, []*lntest.HarnessNode{c.bob, c.carol})
+	route, err := c.buildRoute(
+		ctx, amtMAtoms, []*lntest.HarnessNode{c.bob, c.carol},
+		paymentAddr,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -387,8 +411,8 @@ func (c *interceptorTestContext) sendAliceToCarolPayment(ctx context.Context,
 }
 
 // buildRoute is a helper function to build a route with given hops.
-func (c *interceptorTestContext) buildRoute(ctx context.Context, amtMAtoms int64, hops []*lntest.HarnessNode) (
-	*lnrpc.Route, error) {
+func (c *interceptorTestContext) buildRoute(ctx context.Context, amtMAtoms int64,
+	hops []*lntest.HarnessNode, payAddr []byte) (*lnrpc.Route, error) {
 
 	rpcHops := make([][]byte, 0, len(hops))
 	for _, hop := range hops {
@@ -405,6 +429,7 @@ func (c *interceptorTestContext) buildRoute(ctx context.Context, amtMAtoms int64
 		AmtMAtoms:      amtMAtoms,
 		FinalCltvDelta: chainreg.DefaultDecredTimeLockDelta,
 		HopPubkeys:     rpcHops,
+		PaymentAddr:    payAddr,
 	}
 
 	routeResp, err := c.alice.RouterClient.BuildRoute(ctx, req)

routing/router_test.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
@@ -2920,6 +2921,10 @@ func TestSendToRouteMaxHops(t *testing.T) {
 func TestBuildRoute(t *testing.T) {
 	// Setup a three node network.
 	chanCapSat := dcrutil.Amount(100000)
+	paymentAddrFeatures := lnwire.NewFeatureVector(
+		lnwire.NewRawFeatureVector(lnwire.PaymentAddrOptional),
+		lnwire.Features,
+	)
 	testChannels := []*testChannel{
 		// Create two local channels from a. The bandwidth is estimated
 		// in this test as the channel capacity. For building routes, we
@@ -2944,29 +2949,33 @@ func TestBuildRoute(t *testing.T) {
 		// nodes are recommended to keep their channel policies towards
 		// the same peer identical.
 		symmetricTestChannel("b", "c", chanCapSat, &testChannelPolicy{
-			Expiry:  144,
-			FeeRate: 50000,
-			MinHTLC: lnwire.NewMAtomsFromAtoms(20),
-			MaxHTLC: lnwire.NewMAtomsFromAtoms(120),
+			Expiry:   144,
+			FeeRate:  50000,
+			MinHTLC:  lnwire.NewMAtomsFromAtoms(20),
+			MaxHTLC:  lnwire.NewMAtomsFromAtoms(120),
+			Features: paymentAddrFeatures,
 		}, 2),
 		symmetricTestChannel("b", "c", chanCapSat, &testChannelPolicy{
-			Expiry:  144,
-			FeeRate: 60000,
-			MinHTLC: lnwire.NewMAtomsFromAtoms(20),
-			MaxHTLC: lnwire.NewMAtomsFromAtoms(120),
+			Expiry:   144,
+			FeeRate:  60000,
+			MinHTLC:  lnwire.NewMAtomsFromAtoms(20),
+			MaxHTLC:  lnwire.NewMAtomsFromAtoms(120),
+			Features: paymentAddrFeatures,
 		}, 7),
 
 		symmetricTestChannel("a", "e", chanCapSat, &testChannelPolicy{
-			Expiry:  144,
-			FeeRate: 80000,
-			MinHTLC: lnwire.NewMAtomsFromAtoms(5),
-			MaxHTLC: lnwire.NewMAtomsFromAtoms(10),
+			Expiry:   144,
+			FeeRate:  80000,
+			MinHTLC:  lnwire.NewMAtomsFromAtoms(5),
+			MaxHTLC:  lnwire.NewMAtomsFromAtoms(10),
+			Features: paymentAddrFeatures,
 		}, 5),
 		symmetricTestChannel("e", "c", chanCapSat, &testChannelPolicy{
-			Expiry:  144,
-			FeeRate: 100000,
-			MinHTLC: lnwire.NewMAtomsFromAtoms(20),
-			MaxHTLC: lnwire.NewMAtomsFromAtoms(chanCapSat),
+			Expiry:   144,
+			FeeRate:  100000,
+			MinHTLC:  lnwire.NewMAtomsFromAtoms(20),
+			MaxHTLC:  lnwire.NewMAtomsFromAtoms(chanCapSat),
+			Features: paymentAddrFeatures,
 		}, 4),
 	}
 
@@ -2986,7 +2995,9 @@ func TestBuildRoute(t *testing.T) {
 	}
 	defer cleanUp()
 
-	checkHops := func(rt *route.Route, expected []uint64) {
+	checkHops := func(rt *route.Route, expected []uint64,
+		payAddr [32]byte) {
+
 		t.Helper()
 
 		if len(rt.Hops) != len(expected) {
@@ -2999,7 +3010,15 @@ func TestBuildRoute(t *testing.T) {
 					expected[i], i, hop.ChannelID)
 			}
 		}
+
+		lastHop := rt.Hops[len(rt.Hops)-1]
+		require.NotNil(t, lastHop.MPP)
+		require.Equal(t, lastHop.MPP.PaymentAddr(), payAddr)
 	}
+
+	var payAddr [32]byte
+	_, err = rand.Read(payAddr[:])
+	require.NoError(t, err)
 
 	// Create hop list from the route node pubkeys.
 	hops := []route.Vertex{
@@ -3009,7 +3028,7 @@ func TestBuildRoute(t *testing.T) {
 
 	// Build the route for the given amount.
 	rt, err := ctx.router.BuildRoute(
-		&amt, hops, nil, 40,
+		&amt, hops, nil, 40, &payAddr,
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -3018,14 +3037,14 @@ func TestBuildRoute(t *testing.T) {
 	// Check that we get the expected route back. The total amount should be
 	// the amount to deliver to hop c (100 sats) plus the max fee for the
 	// connection b->c (6 sats).
-	checkHops(rt, []uint64{1, 7})
+	checkHops(rt, []uint64{1, 7}, payAddr)
 	if rt.TotalAmount != 106000 {
 		t.Fatalf("unexpected total amount %v", rt.TotalAmount)
 	}
 
 	// Build the route for the minimum amount.
 	rt, err = ctx.router.BuildRoute(
-		nil, hops, nil, 40,
+		nil, hops, nil, 40, &payAddr,
 	)
 	if err != nil {
 		t.Fatal(err)
@@ -3035,7 +3054,7 @@ func TestBuildRoute(t *testing.T) {
 	// send from b to c is 20 sats. Hop b charges 1200 msat for the
 	// forwarding. The channel between hop a and b can carry amounts in the
 	// range [5, 100], so 21200 msats is the minimum amount for this route.
-	checkHops(rt, []uint64{1, 7})
+	checkHops(rt, []uint64{1, 7}, payAddr)
 	if rt.TotalAmount != 21200 {
 		t.Fatalf("unexpected total amount %v", rt.TotalAmount)
 	}
@@ -3046,7 +3065,7 @@ func TestBuildRoute(t *testing.T) {
 		ctx.aliases["e"], ctx.aliases["c"],
 	}
 	_, err = ctx.router.BuildRoute(
-		nil, hops, nil, 40,
+		nil, hops, nil, 40, nil,
 	)
 	errNoChannel, ok := err.(ErrNoChannel)
 	if !ok {