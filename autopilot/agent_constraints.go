lightningnetwork/lnd

autopilot: split channel definition into LocalChannel/ChannelEdge
Since non-local channels won't have a balance field, we split the
definitions in anticipation of adding one.

autopilot/agent_constraints.go
@@ -13,7 +13,7 @@ type AgentConstraints interface {
 	// the first return value will represent the amount of additional funds
 	// available towards creating channels. The second return value is the
 	// exact *number* of additional channels available.
-	ChannelBudget(chans []Channel, balance btcutil.Amount) (
+	ChannelBudget(chans []LocalChannel, balance btcutil.Amount) (
 		btcutil.Amount, uint32)
 
 	// MaxPendingOpens returns the maximum number of pending channel
@@ -82,7 +82,7 @@ func NewConstraints(minChanSize, maxChanSize btcutil.Amount, chanLimit,
 // additional channels available.
 //
 // Note: part of the AgentConstraints interface.
-func (h *agentConstraints) ChannelBudget(channels []Channel,
+func (h *agentConstraints) ChannelBudget(channels []LocalChannel,
 	funds btcutil.Amount) (btcutil.Amount, uint32) {
 
 	// If we're already over our maximum allowed number of channels, then

autopilot/interface.go
@@ -36,10 +36,10 @@ type Node interface {
 	ForEachChannel(func(ChannelEdge) error) error
 }
 
-// Channel is a simple struct which contains relevant details of a particular
-// channel within the channel graph. The fields in this struct may be used a
-// signals for various AttachmentHeuristic implementations.
-type Channel struct {
+// LocalChannel is a simple struct which contains relevant details of a
+// particular channel the local node has. The fields in this struct may be used
+// a signals for various AttachmentHeuristic implementations.
+type LocalChannel struct {
 	// ChanID is the short channel ID for this channel as defined within
 	// BOLT-0007.
 	ChanID lnwire.ShortChannelID
@@ -59,8 +59,12 @@ type Channel struct {
 // edge within the graph. The existence of this reference to the connected node
 // will allow callers to traverse the graph in an object-oriented manner.
 type ChannelEdge struct {
-	// Channel contains the attributes of this channel.
-	Channel
+	// ChanID is the short channel ID for this channel as defined within
+	// BOLT-0007.
+	ChanID lnwire.ShortChannelID
+
+	// Capacity is the capacity of the channel expressed in satoshis.
+	Capacity btcutil.Amount
 
 	// Peer is the peer that this channel creates an edge to in the channel
 	// graph.
@@ -136,7 +140,7 @@ type AttachmentHeuristic interface {
 	//
 	// NOTE: A NodeID not found in the returned map is implicitly given a
 	// score of 0.
-	NodeScores(g ChannelGraph, chans []Channel,
+	NodeScores(g ChannelGraph, chans []LocalChannel,
 		chanSize btcutil.Amount, nodes map[NodeID]struct{}) (
 		map[NodeID]*NodeScore, error)
 }

decred/dcrlnd

autopilot/agent_constraints.go
@@ -13,7 +13,7 @@ type AgentConstraints interface {
 	// the first return value will represent the amount of additional funds
 	// available towards creating channels. The second return value is the
 	// exact *number* of additional channels available.
-	ChannelBudget(chans []Channel, balance dcrutil.Amount) (
+	ChannelBudget(chans []LocalChannel, balance dcrutil.Amount) (
 		dcrutil.Amount, uint32)
 
 	// MaxPendingOpens returns the maximum number of pending channel
@@ -82,7 +82,7 @@ func NewConstraints(minChanSize, maxChanSize dcrutil.Amount, chanLimit,
 // additional channels available.
 //
 // Note: part of the AgentConstraints interface.
-func (h *agentConstraints) ChannelBudget(channels []Channel,
+func (h *agentConstraints) ChannelBudget(channels []LocalChannel,
 	funds dcrutil.Amount) (dcrutil.Amount, uint32) {
 
 	// If we're already over our maximum allowed number of channels, then

autopilot/interface.go
@@ -36,10 +36,10 @@ type Node interface {
 	ForEachChannel(func(ChannelEdge) error) error
 }
 
-// Channel is a simple struct which contains relevant details of a particular
-// channel within the channel graph. The fields in this struct may be used a
-// signals for various AttachmentHeuristic implementations.
-type Channel struct {
+// LocalChannel is a simple struct which contains relevant details of a
+// particular channel the local node has. The fields in this struct may be used
+// a signals for various AttachmentHeuristic implementations.
+type LocalChannel struct {
 	// ChanID is the short channel ID for this channel as defined within
 	// BOLT-0007.
 	ChanID lnwire.ShortChannelID
@@ -59,8 +59,12 @@ type Channel struct {
 // edge within the graph. The existence of this reference to the connected node
 // will allow callers to traverse the graph in an object-oriented manner.
 type ChannelEdge struct {
-	// Channel contains the attributes of this channel.
-	Channel
+	// ChanID is the short channel ID for this channel as defined within
+	// BOLT-0007.
+	ChanID lnwire.ShortChannelID
+
+	// Capacity is the capacity of the channel expressed in satoshis.
+	Capacity dcrutil.Amount
 
 	// Peer is the peer that this channel creates an edge to in the channel
 	// graph.
@@ -136,7 +140,7 @@ type AttachmentHeuristic interface {
 	//
 	// NOTE: A NodeID not found in the returned map is implicitly given a
 	// score of 0.
-	NodeScores(g ChannelGraph, chans []Channel,
+	NodeScores(g ChannelGraph, chans []LocalChannel,
 		chanSize dcrutil.Amount, nodes map[NodeID]struct{}) (
 		map[NodeID]*NodeScore, error)
 }