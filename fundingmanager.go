Crypt-iQ/lnd

lnd: export Manager's InitFundingWorkflow method

fundingmanager.go
@@ -2912,10 +2912,10 @@ func (m *Manager) announceChannel(localIDKey, remoteIDKey, localFundingKey,
 	return nil
 }
 
-// initFundingWorkflow sends a message to the funding manager instructing it
+// InitFundingWorkflow sends a message to the funding manager instructing it
 // to initiate a single funder workflow with the source peer.
 // TODO(roasbeef): re-visit blocking nature..
-func (m *Manager) initFundingWorkflow(peer lnpeer.Peer, req *openChanReq) {
+func (m *Manager) InitFundingWorkflow(peer lnpeer.Peer, req *openChanReq) {
 	m.fundingRequests <- &initFundingMsg{
 		peer:        peer,
 		openChanReq: req,

decred/dcrlnd

fundingmanager.go
@@ -2926,10 +2926,10 @@ func (f *Manager) announceChannel(localIDKey, remoteIDKey, localFundingKey,
 	return nil
 }
 
-// initFundingWorkflow sends a message to the funding manager instructing it
+// InitFundingWorkflow sends a message to the funding manager instructing it
 // to initiate a single funder workflow with the source peer.
 // TODO(roasbeef): re-visit blocking nature..
-func (f *Manager) initFundingWorkflow(peer lnpeer.Peer, req *openChanReq) {
+func (f *Manager) InitFundingWorkflow(peer lnpeer.Peer, req *openChanReq) {
 	f.fundingRequests <- &initFundingMsg{
 		peer:        peer,
 		openChanReq: req,