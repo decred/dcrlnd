lightningnetwork/lnd

autopilot: rename to Capacity to Balance, define ChannelInfo()
We rename the field Capacity on local channels to Balance, define a new
method ChannelInfo on the manager config that let us query the database
for our latest channels state. Using this we will use the current local
balance instead of the channel capacity when doing allocation
calculations.

autopilot/agent_constraints_test.go
@@ -49,16 +49,16 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(prand.Int31()),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(prand.Int31()),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(prand.Int31()),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(prand.Int31()),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(prand.Int31()),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(prand.Int31()),
 				},
 			},
 			btcutil.Amount(btcutil.SatoshiPerBitcoin * 10),
@@ -72,12 +72,12 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(btcutil.SatoshiPerBitcoin),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(btcutil.SatoshiPerBitcoin),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(btcutil.SatoshiPerBitcoin),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(btcutil.SatoshiPerBitcoin),
 				},
 			},
 			btcutil.Amount(btcutil.SatoshiPerBitcoin * 2),
@@ -95,8 +95,8 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(btcutil.SatoshiPerBitcoin),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(btcutil.SatoshiPerBitcoin),
 				},
 			},
 			btcutil.Amount(btcutil.SatoshiPerBitcoin * 9),
@@ -115,12 +115,12 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(btcutil.SatoshiPerBitcoin),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(btcutil.SatoshiPerBitcoin),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(btcutil.SatoshiPerBitcoin * 3),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(btcutil.SatoshiPerBitcoin * 3),
 				},
 			},
 			btcutil.Amount(btcutil.SatoshiPerBitcoin * 10),
@@ -134,12 +134,12 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(btcutil.SatoshiPerBitcoin),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(btcutil.SatoshiPerBitcoin),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: btcutil.Amount(btcutil.SatoshiPerBitcoin),
+					ChanID:  randChanID(),
+					Balance: btcutil.Amount(btcutil.SatoshiPerBitcoin),
 				},
 			},
 			btcutil.Amount(btcutil.SatoshiPerBitcoin),

autopilot/interface.go
@@ -44,8 +44,8 @@ type LocalChannel struct {
 	// BOLT-0007.
 	ChanID lnwire.ShortChannelID
 
-	// Capacity is the capacity of the channel expressed in satoshis.
-	Capacity btcutil.Amount
+	// Balance is the local balance of the channel expressed in satoshis.
+	Balance btcutil.Amount
 
 	// Node is the peer that this channel has been established with.
 	Node NodeID

autopilot/manager.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/routing"
@@ -24,6 +25,10 @@ type ManagerCfg struct {
 	// ChannelState is a function closure that returns the current set of
 	// channels managed by this node.
 	ChannelState func() ([]LocalChannel, error)
+
+	// ChannelInfo is a function closure that returns the channel managed
+	// by the node given by the passed channel point.
+	ChannelInfo func(wire.OutPoint) (*LocalChannel, error)
 
 	// SubscribeTransactions is used to get a subscription for transactions
 	// relevant to this node's wallet.
@@ -194,18 +199,16 @@ func (m *Manager) StartAgent() error {
 					// opened, then we'll convert it to the
 					// autopilot.Channel format, and notify
 					// the pilot of the new channel.
-					chanNode := NewNodeID(
-						edgeUpdate.ConnectingNode,
-					)
-					chanID := lnwire.NewShortChanIDFromInt(
-						edgeUpdate.ChanID,
-					)
-					edge := LocalChannel{
-						ChanID:   chanID,
-						Capacity: edgeUpdate.Capacity,
-						Node:     chanNode,
+					cp := edgeUpdate.ChanPoint
+					edge, err := m.cfg.ChannelInfo(cp)
+					if err != nil {
+						log.Errorf("Unable to fetch "+
+							"channel info for %v: "+
+							"%v", cp, err)
+						continue
 					}
-					pilot.OnChannelOpen(edge)
+
+					pilot.OnChannelOpen(*edge)
 				}
 
 				// For each closed channel, we'll obtain

pilot.go
@@ -260,15 +260,34 @@ func initAutoPilot(svr *server, cfg *lncfg.AutoPilot,
 			chanState := make([]autopilot.LocalChannel,
 				len(activeChannels))
 			for i, channel := range activeChannels {
+				localCommit := channel.LocalCommitment
+				balance := localCommit.LocalBalance.ToSatoshis()
+
 				chanState[i] = autopilot.LocalChannel{
-					ChanID:   channel.ShortChanID(),
-					Capacity: channel.Capacity,
+					ChanID:  channel.ShortChanID(),
+					Balance: balance,
 					Node: autopilot.NewNodeID(
-						channel.IdentityPub),
+						channel.IdentityPub,
+					),
 				}
 			}
 
 			return chanState, nil
+		},
+		ChannelInfo: func(chanPoint wire.OutPoint) (
+			*autopilot.LocalChannel, error) {
+
+			channel, err := svr.remoteChanDB.FetchChannel(chanPoint)
+			if err != nil {
+				return nil, err
+			}
+
+			localCommit := channel.LocalCommitment
+			return &autopilot.LocalChannel{
+				ChanID:  channel.ShortChanID(),
+				Balance: localCommit.LocalBalance.ToSatoshis(),
+				Node:    autopilot.NewNodeID(channel.IdentityPub),
+			}, nil
 		},
 		SubscribeTransactions: svr.cc.wallet.SubscribeTransactions,
 		SubscribeTopology:     svr.chanRouter.SubscribeTopology,

decred/dcrlnd

autopilot/agent_constraints_test.go
@@ -49,16 +49,16 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(prand.Int31()),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(prand.Int31()),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(prand.Int31()),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(prand.Int31()),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(prand.Int31()),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(prand.Int31()),
 				},
 			},
 			dcrutil.Amount(dcrutil.AtomsPerCoin * 10),
@@ -72,12 +72,12 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(dcrutil.AtomsPerCoin),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(dcrutil.AtomsPerCoin),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(dcrutil.AtomsPerCoin),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(dcrutil.AtomsPerCoin),
 				},
 			},
 			dcrutil.Amount(dcrutil.AtomsPerCoin * 2),
@@ -95,8 +95,8 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(dcrutil.AtomsPerCoin),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(dcrutil.AtomsPerCoin),
 				},
 			},
 			dcrutil.Amount(dcrutil.AtomsPerCoin * 9),
@@ -115,12 +115,12 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(dcrutil.AtomsPerCoin),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(dcrutil.AtomsPerCoin),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(dcrutil.AtomsPerCoin * 3),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(dcrutil.AtomsPerCoin * 3),
 				},
 			},
 			dcrutil.Amount(dcrutil.AtomsPerCoin * 10),
@@ -134,12 +134,12 @@ func TestConstraintsChannelBudget(t *testing.T) {
 		{
 			[]LocalChannel{
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(dcrutil.AtomsPerCoin),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(dcrutil.AtomsPerCoin),
 				},
 				{
-					ChanID:   randChanID(),
-					Capacity: dcrutil.Amount(dcrutil.AtomsPerCoin),
+					ChanID:  randChanID(),
+					Balance: dcrutil.Amount(dcrutil.AtomsPerCoin),
 				},
 			},
 			dcrutil.Amount(dcrutil.AtomsPerCoin),

autopilot/interface.go
@@ -44,8 +44,8 @@ type LocalChannel struct {
 	// BOLT-0007.
 	ChanID lnwire.ShortChannelID
 
-	// Capacity is the capacity of the channel expressed in base units.
-	Capacity dcrutil.Amount
+	// Balance is the local balance of the channel expressed in satoshis.
+	Balance dcrutil.Amount
 
 	// Node is the peer that this channel has been established with.
 	Node NodeID

autopilot/manager.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
+	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/routing"
@@ -24,6 +25,10 @@ type ManagerCfg struct {
 	// ChannelState is a function closure that returns the current set of
 	// channels managed by this node.
 	ChannelState func() ([]LocalChannel, error)
+
+	// ChannelInfo is a function closure that returns the channel managed
+	// by the node given by the passed channel point.
+	ChannelInfo func(wire.OutPoint) (*LocalChannel, error)
 
 	// SubscribeTransactions is used to get a subscription for transactions
 	// relevant to this node's wallet.
@@ -194,18 +199,16 @@ func (m *Manager) StartAgent() error {
 					// opened, then we'll convert it to the
 					// autopilot.Channel format, and notify
 					// the pilot of the new channel.
-					chanNode := NewNodeID(
-						edgeUpdate.ConnectingNode,
-					)
-					chanID := lnwire.NewShortChanIDFromInt(
-						edgeUpdate.ChanID,
-					)
-					edge := LocalChannel{
-						ChanID:   chanID,
-						Capacity: edgeUpdate.Capacity,
-						Node:     chanNode,
+					cp := edgeUpdate.ChanPoint
+					edge, err := m.cfg.ChannelInfo(cp)
+					if err != nil {
+						log.Errorf("Unable to fetch "+
+							"channel info for %v: "+
+							"%v", cp, err)
+						continue
 					}
-					pilot.OnChannelOpen(edge)
+
+					pilot.OnChannelOpen(*edge)
 				}
 
 				// For each closed channel, we'll obtain

pilot.go
@@ -263,15 +263,34 @@ func initAutoPilot(svr *server, cfg *lncfg.AutoPilot,
 			chanState := make([]autopilot.LocalChannel,
 				len(activeChannels))
 			for i, channel := range activeChannels {
+				localCommit := channel.LocalCommitment
+				balance := localCommit.LocalBalance.ToAtoms()
+
 				chanState[i] = autopilot.LocalChannel{
-					ChanID:   channel.ShortChanID(),
-					Capacity: channel.Capacity,
+					ChanID:  channel.ShortChanID(),
+					Balance: balance,
 					Node: autopilot.NewNodeID(
-						channel.IdentityPub),
+						channel.IdentityPub,
+					),
 				}
 			}
 
 			return chanState, nil
+		},
+		ChannelInfo: func(chanPoint wire.OutPoint) (
+			*autopilot.LocalChannel, error) {
+
+			channel, err := svr.remoteChanDB.FetchChannel(chanPoint)
+			if err != nil {
+				return nil, err
+			}
+
+			localCommit := channel.LocalCommitment
+			return &autopilot.LocalChannel{
+				ChanID:  channel.ShortChanID(),
+				Balance: localCommit.LocalBalance.ToAtoms(),
+				Node:    autopilot.NewNodeID(channel.IdentityPub),
+			}, nil
 		},
 		SubscribeTransactions: svr.cc.wallet.SubscribeTransactions,
 		SubscribeTopology:     svr.chanRouter.SubscribeTopology,