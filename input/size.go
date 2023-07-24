halseth/lnd

input+sweep: let weightestimator take known TxOut

input/size.go
@@ -533,6 +533,14 @@ func (twe *TxWeightEstimator) AddNestedP2WSHInput(witnessSize int) *TxWeightEsti
 	return twe
 }
 
+// AddTxOutput adds a known TxOut to the weight estimator.
+func (twe *TxWeightEstimator) AddTxOutput(txOut *wire.TxOut) *TxWeightEstimator {
+	twe.outputSize += txOut.SerializeSize()
+	twe.outputCount++
+
+	return twe
+}
+
 // AddP2PKHOutput updates the weight estimate to account for an additional P2PKH
 // output.
 func (twe *TxWeightEstimator) AddP2PKHOutput() *TxWeightEstimator {

sweep/weight_estimator.go
@@ -2,6 +2,7 @@ package sweep
 
 import (
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
@@ -90,6 +91,12 @@ func (w *weightEstimator) tryAddParent(inp input.Input) {
 // native P2WKH output.
 func (w *weightEstimator) addP2WKHOutput() {
 	w.estimator.AddP2WKHOutput()
+}
+
+// addOutput updates the weight estimate to account for the known
+// output given.
+func (w *weightEstimator) addOutput(txOut *wire.TxOut) {
+	w.estimator.AddTxOutput(txOut)
 }
 
 // weight gets the estimated weight of the transaction.

decred/dcrlnd

input/size.go
@@ -765,6 +765,14 @@ func (twe *TxSizeEstimator) AddCustomInput(sigScriptSize int64) *TxSizeEstimator
 	return twe
 }
 
+// AddTxOutput adds a known TxOut to the weight estimator.
+func (twe *TxSizeEstimator) AddTxOutput(txOut *wire.TxOut) *TxSizeEstimator {
+	twe.OutputSize += int64(txOut.SerializeSize())
+	twe.outputCount++
+
+	return twe
+}
+
 // AddP2PKHOutput updates the size estimate to account for an additional P2PKH
 // output.
 func (twe *TxSizeEstimator) AddP2PKHOutput() *TxSizeEstimator {

sweep/weight_estimator.go
@@ -3,6 +3,7 @@ package sweep
 import (
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrutil/v4"
+	"github.com/decred/dcrd/wire"
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnwallet/chainfee"
 )
@@ -89,6 +90,12 @@ func (w *sizeEstimator) tryAddParent(inp input.Input) {
 // native P2PKH output.
 func (w *sizeEstimator) addP2PKHOutput() {
 	w.estimator.AddP2PKHOutput()
+}
+
+// addOutput updates the weight estimate to account for the known
+// output given.
+func (w *sizeEstimator) addOutput(txOut *wire.TxOut) {
+	w.estimator.AddTxOutput(txOut)
 }
 
 // size gets the estimated size of the transaction.