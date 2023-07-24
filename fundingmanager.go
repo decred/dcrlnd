Crypt-iQ/lnd

lnd: remove Manager's stop method, linter complaints

fundingmanager.go
@@ -614,21 +614,12 @@ func (m *Manager) start() error {
 
 // Stop signals all helper goroutines to execute a graceful shutdown. This
 // method will block until all goroutines have exited.
-func (m *Manager) Stop() error {
-	var err error
+func (m *Manager) Stop() {
 	m.stopped.Do(func() {
-		err = m.stop()
+		fndgLog.Info("Funding manager shutting down")
+		close(m.quit)
+		m.wg.Wait()
 	})
-	return err
-}
-
-func (m *Manager) stop() error {
-	fndgLog.Infof("Funding manager shutting down")
-
-	close(m.quit)
-	m.wg.Wait()
-
-	return nil
 }
 
 // nextPendingChanID returns the next free pending channel ID to be used to

decred/dcrlnd

fundingmanager.go
@@ -618,21 +618,12 @@ func (f *Manager) start() error {
 
 // Stop signals all helper goroutines to execute a graceful shutdown. This
 // method will block until all goroutines have exited.
-func (f *Manager) Stop() error {
-	var err error
+func (f *Manager) Stop() {
 	f.stopped.Do(func() {
-		err = f.stop()
+		fndgLog.Info("Funding manager shutting down")
+		close(f.quit)
+		f.wg.Wait()
 	})
-	return err
-}
-
-func (f *Manager) stop() error {
-	fndgLog.Infof("Funding manager shutting down")
-
-	close(f.quit)
-	f.wg.Wait()
-
-	return nil
 }
 
 // nextPendingChanID returns the next free pending channel ID to be used to