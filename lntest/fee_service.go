lightningnetwork/lnd

lntest: use web fee estimator in itests

lntest/fee_service.go
@@ -1,0 +1,106 @@
+package lntest
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
+)
+
+const (
+	// feeServiceTarget is the confirmation target for which a fee estimate
+	// is returned. Requests for higher confirmation targets will fall back
+	// to this.
+	feeServiceTarget = 2
+
+	// feeServicePort is the tcp port on which the service runs.
+	feeServicePort = 16534
+)
+
+// feeService runs a web service that provides fee estimation information.
+type feeService struct {
+	feeEstimates
+
+	srv *http.Server
+	wg  sync.WaitGroup
+
+	url string
+
+	lock sync.Mutex
+}
+
+// feeEstimates contains the current fee estimates.
+type feeEstimates struct {
+	Fees map[uint32]uint32 `json:"fee_by_block_target"`
+}
+
+// startFeeService spins up a go-routine to serve fee estimates.
+func startFeeService() *feeService {
+	f := feeService{
+		url: fmt.Sprintf(
+			"http://localhost:%v/fee-estimates.json", feeServicePort,
+		),
+	}
+
+	// Initialize default fee estimate.
+	f.Fees = map[uint32]uint32{feeServiceTarget: 50000}
+
+	listenAddr := fmt.Sprintf(":%v", feeServicePort)
+	f.srv = &http.Server{
+		Addr: listenAddr,
+	}
+
+	http.HandleFunc("/fee-estimates.json", f.handleRequest)
+
+	f.wg.Add(1)
+	go func() {
+		defer f.wg.Done()
+
+		if err := f.srv.ListenAndServe(); err != http.ErrServerClosed {
+			fmt.Printf("error: cannot start fee api: %v", err)
+		}
+	}()
+
+	return &f
+}
+
+// handleRequest handles a client request for fee estimates.
+func (f *feeService) handleRequest(w http.ResponseWriter, r *http.Request) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+
+	bytes, err := json.Marshal(f.feeEstimates)
+	if err != nil {
+		fmt.Printf("error: cannot serialize "+
+			"estimates: %v", err)
+
+		return
+	}
+
+	_, err = io.WriteString(w, string(bytes))
+	if err != nil {
+		fmt.Printf("error: cannot send estimates: %v",
+			err)
+	}
+}
+
+// stop stops the web server.
+func (f *feeService) stop() {
+	if err := f.srv.Shutdown(context.Background()); err != nil {
+		fmt.Printf("error: cannot stop fee api: %v", err)
+	}
+
+	f.wg.Wait()
+}
+
+// setFee changes the current fee estimate for the fixed confirmation target.
+func (f *feeService) setFee(fee chainfee.SatPerKWeight) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+
+	f.Fees[feeServiceTarget] = uint32(fee.FeePerKVByte())
+}

lntest/harness.go
@@ -22,6 +22,7 @@ import (
 	"github.com/lightningnetwork/lnd"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lntest/wait"
+	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"google.golang.org/grpc/grpclog"
 )
@@ -63,6 +64,10 @@ type NetworkHarness struct {
 	// to main process.
 	lndErrorChan chan error
 
+	// feeService is a web service that provides external fee estimates to
+	// lnd.
+	feeService *feeService
+
 	quit chan struct{}
 
 	mtx sync.Mutex
@@ -75,6 +80,8 @@ type NetworkHarness struct {
 func NewNetworkHarness(r *rpctest.Harness, b BackendConfig, lndBinary string) (
 	*NetworkHarness, error) {
 
+	feeService := startFeeService()
+
 	n := NetworkHarness{
 		activeNodes:          make(map[int]*HarnessNode),
 		nodesByPub:           make(map[string]*HarnessNode),
@@ -84,6 +91,7 @@ func NewNetworkHarness(r *rpctest.Harness, b BackendConfig, lndBinary string) (
 		netParams:            r.ActiveNet,
 		Miner:                r,
 		BackendCfg:           b,
+		feeService:           feeService,
 		quit:                 make(chan struct{}),
 		lndBinary:            lndBinary,
 	}
@@ -251,6 +259,8 @@ func (n *NetworkHarness) TearDownAll() error {
 	close(n.lndErrorChan)
 	close(n.quit)
 
+	n.feeService.stop()
+
 	return nil
 }
 
@@ -358,6 +368,7 @@ func (n *NetworkHarness) newNode(name string, extraArgs []string,
 		BackendCfg: n.BackendCfg,
 		NetParams:  n.netParams,
 		ExtraArgs:  extraArgs,
+		FeeURL:     n.feeService.url,
 	})
 	if err != nil {
 		return nil, err
@@ -1402,6 +1413,10 @@ func (n *NetworkHarness) sendCoins(ctx context.Context, amt btcutil.Amount,
 
 	expectedBalance := btcutil.Amount(initialBalance.ConfirmedBalance) + amt
 	return target.WaitForBalance(expectedBalance, true)
+}
+
+func (n *NetworkHarness) SetFeeEstimate(fee chainfee.SatPerKWeight) {
+	n.feeService.setFee(fee)
 }
 
 // CopyFile copies the file src to dest.

decred/dcrlnd

lntest/fee_service.go
@@ -1,0 +1,106 @@
+package lntest
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
+)
+
+const (
+	// feeServiceTarget is the confirmation target for which a fee estimate
+	// is returned. Requests for higher confirmation targets will fall back
+	// to this.
+	feeServiceTarget = 2
+
+	// feeServicePort is the tcp port on which the service runs.
+	feeServicePort = 16534
+)
+
+// feeService runs a web service that provides fee estimation information.
+type feeService struct {
+	feeEstimates
+
+	srv *http.Server
+	wg  sync.WaitGroup
+
+	url string
+
+	lock sync.Mutex
+}
+
+// feeEstimates contains the current fee estimates.
+type feeEstimates struct {
+	Fees map[uint32]uint32 `json:"fee_by_block_target"`
+}
+
+// startFeeService spins up a go-routine to serve fee estimates.
+func startFeeService() *feeService {
+	f := feeService{
+		url: fmt.Sprintf(
+			"http://localhost:%v/fee-estimates.json", feeServicePort,
+		),
+	}
+
+	// Initialize default fee estimate.
+	f.Fees = map[uint32]uint32{feeServiceTarget: 1e4}
+
+	listenAddr := fmt.Sprintf(":%v", feeServicePort)
+	f.srv = &http.Server{
+		Addr: listenAddr,
+	}
+
+	http.HandleFunc("/fee-estimates.json", f.handleRequest)
+
+	f.wg.Add(1)
+	go func() {
+		defer f.wg.Done()
+
+		if err := f.srv.ListenAndServe(); err != http.ErrServerClosed {
+			fmt.Printf("error: cannot start fee api: %v", err)
+		}
+	}()
+
+	return &f
+}
+
+// handleRequest handles a client request for fee estimates.
+func (f *feeService) handleRequest(w http.ResponseWriter, r *http.Request) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+
+	bytes, err := json.Marshal(f.feeEstimates)
+	if err != nil {
+		fmt.Printf("error: cannot serialize "+
+			"estimates: %v", err)
+
+		return
+	}
+
+	_, err = io.WriteString(w, string(bytes))
+	if err != nil {
+		fmt.Printf("error: cannot send estimates: %v",
+			err)
+	}
+}
+
+// stop stops the web server.
+func (f *feeService) stop() {
+	if err := f.srv.Shutdown(context.Background()); err != nil {
+		fmt.Printf("error: cannot stop fee api: %v", err)
+	}
+
+	f.wg.Wait()
+}
+
+// setFee changes the current fee estimate for the fixed confirmation target.
+func (f *feeService) setFee(fee chainfee.AtomPerKByte) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+
+	f.Fees[feeServiceTarget] = uint32(fee)
+}

lntest/harness.go
@@ -13,7 +13,12 @@ import (
 	"sync"
 	"time"
 
-	"google.golang.org/grpc/grpclog"
+	"github.com/decred/dcrlnd"
+	"github.com/decred/dcrlnd/input"
+	"github.com/decred/dcrlnd/lnrpc"
+	"github.com/decred/dcrlnd/lntest/wait"
+	"github.com/decred/dcrlnd/lnwallet/chainfee"
+	"github.com/decred/dcrlnd/lnwire"
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/chaincfg/v3"
@@ -21,12 +26,7 @@ import (
 	"github.com/decred/dcrd/txscript/v4/stdaddr"
 	"github.com/decred/dcrd/wire"
 	rpctest "github.com/decred/dcrtest/dcrdtest"
-
-	"github.com/decred/dcrlnd"
-	"github.com/decred/dcrlnd/input"
-	"github.com/decred/dcrlnd/lnrpc"
-	"github.com/decred/dcrlnd/lntest/wait"
-	"github.com/decred/dcrlnd/lnwire"
+	"google.golang.org/grpc/grpclog"
 )
 
 // DefaultCSV is the CSV delay (remotedelay) we will start our test nodes with.
@@ -69,6 +69,10 @@ type NetworkHarness struct {
 	// to main process.
 	lndErrorChan chan error
 
+	// feeService is a web service that provides external fee estimates to
+	// lnd.
+	feeService *feeService
+
 	quit chan struct{}
 
 	mtx sync.Mutex
@@ -81,6 +85,8 @@ type NetworkHarness struct {
 func NewNetworkHarness(r *rpctest.Harness, b BackendConfig, lndBinary string) (
 	*NetworkHarness, error) {
 
+	feeService := startFeeService()
+
 	n := NetworkHarness{
 		activeNodes:         make(map[int]*HarnessNode),
 		nodesByPub:          make(map[string]*HarnessNode),
@@ -91,6 +97,7 @@ func NewNetworkHarness(r *rpctest.Harness, b BackendConfig, lndBinary string) (
 		Miner:               r,
 		BackendCfg:          b,
 		quit:                make(chan struct{}),
+		feeService:          feeService,
 		lndBinary:           lndBinary,
 	}
 	go n.networkWatcher()
@@ -293,6 +300,8 @@ func (n *NetworkHarness) TearDownAll() error {
 	close(n.lndErrorChan)
 	close(n.quit)
 
+	n.feeService.stop()
+
 	return nil
 }
 
@@ -402,6 +411,7 @@ func (n *NetworkHarness) newNode(name string, extraArgs []string,
 		ExtraArgs:    extraArgs,
 		RemoteWallet: useRemoteWallet(),
 		DcrwNode:     useDcrwNode(),
+		FeeURL:       n.feeService.url,
 	})
 	if err != nil {
 		return nil, err
@@ -1522,6 +1532,10 @@ func (n *NetworkHarness) SlowGenerate(nb uint32) ([]*chainhash.Hash, error) {
 		res[i] = genRes[0]
 	}
 	return res, nil
+}
+
+func (n *NetworkHarness) SetFeeEstimate(fee chainfee.AtomPerKByte) {
+	n.feeService.setFee(fee)
 }
 
 // CopyFile copies the file src to dest.