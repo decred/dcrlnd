lightningnetwork/lnd

chainreg: register logger

log.go
@@ -10,6 +10,7 @@ import (
 	"github.com/lightningnetwork/lnd/autopilot"
 	"github.com/lightningnetwork/lnd/build"
 	"github.com/lightningnetwork/lnd/chainntnfs"
+	"github.com/lightningnetwork/lnd/chainreg"
 	"github.com/lightningnetwork/lnd/chanbackup"
 	"github.com/lightningnetwork/lnd/chanfitness"
 	"github.com/lightningnetwork/lnd/channeldb"
@@ -131,6 +132,7 @@ func SetupLoggers(root *build.RotatingLogWriter) {
 	AddSubLogger(root, chanfitness.Subsystem, chanfitness.UseLogger)
 	AddSubLogger(root, verrpc.Subsystem, verrpc.UseLogger)
 	AddSubLogger(root, healthcheck.Subsystem, healthcheck.UseLogger)
+	AddSubLogger(root, chainreg.Subsystem, chainreg.UseLogger)
 }
 
 // AddSubLogger is a helper method to conveniently create and register the

decred/dcrlnd

log.go
@@ -7,6 +7,7 @@ import (
 	"github.com/decred/dcrlnd/autopilot"
 	"github.com/decred/dcrlnd/build"
 	"github.com/decred/dcrlnd/chainntnfs"
+	"github.com/decred/dcrlnd/chainreg"
 	"github.com/decred/dcrlnd/chainscan"
 	"github.com/decred/dcrlnd/chainscan/csdrivers"
 	"github.com/decred/dcrlnd/chanbackup"
@@ -136,6 +137,7 @@ func SetupLoggers(root *build.RotatingLogWriter) {
 	AddSubLogger(root, chanfitness.Subsystem, chanfitness.UseLogger)
 	AddSubLogger(root, verrpc.Subsystem, verrpc.UseLogger)
 	AddSubLogger(root, healthcheck.Subsystem, healthcheck.UseLogger)
+	AddSubLogger(root, chainreg.Subsystem, chainreg.UseLogger)
 
 	// Decred-specific logs.
 	AddSubLogger(root, "DCRW", dcrwallet.UseLogger)