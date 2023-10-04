lightningnetwork/lnd

lnd+lnwallet: make capacity check stricter by adding fee
Also patches breacharbiter tests by using the correct commitment
fee and balances.

breacharbiter_test.go
@@ -1843,22 +1843,24 @@ func createInitChannels(revocationWindow int) (*lnwallet.LightningChannel, *lnwa
 		return nil, nil, nil, err
 	}
 
+	commitFee := feePerKw.FeeForWeight(input.CommitWeight)
+
 	// TODO(roasbeef): need to factor in commit fee?
 	aliceCommit := channeldb.ChannelCommitment{
 		CommitHeight:  0,
-		LocalBalance:  lnwire.NewMSatFromSatoshis(channelBal),
+		LocalBalance:  lnwire.NewMSatFromSatoshis(channelBal - commitFee),
 		RemoteBalance: lnwire.NewMSatFromSatoshis(channelBal),
 		FeePerKw:      btcutil.Amount(feePerKw),
-		CommitFee:     8688,
+		CommitFee:     commitFee,
 		CommitTx:      aliceCommitTx,
 		CommitSig:     bytes.Repeat([]byte{1}, 71),
 	}
 	bobCommit := channeldb.ChannelCommitment{
 		CommitHeight:  0,
 		LocalBalance:  lnwire.NewMSatFromSatoshis(channelBal),
-		RemoteBalance: lnwire.NewMSatFromSatoshis(channelBal),
+		RemoteBalance: lnwire.NewMSatFromSatoshis(channelBal - commitFee),
 		FeePerKw:      btcutil.Amount(feePerKw),
-		CommitFee:     8688,
+		CommitFee:     commitFee,
 		CommitTx:      bobCommitTx,
 		CommitSig:     bytes.Repeat([]byte{1}, 71),
 	}

decred/dcrlnd

breacharbiter_test.go
@@ -1852,10 +1852,12 @@ func createInitChannels(revocationWindow int) (*lnwallet.LightningChannel, *lnwa
 		return nil, nil, nil, err
 	}
 
+	commitFee := initiatorFee
+
 	// TODO(roasbeef): need to factor in commit fee?
 	aliceCommit := channeldb.ChannelCommitment{
 		CommitHeight:  0,
-		LocalBalance:  lnwire.NewMAtomsFromAtoms(channelBal - initiatorFee),
+		LocalBalance:  lnwire.NewMAtomsFromAtoms(channelBal - commitFee),
 		RemoteBalance: lnwire.NewMAtomsFromAtoms(channelBal),
 		FeePerKB:      dcrutil.Amount(feePerKB),
 		CommitFee:     3640,
@@ -1865,7 +1867,7 @@ func createInitChannels(revocationWindow int) (*lnwallet.LightningChannel, *lnwa
 	bobCommit := channeldb.ChannelCommitment{
 		CommitHeight:  0,
 		LocalBalance:  lnwire.NewMAtomsFromAtoms(channelBal),
-		RemoteBalance: lnwire.NewMAtomsFromAtoms(channelBal - initiatorFee),
+		RemoteBalance: lnwire.NewMAtomsFromAtoms(channelBal - commitFee),
 		FeePerKB:      dcrutil.Amount(feePerKB),
 		CommitFee:     3640,
 		CommitTx:      bobCommitTx,