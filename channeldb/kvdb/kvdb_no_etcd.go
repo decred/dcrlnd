lightningnetwork/lnd

etcd: refactors to simplify etcd configuration
This refactor removes a redundancy where we had etcd configuration under
kvdb and kvdb/etcd packages.

channeldb/kvdb/kvdb_no_etcd.go
@@ -5,6 +5,8 @@ package kvdb
 import (
 	"context"
 	"fmt"
+
+	"github.com/lightningnetwork/lnd/channeldb/kvdb/etcd"
 )
 
 // TestBackend is conditionally set to bdb when the kvdb_etcd build tag is
@@ -14,7 +16,7 @@ const TestBackend = BoltBackendName
 var errEtcdNotAvailable = fmt.Errorf("etcd backend not available")
 
 // GetEtcdBackend is a stub returning nil and errEtcdNotAvailable error.
-func GetEtcdBackend(ctx context.Context, etcdConfig *EtcdConfig) (
+func GetEtcdBackend(ctx context.Context, etcdConfig *etcd.Config) (
 	Backend, error) {
 
 	return nil, errEtcdNotAvailable

lncfg/db.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/lightningnetwork/lnd/channeldb/kvdb"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb/etcd"
 )
 
 const (
@@ -21,7 +22,7 @@ type DB struct {
 
 	BatchCommitInterval time.Duration `long:"batch-commit-interval" description:"The maximum duration the channel graph batch schedulers will wait before attempting to commit a batch of pending updates. This can be tradeoff database contenion for commit latency."`
 
-	Etcd *kvdb.EtcdConfig `group:"etcd" namespace:"etcd" description:"Etcd settings."`
+	Etcd *etcd.Config `group:"etcd" namespace:"etcd" description:"Etcd settings."`
 
 	Bolt *kvdb.BoltConfig `group:"bolt" namespace:"bolt" description:"Bolt settings."`
 }

decred/dcrlnd

channeldb/kvdb/kvdb_no_etcd.go
@@ -6,6 +6,8 @@ package kvdb
 import (
 	"context"
 	"fmt"
+
+	"github.com/decred/dcrlnd/channeldb/kvdb/etcd"
 )
 
 // TestBackend is conditionally set to bdb when the kvdb_etcd build tag is
@@ -15,7 +17,7 @@ const TestBackend = BoltBackendName
 var errEtcdNotAvailable = fmt.Errorf("etcd backend not available")
 
 // GetEtcdBackend is a stub returning nil and errEtcdNotAvailable error.
-func GetEtcdBackend(ctx context.Context, etcdConfig *EtcdConfig) (
+func GetEtcdBackend(ctx context.Context, etcdConfig *etcd.Config) (
 	Backend, error) {
 
 	return nil, errEtcdNotAvailable

lncfg/db.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/decred/dcrlnd/channeldb/kvdb"
+	"github.com/decred/dcrlnd/channeldb/kvdb/etcd"
 )
 
 const (
@@ -21,7 +22,7 @@ type DB struct {
 
 	BatchCommitInterval time.Duration `long:"batch-commit-interval" description:"The maximum duration the channel graph batch schedulers will wait before attempting to commit a batch of pending updates. This can be tradeoff database contenion for commit latency."`
 
-	Etcd *kvdb.EtcdConfig `group:"etcd" namespace:"etcd" description:"Etcd settings."`
+	Etcd *etcd.Config `group:"etcd" namespace:"etcd" description:"Etcd settings."`
 
 	Bolt *kvdb.BoltConfig `group:"bolt" namespace:"bolt" description:"Bolt settings."`
 }