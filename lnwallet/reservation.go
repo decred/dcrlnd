lightningnetwork/lnd

multi: replace DefaultDustLimit with script-specific DustLimitForSize
This commit updates call-sites to use the proper dust limits for
various script types. This also updates the default dust limit used
in the funding flow to be 354 satoshis instead of 573 satoshis.

lnwallet/reservation.go
@@ -205,6 +205,9 @@ func NewChannelReservation(capacity, localFundingAmt btcutil.Amount,
 		feeMSat += 2 * lnwire.NewMSatFromSatoshis(anchorSize)
 	}
 
+	// Used to cut down on verbosity.
+	defaultDust := wallet.Cfg.DefaultConstraints.DustLimit
+
 	// If we're the responder to a single-funder reservation, then we have
 	// no initial balance in the channel unless the remote party is pushing
 	// some funds to us within the first commitment state.
@@ -218,7 +221,7 @@ func NewChannelReservation(capacity, localFundingAmt btcutil.Amount,
 		if int64(theirBalance) < 0 {
 			return nil, ErrFunderBalanceDust(
 				int64(commitFee), int64(theirBalance.ToSatoshis()),
-				int64(2*DefaultDustLimit()),
+				int64(2*defaultDust),
 			)
 		}
 	} else {
@@ -247,7 +250,7 @@ func NewChannelReservation(capacity, localFundingAmt btcutil.Amount,
 		if int64(ourBalance) < 0 {
 			return nil, ErrFunderBalanceDust(
 				int64(commitFee), int64(ourBalance),
-				int64(2*DefaultDustLimit()),
+				int64(2*defaultDust),
 			)
 		}
 	}
@@ -257,21 +260,21 @@ func NewChannelReservation(capacity, localFundingAmt btcutil.Amount,
 	// reject this channel creation request.
 	//
 	// TODO(roasbeef): reject if 30% goes to fees? dust channel
-	if initiator && ourBalance.ToSatoshis() <= 2*DefaultDustLimit() {
+	if initiator && ourBalance.ToSatoshis() <= 2*defaultDust {
 		return nil, ErrFunderBalanceDust(
 			int64(commitFee),
 			int64(ourBalance.ToSatoshis()),
-			int64(2*DefaultDustLimit()),
+			int64(2*defaultDust),
 		)
 	}
 
 	// Similarly we ensure their balance is reasonable if we are not the
 	// initiator.
-	if !initiator && theirBalance.ToSatoshis() <= 2*DefaultDustLimit() {
+	if !initiator && theirBalance.ToSatoshis() <= 2*defaultDust {
 		return nil, ErrFunderBalanceDust(
 			int64(commitFee),
 			int64(theirBalance.ToSatoshis()),
-			int64(2*DefaultDustLimit()),
+			int64(2*defaultDust),
 		)
 	}
 

watchtower/wtpolicy/policy.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/lnwallet"
 	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
 	"github.com/lightningnetwork/lnd/watchtower/blob"
@@ -165,10 +166,9 @@ func (p *Policy) ComputeAltruistOutput(totalAmt btcutil.Amount,
 	sweepAmt := totalAmt - txFee
 
 	// TODO(conner): replace w/ configurable dust limit
-	dustLimit := lnwallet.DefaultDustLimit()
-
-	// Check that the created outputs won't be dusty.
-	if sweepAmt <= dustLimit {
+	// Check that the created outputs won't be dusty. The sweep pkscript is
+	// currently a p2wpkh, so we'll use that script's dust limit.
+	if sweepAmt < lnwallet.DustLimitForSize(input.P2WPKHSize) {
 		return 0, ErrCreatesDust
 	}
 
@@ -199,10 +199,9 @@ func (p *Policy) ComputeRewardOutputs(totalAmt btcutil.Amount,
 	sweepAmt := totalAmt - rewardAmt - txFee
 
 	// TODO(conner): replace w/ configurable dust limit
-	dustLimit := lnwallet.DefaultDustLimit()
-
-	// Check that the created outputs won't be dusty.
-	if sweepAmt <= dustLimit {
+	// Check that the created outputs won't be dusty. The sweep pkscript is
+	// currently a p2wpkh, so we'll use that script's dust limit.
+	if sweepAmt < lnwallet.DustLimitForSize(input.P2WPKHSize) {
 		return 0, 0, ErrCreatesDust
 	}
 

decred/dcrlnd

lnwallet/reservation.go
@@ -205,6 +205,9 @@ func NewChannelReservation(capacity, localFundingAmt dcrutil.Amount,
 		feeMAtoms += 2 * lnwire.NewMAtomsFromAtoms(anchorSize)
 	}
 
+	// Used to cut down on verbosity.
+	defaultDust := wallet.Cfg.DefaultConstraints.DustLimit
+
 	// If we're the responder to a single-funder reservation, then we have
 	// no initial balance in the channel unless the remote party is pushing
 	// some funds to us within the first commitment state.
@@ -218,7 +221,7 @@ func NewChannelReservation(capacity, localFundingAmt dcrutil.Amount,
 		if int64(theirBalance) < 0 {
 			return nil, ErrFunderBalanceDust(
 				int64(commitFee), int64(theirBalance.ToAtoms()),
-				int64(2*DefaultDustLimit()),
+				int64(2*defaultDust),
 			)
 		}
 	} else {
@@ -247,7 +250,7 @@ func NewChannelReservation(capacity, localFundingAmt dcrutil.Amount,
 		if int64(ourBalance) < 0 {
 			return nil, ErrFunderBalanceDust(
 				int64(commitFee), int64(ourBalance),
-				int64(2*DefaultDustLimit()),
+				int64(2*defaultDust),
 			)
 		}
 	}
@@ -257,21 +260,21 @@ func NewChannelReservation(capacity, localFundingAmt dcrutil.Amount,
 	// reject this channel creation request.
 	//
 	// TODO(roasbeef): reject if 30% goes to fees? dust channel
-	if initiator && ourBalance.ToAtoms() <= 2*DefaultDustLimit() {
+	if initiator && ourBalance.ToAtoms() <= 2*defaultDust {
 		return nil, ErrFunderBalanceDust(
 			int64(commitFee),
 			int64(ourBalance.ToAtoms()),
-			int64(2*DefaultDustLimit()),
+			int64(2*defaultDust),
 		)
 	}
 
 	// Similarly we ensure their balance is reasonable if we are not the
 	// initiator.
-	if !initiator && theirBalance.ToAtoms() <= 2*DefaultDustLimit() {
+	if !initiator && theirBalance.ToAtoms() <= 2*defaultDust {
 		return nil, ErrFunderBalanceDust(
 			int64(commitFee),
 			int64(theirBalance.ToAtoms()),
-			int64(2*DefaultDustLimit()),
+			int64(2*defaultDust),
 		)
 	}
 

watchtower/wtpolicy/policy.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/decred/dcrd/dcrutil/v4"
 	"github.com/decred/dcrd/wire"
+	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnwallet"
 	"github.com/decred/dcrlnd/lnwallet/chainfee"
 	"github.com/decred/dcrlnd/watchtower/blob"
@@ -167,10 +168,9 @@ func (p *Policy) ComputeAltruistOutput(totalAmt dcrutil.Amount,
 	sweepAmt := totalAmt - txFee
 
 	// TODO(conner): replace w/ configurable dust limit
-	dustLimit := lnwallet.DefaultDustLimit()
-
-	// Check that the created outputs won't be dusty.
-	if sweepAmt <= dustLimit {
+	// Check that the created outputs won't be dusty. The sweep pkscript is
+	// currently a p2pkh, so we'll use that script's dust limit.
+	if sweepAmt < lnwallet.DustLimitForSize(input.P2PKHPkScriptSize) {
 		return 0, ErrCreatesDust
 	}
 
@@ -201,10 +201,9 @@ func (p *Policy) ComputeRewardOutputs(totalAmt dcrutil.Amount,
 	sweepAmt := totalAmt - rewardAmt - txFee
 
 	// TODO(conner): replace w/ configurable dust limit
-	dustLimit := lnwallet.DefaultDustLimit()
-
-	// Check that the created outputs won't be dusty.
-	if sweepAmt <= dustLimit {
+	// Check that the created outputs won't be dusty. The sweep pkscript is
+	// currently a p2pkh, so we'll use that script's dust limit.
+	if sweepAmt < lnwallet.DustLimitForSize(input.P2PKHPkScriptSize) {
 		return 0, 0, ErrCreatesDust
 	}
 