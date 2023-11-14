lightningnetwork/lnd

routing: add private key attribute in router test

routing/router_test.go
@@ -35,6 +35,8 @@ type testCtx struct {
 
 	aliases map[string]route.Vertex
 
+	privKeys map[string]*btcec.PrivateKey
+
 	chain *mockChain
 
 	chainView *mockChainView
@@ -151,6 +153,7 @@ func createTestCtxFromGraphInstanceAssumeValid(startingHeight uint32,
 		router:    router,
 		graph:     graphInstance.graph,
 		aliases:   graphInstance.aliasMap,
+		privKeys:  graphInstance.privKeyMap,
 		chain:     chain,
 		chainView: chainView,
 	}

decred/dcrlnd

routing/router_test.go
@@ -36,6 +36,8 @@ type testCtx struct {
 
 	aliases map[string]route.Vertex
 
+	privKeys map[string]*secp256k1.PrivateKey
+
 	chain *mockChain
 
 	chainView *mockChainView
@@ -152,6 +154,7 @@ func createTestCtxFromGraphInstanceAssumeValid(startingHeight uint32,
 		router:    router,
 		graph:     graphInstance.graph,
 		aliases:   graphInstance.aliasMap,
+		privKeys:  graphInstance.privKeyMap,
 		chain:     chain,
 		chainView: chainView,
 	}