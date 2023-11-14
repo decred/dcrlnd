lightningnetwork/lnd

routerrpc: add mission control import

lnrpc/routerrpc/router_server.go
@@ -81,6 +81,10 @@ var (
 			Entity: "offchain",
 			Action: "read",
 		}},
+		"/routerrpc.Router/XImportMissionControl": {{
+			Entity: "offchain",
+			Action: "write",
+		}},
 		"/routerrpc.Router/GetMissionControlConfig": {{
 			Entity: "offchain",
 			Action: "read",
@@ -517,6 +521,159 @@ func toRPCPairData(data *routing.TimedPairResult) *PairData {
 	}
 
 	return &rpcData
+}
+
+// XImportMissionControl imports the state provided to our internal mission
+// control. Only entries that are fresher than our existing state will be used.
+func (s *Server) XImportMissionControl(ctx context.Context,
+	req *XImportMissionControlRequest) (*XImportMissionControlResponse,
+	error) {
+
+	if len(req.Pairs) == 0 {
+		return nil, errors.New("at least one pair required for import")
+	}
+
+	snapshot := &routing.MissionControlSnapshot{
+		Pairs: make(
+			[]routing.MissionControlPairSnapshot, len(req.Pairs),
+		),
+	}
+
+	for i, pairResult := range req.Pairs {
+		pairSnapshot, err := toPairSnapshot(pairResult)
+		if err != nil {
+			return nil, err
+		}
+
+		snapshot.Pairs[i] = *pairSnapshot
+	}
+
+	err := s.cfg.RouterBackend.MissionControl.ImportHistory(snapshot)
+	if err != nil {
+		return nil, err
+	}
+
+	return &XImportMissionControlResponse{}, nil
+}
+
+func toPairSnapshot(pairResult *PairHistory) (*routing.MissionControlPairSnapshot,
+	error) {
+
+	from, err := route.NewVertexFromBytes(pairResult.NodeFrom)
+	if err != nil {
+		return nil, err
+	}
+
+	to, err := route.NewVertexFromBytes(pairResult.NodeTo)
+	if err != nil {
+		return nil, err
+	}
+
+	pairPrefix := fmt.Sprintf("pair: %v -> %v:", from, to)
+
+	if from == to {
+		return nil, fmt.Errorf("%v source and destination node must "+
+			"differ", pairPrefix)
+	}
+
+	failAmt, failTime, err := getPair(
+		lnwire.MilliSatoshi(pairResult.History.FailAmtMsat),
+		btcutil.Amount(pairResult.History.FailAmtSat),
+		pairResult.History.FailTime,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("%v invalid failure: %v", pairPrefix,
+			err)
+	}
+
+	successAmt, successTime, err := getPair(
+		lnwire.MilliSatoshi(pairResult.History.SuccessAmtMsat),
+		btcutil.Amount(pairResult.History.SuccessAmtSat),
+		pairResult.History.SuccessTime,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("%v invalid success: %v", pairPrefix,
+			err)
+	}
+
+	if successAmt == 0 && failAmt == 0 {
+		return nil, fmt.Errorf("%v: either success or failure result "+
+			"required", pairPrefix)
+	}
+
+	pair := routing.NewDirectedNodePair(from, to)
+
+	result := &routing.TimedPairResult{
+		FailAmt:     failAmt,
+		FailTime:    failTime,
+		SuccessAmt:  successAmt,
+		SuccessTime: successTime,
+	}
+
+	return &routing.MissionControlPairSnapshot{
+		Pair:            pair,
+		TimedPairResult: *result,
+	}, nil
+}
+
+// getPair validates the values provided for a mission control result and
+// returns the msat amount and timestamp for it.
+func getPair(amtMsat lnwire.MilliSatoshi, amtSat btcutil.Amount,
+	timestamp int64) (lnwire.MilliSatoshi, time.Time, error) {
+
+	amt, err := getMsatPairValue(amtMsat, amtSat)
+	if err != nil {
+		return 0, time.Time{}, err
+	}
+
+	var (
+		timeSet   = timestamp != 0
+		amountSet = amt != 0
+	)
+
+	switch {
+	case timeSet && amountSet:
+		return amt, time.Unix(timestamp, 0), nil
+
+	case timeSet && !amountSet:
+		return 0, time.Time{}, errors.New("non-zero timestamp " +
+			"requires non-zero amount")
+
+	case !timeSet && amountSet:
+		return 0, time.Time{}, errors.New("non-zero amount requires " +
+			"non-zero timestamp")
+
+	default:
+		return 0, time.Time{}, nil
+	}
+}
+
+// getMsatPairValue checks the msat and sat values set for a pair and ensures
+// that the values provided are either the same, or only a single value is set.
+func getMsatPairValue(msatValue lnwire.MilliSatoshi,
+	satValue btcutil.Amount) (lnwire.MilliSatoshi, error) {
+
+	// If our msat value converted to sats equals our sat value, we just
+	// return the msat value, since the values are the same.
+	if msatValue.ToSatoshis() == satValue {
+		return msatValue, nil
+	}
+
+	// If we have no msatValue, we can just return our sate value even if
+	// it is zero, because it's impossible that we have mismatched values.
+	if msatValue == 0 {
+		return lnwire.MilliSatoshi(satValue * 1000), nil
+	}
+
+	// Likewise, we can just use msat value if we have no sat value set.
+	if satValue == 0 {
+		return msatValue, nil
+	}
+
+	// If our values are non-zero but not equal, we have invalid amounts
+	// set, so we fail.
+	return 0, fmt.Errorf("msat: %v and sat: %v values not equal", msatValue,
+		satValue)
 }
 
 // QueryProbability returns the current success probability estimate for a

lntest/itest/lnd_test.go
@@ -13116,8 +13116,12 @@ func testQueryRoutes(net *lntest.NetworkHarness, t *harnessTest) {
 	}
 
 	// While we're here, we test updating mission control's config values
-	// and assert that they are correctly updated.
+	// and assert that they are correctly updated and check that our mission
+	// control import function updates appropriately.
 	testMissionControlCfg(t.t, net.Alice)
+	testMissionControlImport(
+		t.t, net.Alice, net.Bob.PubKey[:], carol.PubKey[:],
+	)
 
 	// We clean up the test case by closing channels that were created for
 	// the duration of the tests.
@@ -13172,6 +13176,68 @@ func testMissionControlCfg(t *testing.T, node *lntest.HarnessNode) {
 		},
 	)
 	require.NoError(t, err)
+}
+
+// testMissionControlImport tests import of mission control results from an
+// external source.
+func testMissionControlImport(t *testing.T, node *lntest.HarnessNode,
+	fromNode, toNode []byte) {
+
+	ctxb := context.Background()
+
+	// Reset mission control so that our query will return the default
+	// probability for our first request.
+	_, err := node.RouterClient.ResetMissionControl(
+		ctxb, &routerrpc.ResetMissionControlRequest{},
+	)
+	require.NoError(t, err, "could not reset mission control")
+
+	// Get our baseline probability for a 10 msat hop between our target
+	// nodes.
+	var amount int64 = 10
+	probReq := &routerrpc.QueryProbabilityRequest{
+		FromNode: fromNode,
+		ToNode:   toNode,
+		AmtMsat:  amount,
+	}
+
+	importHistory := &routerrpc.PairData{
+		FailTime:    time.Now().Unix(),
+		FailAmtMsat: amount,
+	}
+
+	// Assert that our history is not already equal to the value we want to
+	// set. This should not happen because we have just cleared our state.
+	resp1, err := node.RouterClient.QueryProbability(ctxb, probReq)
+	require.NoError(t, err, "query probability failed")
+	require.Zero(t, resp1.History.FailTime)
+	require.Zero(t, resp1.History.FailAmtMsat)
+
+	// Now, we import a single entry which tracks a failure of the amount
+	// we want to query between our nodes.
+	req := &routerrpc.XImportMissionControlRequest{
+		Pairs: []*routerrpc.PairHistory{
+			{
+				NodeFrom: fromNode,
+				NodeTo:   toNode,
+				History:  importHistory,
+			},
+		},
+	}
+
+	_, err = node.RouterClient.XImportMissionControl(ctxb, req)
+	require.NoError(t, err, "could not import config")
+
+	resp2, err := node.RouterClient.QueryProbability(ctxb, probReq)
+	require.NoError(t, err, "query probability failed")
+	require.Equal(t, importHistory.FailTime, resp2.History.FailTime)
+	require.Equal(t, importHistory.FailAmtMsat, resp2.History.FailAmtMsat)
+
+	// Finally, check that we will fail if inconsistent sat/msat values are
+	// set.
+	importHistory.FailAmtSat = amount * 2
+	_, err = node.RouterClient.XImportMissionControl(ctxb, req)
+	require.Error(t, err, "mismatched import amounts succeeded")
 }
 
 // testRouteFeeCutoff tests that we are able to prevent querying routes and

decred/dcrlnd

lnrpc/routerrpc/router_server.go
@@ -81,6 +81,10 @@ var (
 			Entity: "offchain",
 			Action: "read",
 		}},
+		"/routerrpc.Router/XImportMissionControl": {{
+			Entity: "offchain",
+			Action: "write",
+		}},
 		"/routerrpc.Router/GetMissionControlConfig": {{
 			Entity: "offchain",
 			Action: "read",
@@ -517,6 +521,159 @@ func toRPCPairData(data *routing.TimedPairResult) *PairData {
 	}
 
 	return &rpcData
+}
+
+// XImportMissionControl imports the state provided to our internal mission
+// control. Only entries that are fresher than our existing state will be used.
+func (s *Server) XImportMissionControl(ctx context.Context,
+	req *XImportMissionControlRequest) (*XImportMissionControlResponse,
+	error) {
+
+	if len(req.Pairs) == 0 {
+		return nil, errors.New("at least one pair required for import")
+	}
+
+	snapshot := &routing.MissionControlSnapshot{
+		Pairs: make(
+			[]routing.MissionControlPairSnapshot, len(req.Pairs),
+		),
+	}
+
+	for i, pairResult := range req.Pairs {
+		pairSnapshot, err := toPairSnapshot(pairResult)
+		if err != nil {
+			return nil, err
+		}
+
+		snapshot.Pairs[i] = *pairSnapshot
+	}
+
+	err := s.cfg.RouterBackend.MissionControl.ImportHistory(snapshot)
+	if err != nil {
+		return nil, err
+	}
+
+	return &XImportMissionControlResponse{}, nil
+}
+
+func toPairSnapshot(pairResult *PairHistory) (*routing.MissionControlPairSnapshot,
+	error) {
+
+	from, err := route.NewVertexFromBytes(pairResult.NodeFrom)
+	if err != nil {
+		return nil, err
+	}
+
+	to, err := route.NewVertexFromBytes(pairResult.NodeTo)
+	if err != nil {
+		return nil, err
+	}
+
+	pairPrefix := fmt.Sprintf("pair: %v -> %v:", from, to)
+
+	if from == to {
+		return nil, fmt.Errorf("%v source and destination node must "+
+			"differ", pairPrefix)
+	}
+
+	failAmt, failTime, err := getPair(
+		lnwire.MilliAtom(pairResult.History.FailAmtMAtoms),
+		dcrutil.Amount(pairResult.History.FailAmtAtoms),
+		pairResult.History.FailTime,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("%v invalid failure: %v", pairPrefix,
+			err)
+	}
+
+	successAmt, successTime, err := getPair(
+		lnwire.MilliAtom(pairResult.History.SuccessAmtMAtoms),
+		dcrutil.Amount(pairResult.History.SuccessAmtAtoms),
+		pairResult.History.SuccessTime,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("%v invalid success: %v", pairPrefix,
+			err)
+	}
+
+	if successAmt == 0 && failAmt == 0 {
+		return nil, fmt.Errorf("%v: either success or failure result "+
+			"required", pairPrefix)
+	}
+
+	pair := routing.NewDirectedNodePair(from, to)
+
+	result := &routing.TimedPairResult{
+		FailAmt:     failAmt,
+		FailTime:    failTime,
+		SuccessAmt:  successAmt,
+		SuccessTime: successTime,
+	}
+
+	return &routing.MissionControlPairSnapshot{
+		Pair:            pair,
+		TimedPairResult: *result,
+	}, nil
+}
+
+// getPair validates the values provided for a mission control result and
+// returns the msat amount and timestamp for it.
+func getPair(amtMAtoms lnwire.MilliAtom, amtAtoms dcrutil.Amount,
+	timestamp int64) (lnwire.MilliAtom, time.Time, error) {
+
+	amt, err := getMsatPairValue(amtMAtoms, amtAtoms)
+	if err != nil {
+		return 0, time.Time{}, err
+	}
+
+	var (
+		timeSet   = timestamp != 0
+		amountSet = amt != 0
+	)
+
+	switch {
+	case timeSet && amountSet:
+		return amt, time.Unix(timestamp, 0), nil
+
+	case timeSet && !amountSet:
+		return 0, time.Time{}, errors.New("non-zero timestamp " +
+			"requires non-zero amount")
+
+	case !timeSet && amountSet:
+		return 0, time.Time{}, errors.New("non-zero amount requires " +
+			"non-zero timestamp")
+
+	default:
+		return 0, time.Time{}, nil
+	}
+}
+
+// getMsatPairValue checks the msat and sat values set for a pair and ensures
+// that the values provided are either the same, or only a single value is set.
+func getMsatPairValue(msatValue lnwire.MilliAtom,
+	satValue dcrutil.Amount) (lnwire.MilliAtom, error) {
+
+	// If our msat value converted to sats equals our sat value, we just
+	// return the msat value, since the values are the same.
+	if msatValue.ToAtoms() == satValue {
+		return msatValue, nil
+	}
+
+	// If we have no msatValue, we can just return our sate value even if
+	// it is zero, because it's impossible that we have mismatched values.
+	if msatValue == 0 {
+		return lnwire.MilliAtom(satValue * 1000), nil
+	}
+
+	// Likewise, we can just use msat value if we have no sat value set.
+	if satValue == 0 {
+		return msatValue, nil
+	}
+
+	// If our values are non-zero but not equal, we have invalid amounts
+	// set, so we fail.
+	return 0, fmt.Errorf("msat: %v and sat: %v values not equal", msatValue,
+		satValue)
 }
 
 // QueryProbability returns the current success probability estimate for a

lntest/itest/lnd_test.go
@@ -14127,8 +14127,12 @@ func testQueryRoutes(net *lntest.NetworkHarness, t *harnessTest) {
 	}
 
 	// While we're here, we test updating mission control's config values
-	// and assert that they are correctly updated.
+	// and assert that they are correctly updated and check that our mission
+	// control import function updates appropriately.
 	testMissionControlCfg(t.t, net.Alice)
+	testMissionControlImport(
+		t.t, net.Alice, net.Bob.PubKey[:], carol.PubKey[:],
+	)
 
 	// We clean up the test case by closing channels that were created for
 	// the duration of the tests.
@@ -14177,6 +14181,68 @@ func testMissionControlCfg(t *testing.T, node *lntest.HarnessNode) {
 		},
 	)
 	require.NoError(t, err)
+}
+
+// testMissionControlImport tests import of mission control results from an
+// external source.
+func testMissionControlImport(t *testing.T, node *lntest.HarnessNode,
+	fromNode, toNode []byte) {
+
+	ctxb := context.Background()
+
+	// Reset mission control so that our query will return the default
+	// probability for our first request.
+	_, err := node.RouterClient.ResetMissionControl(
+		ctxb, &routerrpc.ResetMissionControlRequest{},
+	)
+	require.NoError(t, err, "could not reset mission control")
+
+	// Get our baseline probability for a 10 msat hop between our target
+	// nodes.
+	var amount int64 = 10
+	probReq := &routerrpc.QueryProbabilityRequest{
+		FromNode:  fromNode,
+		ToNode:    toNode,
+		AmtMAtoms: amount,
+	}
+
+	importHistory := &routerrpc.PairData{
+		FailTime:      time.Now().Unix(),
+		FailAmtMAtoms: amount,
+	}
+
+	// Assert that our history is not already equal to the value we want to
+	// set. This should not happen because we have just cleared our state.
+	resp1, err := node.RouterClient.QueryProbability(ctxb, probReq)
+	require.NoError(t, err, "query probability failed")
+	require.Zero(t, resp1.History.FailTime)
+	require.Zero(t, resp1.History.FailAmtMAtoms)
+
+	// Now, we import a single entry which tracks a failure of the amount
+	// we want to query between our nodes.
+	req := &routerrpc.XImportMissionControlRequest{
+		Pairs: []*routerrpc.PairHistory{
+			{
+				NodeFrom: fromNode,
+				NodeTo:   toNode,
+				History:  importHistory,
+			},
+		},
+	}
+
+	_, err = node.RouterClient.XImportMissionControl(ctxb, req)
+	require.NoError(t, err, "could not import config")
+
+	resp2, err := node.RouterClient.QueryProbability(ctxb, probReq)
+	require.NoError(t, err, "query probability failed")
+	require.Equal(t, importHistory.FailTime, resp2.History.FailTime)
+	require.Equal(t, importHistory.FailAmtMAtoms, resp2.History.FailAmtMAtoms)
+
+	// Finally, check that we will fail if inconsistent sat/msat values are
+	// set.
+	importHistory.FailAmtAtoms = amount * 2
+	_, err = node.RouterClient.XImportMissionControl(ctxb, req)
+	require.Error(t, err, "mismatched import amounts succeeded")
 }
 
 // testRouteFeeCutoff tests that we are able to prevent querying routes and