lightningnetwork/lnd

rpc: minConfs and spendUnconfirmed for EstimateFee

lntest/mock/walletcontroller.go
@@ -107,7 +107,7 @@ func (w *WalletController) SendOutputs(outputs []*wire.TxOut,
 
 // CreateSimpleTx currently returns dummy values.
 func (w *WalletController) CreateSimpleTx(outputs []*wire.TxOut,
-	_ chainfee.SatPerKWeight, _ bool) (*txauthor.AuthoredTx, error) {
+	_ chainfee.SatPerKWeight, _ int32, _ bool) (*txauthor.AuthoredTx, error) {
 
 	return nil, nil
 }

lnwallet/test/test_interface.go
@@ -2547,6 +2547,8 @@ func testLastUnusedAddr(miner *rpctest.Harness,
 // testCreateSimpleTx checks that a call to CreateSimpleTx will return a
 // transaction that is equal to the one that is being created by SendOutputs in
 // a subsequent call.
+// All test cases are doubled-up: one for testing unconfirmed inputs,
+// one for testing only confirmed inputs.
 func testCreateSimpleTx(r *rpctest.Harness, w *lnwallet.LightningWallet,
 	_ *lnwallet.LightningWallet, t *testing.T) {
 
@@ -2558,51 +2560,108 @@ func testCreateSimpleTx(r *rpctest.Harness, w *lnwallet.LightningWallet,
 
 	// The test cases we will run through for all backends.
 	testCases := []struct {
-		outVals []int64
-		feeRate chainfee.SatPerKWeight
-		valid   bool
+		outVals     []int64
+		feeRate     chainfee.SatPerKWeight
+		valid       bool
+		unconfirmed bool
 	}{
 		{
-			outVals: []int64{},
-			feeRate: 2500,
-			valid:   false, // No outputs.
+			outVals:     []int64{},
+			feeRate:     2500,
+			valid:       false, // No outputs.
+			unconfirmed: false,
+		},
+		{
+			outVals:     []int64{},
+			feeRate:     2500,
+			valid:       false, // No outputs.
+			unconfirmed: true,
 		},
 
 		{
-			outVals: []int64{200},
-			feeRate: 2500,
-			valid:   false, // Dust output.
+			outVals:     []int64{200},
+			feeRate:     2500,
+			valid:       false, // Dust output.
+			unconfirmed: false,
+		},
+		{
+			outVals:     []int64{200},
+			feeRate:     2500,
+			valid:       false, // Dust output.
+			unconfirmed: true,
 		},
 
 		{
-			outVals: []int64{1e8},
-			feeRate: 2500,
-			valid:   true,
+			outVals:     []int64{1e8},
+			feeRate:     2500,
+			valid:       true,
+			unconfirmed: false,
 		},
 		{
-			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5},
-			feeRate: 2500,
-			valid:   true,
+			outVals:     []int64{1e8},
+			feeRate:     2500,
+			valid:       true,
+			unconfirmed: true,
+		},
+
+		{
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     2500,
+			valid:       true,
+			unconfirmed: false,
 		},
 		{
-			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5},
-			feeRate: 12500,
-			valid:   true,
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     2500,
+			valid:       true,
+			unconfirmed: true,
+		},
+
+		{
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     12500,
+			valid:       true,
+			unconfirmed: false,
 		},
 		{
-			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5},
-			feeRate: 50000,
-			valid:   true,
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     12500,
+			valid:       true,
+			unconfirmed: true,
+		},
+
+		{
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     50000,
+			valid:       true,
+			unconfirmed: false,
+		},
+		{
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     50000,
+			valid:       true,
+			unconfirmed: true,
+		},
+
+		{
+			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5, 1e8, 2e8,
+				1e8, 2e7, 3e5},
+			feeRate:     44250,
+			valid:       true,
+			unconfirmed: false,
 		},
 		{
 			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5, 1e8, 2e8,
 				1e8, 2e7, 3e5},
-			feeRate: 44250,
-			valid:   true,
+			feeRate:     44250,
+			valid:       true,
+			unconfirmed: true,
 		},
 	}
 
 	for i, test := range testCases {
+		var minConfs int32 = 1
+
 		feeRate := test.feeRate
 
 		// Grab some fresh addresses from the miner that we will send
@@ -2629,7 +2688,7 @@ func testCreateSimpleTx(r *rpctest.Harness, w *lnwallet.LightningWallet,
 
 		// Now try creating a tx spending to these outputs.
 		createTx, createErr := w.CreateSimpleTx(
-			outputs, feeRate, true,
+			outputs, feeRate, minConfs, true,
 		)
 		switch {
 		case test.valid && createErr != nil:
@@ -2646,7 +2705,7 @@ func testCreateSimpleTx(r *rpctest.Harness, w *lnwallet.LightningWallet,
 		// _very_ similar to the one we just created being sent. The
 		// only difference is that the dry run tx is not signed, and
 		// that the change output position might be different.
-		tx, sendErr := w.SendOutputs(outputs, feeRate, 1, labels.External)
+		tx, sendErr := w.SendOutputs(outputs, feeRate, minConfs, labels.External)
 		switch {
 		case test.valid && sendErr != nil:
 			t.Fatalf("got unexpected error when sending tx: %v",

rpcserver.go
@@ -975,7 +975,7 @@ func (r *rpcServer) sendCoinsOnChain(paymentMap map[string]int64,
 	// We first do a dry run, to sanity check we won't spend our wallet
 	// balance below the reserved amount.
 	authoredTx, err := r.server.cc.Wallet.CreateSimpleTx(
-		outputs, feeRate, true,
+		outputs, feeRate, minConfs, true,
 	)
 	if err != nil {
 		return nil, err
@@ -1073,12 +1073,21 @@ func (r *rpcServer) EstimateFee(ctx context.Context,
 		return nil, err
 	}
 
+	// Then, we'll extract the minimum number of confirmations that each
+	// output we use to fund the transaction should satisfy.
+	minConfs, err := lnrpc.ExtractMinConfs(
+		in.GetMinConfs(), in.GetSpendUnconfirmed(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	// We will ask the wallet to create a tx using this fee rate. We set
 	// dryRun=true to avoid inflating the change addresses in the db.
 	var tx *txauthor.AuthoredTx
 	wallet := r.server.cc.Wallet
 	err = wallet.WithCoinSelectLock(func() error {
-		tx, err = wallet.CreateSimpleTx(outputs, feePerKw, true)
+		tx, err = wallet.CreateSimpleTx(outputs, feePerKw, minConfs, true)
 		return err
 	})
 	if err != nil {

decred/dcrlnd

lntest/mock/walletcontroller.go
@@ -104,7 +104,7 @@ func (w *WalletController) SendOutputs(outputs []*wire.TxOut,
 
 // CreateSimpleTx currently returns dummy values.
 func (w *WalletController) CreateSimpleTx(outputs []*wire.TxOut,
-	_ chainfee.AtomPerKByte, _ bool) (*txauthor.AuthoredTx, error) {
+	_ chainfee.AtomPerKByte, _ int32, _ bool) (*txauthor.AuthoredTx, error) {
 
 	return nil, nil
 }

lnwallet/test/test_interface.go
@@ -2675,52 +2675,107 @@ func testCreateSimpleTx(r *rpctest.Harness, // nolint: unused
 
 	// The test cases we will run through for all backends.
 	testCases := []struct {
-		outVals []int64
-		feeRate chainfee.AtomPerKByte
-		valid   bool
+		outVals     []int64
+		feeRate     chainfee.AtomPerKByte
+		valid       bool
+		unconfirmed bool
 	}{
 		{
-			outVals: []int64{},
-			feeRate: 2500,
-			valid:   false, // No outputs.
+			outVals:     []int64{},
+			feeRate:     2500,
+			valid:       false, // No outputs.
+			unconfirmed: false,
+		},
+		{
+			outVals:     []int64{},
+			feeRate:     2500,
+			valid:       false, // No outputs.
+			unconfirmed: true,
 		},
 
 		{
-			outVals: []int64{1e3},
-			feeRate: 2500,
-			valid:   false, // Dust output.
+			outVals:     []int64{1e3},
+			feeRate:     2500,
+			valid:       false, // Dust output.
+			unconfirmed: false,
+		},
+		{
+			outVals:     []int64{1e3},
+			feeRate:     2500,
+			valid:       false, // Dust output.
+			unconfirmed: true,
+		},
+		{
+			outVals:     []int64{1e8},
+			feeRate:     2500,
+			valid:       true,
+			unconfirmed: false,
+		},
+		{
+			outVals:     []int64{1e8},
+			feeRate:     2500,
+			valid:       true,
+			unconfirmed: true,
 		},
 
 		{
-			outVals: []int64{1e8},
-			feeRate: 2500,
-			valid:   true,
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     2500,
+			valid:       true,
+			unconfirmed: false,
 		},
 		{
-			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5},
-			feeRate: 2500,
-			valid:   true,
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     2500,
+			valid:       true,
+			unconfirmed: true,
+		},
+
+		{
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     12500,
+			valid:       true,
+			unconfirmed: false,
 		},
 		{
-			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5},
-			feeRate: 12500,
-			valid:   true,
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     12500,
+			valid:       true,
+			unconfirmed: true,
+		},
+
+		{
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     50000,
+			valid:       true,
+			unconfirmed: false,
 		},
 		{
-			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5},
-			feeRate: 50000,
-			valid:   true,
+			outVals:     []int64{1e8, 2e8, 1e8, 2e7, 3e5},
+			feeRate:     50000,
+			valid:       true,
+			unconfirmed: true,
+		},
+
+		{
+			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5, 1e8, 2e8,
+				1e8, 2e7, 3e5},
+			feeRate:     44250,
+			valid:       true,
+			unconfirmed: false,
 		},
 		{
 			outVals: []int64{1e8, 2e8, 1e8, 2e7, 3e5, 1e8, 2e8,
 				1e8, 2e7, 3e5},
-			feeRate: 44250,
-			valid:   true,
+			feeRate:     44250,
+			valid:       true,
+			unconfirmed: true,
 		},
 	}
 
 	ctxb := context.Background()
 	for _, test := range testCases {
+		var minConfs int32 = 1
 		feeRate := test.feeRate
 
 		// Grab some fresh addresses from the miner that we will send
@@ -2747,7 +2802,7 @@ func testCreateSimpleTx(r *rpctest.Harness, // nolint: unused
 
 		// Now try creating a tx spending to these outputs.
 		createTx, createErr := w.CreateSimpleTx(
-			outputs, feeRate, true,
+			outputs, feeRate, minConfs, true,
 		)
 		if test.valid == (createErr != nil) {
 			fmt.Println(spew.Sdump(createTx.Tx))
@@ -2759,7 +2814,7 @@ func testCreateSimpleTx(r *rpctest.Harness, // nolint: unused
 		// _very_ similar to the one we just created being sent. The
 		// only difference is that the dry run tx is not signed, and
 		// that the change output position might be different.
-		tx, sendErr := w.SendOutputs(outputs, feeRate, 1, labels.External, "")
+		tx, sendErr := w.SendOutputs(outputs, feeRate, minConfs, labels.External, "")
 		if test.valid == (sendErr != nil) {
 			t.Fatalf("got unexpected error when sending tx: %v",
 				sendErr)

rpcserver.go
@@ -976,7 +976,7 @@ func (r *rpcServer) sendCoinsOnChain(paymentMap map[string]int64,
 	// We first do a dry run, to sanity check we won't spend our wallet
 	// balance below the reserved amount.
 	// 	authoredTx, err := r.server.cc.Wallet.CreateSimpleTx(
-	// 		outputs, feeRate, true,
+	// 		outputs, feeRate, minConfs, true,
 	// 	)
 	// 	if err != nil {
 	// 		return nil, err
@@ -1074,12 +1074,21 @@ func (r *rpcServer) EstimateFee(ctx context.Context,
 		return nil, err
 	}
 
+	// Then, we'll extract the minimum number of confirmations that each
+	// output we use to fund the transaction should satisfy.
+	minConfs, err := lnrpc.ExtractMinConfs(
+		in.GetMinConfs(), in.GetSpendUnconfirmed(),
+	)
+	if err != nil {
+		return nil, err
+	}
+
 	// We will ask the wallet to create a tx using this fee rate. We set
 	// dryRun=true to avoid inflating the change addresses in the db.
 	var tx *txauthor.AuthoredTx
 	wallet := r.server.cc.Wallet
 	err = wallet.WithCoinSelectLock(func() error {
-		tx, err = wallet.CreateSimpleTx(outputs, feePerKB, true)
+		tx, err = wallet.CreateSimpleTx(outputs, feePerKB, minConfs, true)
 		return err
 	})
 	if err != nil {