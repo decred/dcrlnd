lightningnetwork/lnd

walletrpc: add ListLeases

lntest/mock/walletcontroller.go
@@ -142,6 +142,10 @@ func (w *WalletController) ReleaseOutput(wtxmgr.LockID, wire.OutPoint) error {
 	return nil
 }
 
+func (w *WalletController) ListLeasedOutputs() ([]*wtxmgr.LockedOutput, error) {
+	return nil, nil
+}
+
 // FundPsbt currently does nothing.
 func (w *WalletController) FundPsbt(_ *psbt.Packet,
 	_ chainfee.SatPerKWeight) (int32, error) {

decred/dcrlnd

lntest/mock/walletcontroller.go
@@ -159,6 +159,10 @@ func (w *WalletController) ReleaseOutput(lnwallet.LockID, wire.OutPoint) error {
 	return nil
 }
 
+func (w *WalletController) ListLeasedOutputs() ([]*lnwallet.LockedOutput, error) {
+	return nil, nil
+}
+
 // FundPsbt currently does nothing.
 func (w *WalletController) FundPsbt(_ *psbt.Packet,
 	_ chainfee.AtomPerKByte) (int32, error) {