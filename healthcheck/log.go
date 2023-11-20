lightningnetwork/lnd

healthcheck: disable default healthcheck logger
Disable logging by default in the healthcheck submodule to cut
dependency on lnd/build as we are hooking the logger separately
during lnd init.

healthcheck/log.go
@@ -2,7 +2,6 @@ package healthcheck
 
 import (
 	"github.com/btcsuite/btclog"
-	"github.com/lightningnetwork/lnd/build"
 )
 
 // Subsystem defines the logging code for this subsystem.
@@ -11,12 +10,7 @@ const Subsystem = "HLCK"
 // log is a logger that is initialized with no output filters.  This
 // means the package will not perform any logging by default until the caller
 // requests it.
-var log btclog.Logger
-
-// The default amount of logging is none.
-func init() {
-	UseLogger(build.NewSubLogger(Subsystem, nil))
-}
+var log = btclog.Disabled
 
 // DisableLog disables all library log output.  Logging output is disabled
 // by default until UseLogger is called.

decred/dcrlnd

healthcheck/log.go
@@ -1,7 +1,6 @@
 package healthcheck
 
 import (
-	"github.com/decred/dcrlnd/build"
 	"github.com/decred/slog"
 )
 
@@ -11,12 +10,7 @@ const Subsystem = "HLCK"
 // log is a logger that is initialized with no output filters.  This
 // means the package will not perform any logging by default until the caller
 // requests it.
-var log slog.Logger
-
-// The default amount of logging is none.
-func init() {
-	UseLogger(build.NewSubLogger(Subsystem, nil))
-}
+var log = slog.Disabled
 
 // DisableLog disables all library log output.  Logging output is disabled
 // by default until UseLogger is called.