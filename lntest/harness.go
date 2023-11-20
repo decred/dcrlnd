lightningnetwork/lnd

itest: basic failover itest when using leader election on etcd

lntest/harness.go
@@ -21,6 +21,7 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/btcsuite/btcutil"
 	"github.com/lightningnetwork/lnd"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb/etcd"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lntest/wait"
 	"github.com/lightningnetwork/lnd/lnwallet/chainfee"
@@ -61,9 +62,9 @@ type NetworkHarness struct {
 	Alice *HarnessNode
 	Bob   *HarnessNode
 
-	// useEtcd is set to true if new nodes are to be created with an
+	// embeddedEtcd is set to true if new nodes are to be created with an
 	// embedded etcd backend instead of just bbolt.
-	useEtcd bool
+	embeddedEtcd bool
 
 	// Channel for transmitting stderr output from failed lightning node
 	// to main process.
@@ -83,7 +84,7 @@ type NetworkHarness struct {
 // current repo. This will save developers from having to manually `go install`
 // within the repo each time before changes
 func NewNetworkHarness(r *rpctest.Harness, b BackendConfig, lndBinary string,
-	useEtcd bool) (*NetworkHarness, error) {
+	embeddedEtcd bool) (*NetworkHarness, error) {
 
 	feeService := startFeeService()
 
@@ -97,7 +98,7 @@ func NewNetworkHarness(r *rpctest.Harness, b BackendConfig, lndBinary string,
 		feeService:   feeService,
 		quit:         make(chan struct{}),
 		lndBinary:    lndBinary,
-		useEtcd:      useEtcd,
+		embeddedEtcd: embeddedEtcd,
 	}
 	return &n, nil
 }
@@ -270,11 +271,70 @@ func (n *NetworkHarness) Stop() {
 	n.feeService.stop()
 }
 
+// extraArgsEtcd returns extra args for configuring LND to use an external etcd
+// database (for remote channel DB and wallet DB).
+func extraArgsEtcd(etcdCfg *etcd.Config, name string, cluster bool) []string {
+	extraArgs := []string{
+		"--db.backend=etcd",
+		fmt.Sprintf("--db.etcd.host=%v", etcdCfg.Host),
+		fmt.Sprintf("--db.etcd.user=%v", etcdCfg.User),
+		fmt.Sprintf("--db.etcd.pass=%v", etcdCfg.Pass),
+		fmt.Sprintf("--db.etcd.namespace=%v", etcdCfg.Namespace),
+	}
+
+	if etcdCfg.InsecureSkipVerify {
+		extraArgs = append(extraArgs, "--db.etcd.insecure_skip_verify")
+
+	}
+
+	if cluster {
+		extraArgs = append(extraArgs, "--cluster.enable-leader-election")
+		extraArgs = append(
+			extraArgs, fmt.Sprintf("--cluster.id=%v", name),
+		)
+	}
+
+	return extraArgs
+}
+
+// NewNodeWithSeedEtcd starts a new node with seed that'll use an external
+// etcd database as its (remote) channel and wallet DB. The passsed cluster
+// flag indicates that we'd like the node to join the cluster leader election.
+func (n *NetworkHarness) NewNodeWithSeedEtcd(name string, etcdCfg *etcd.Config,
+	password []byte, entropy []byte, statelessInit, cluster bool) (
+	*HarnessNode, []string, []byte, error) {
+
+	// We don't want to use the embedded etcd instance.
+	const embeddedEtcd = false
+
+	extraArgs := extraArgsEtcd(etcdCfg, name, cluster)
+	return n.newNodeWithSeed(
+		name, extraArgs, password, entropy, statelessInit, embeddedEtcd,
+	)
+}
+
+// NewNodeWithSeedEtcd starts a new node with seed that'll use an external
+// etcd database as its (remote) channel and wallet DB. The passsed cluster
+// flag indicates that we'd like the node to join the cluster leader election.
+// If the wait flag is false then we won't wait until RPC is available (this is
+// useful when the node is not expected to become the leader right away).
+func (n *NetworkHarness) NewNodeEtcd(name string, etcdCfg *etcd.Config,
+	password []byte, cluster, wait bool) (*HarnessNode, error) {
+
+	// We don't want to use the embedded etcd instance.
+	const embeddedEtcd = false
+
+	extraArgs := extraArgsEtcd(etcdCfg, name, cluster)
+	return n.newNode(name, extraArgs, true, password, embeddedEtcd, wait)
+}
+
 // NewNode fully initializes a returns a new HarnessNode bound to the
 // current instance of the network harness. The created node is running, but
 // not yet connected to other nodes within the network.
-func (n *NetworkHarness) NewNode(name string, extraArgs []string) (*HarnessNode, error) {
-	return n.newNode(name, extraArgs, false, nil)
+func (n *NetworkHarness) NewNode(name string, extraArgs []string) (*HarnessNode,
+	error) {
+
+	return n.newNode(name, extraArgs, false, nil, n.embeddedEtcd, true)
 }
 
 // NewNodeWithSeed fully initializes a new HarnessNode after creating a fresh
@@ -285,7 +345,18 @@ func (n *NetworkHarness) NewNodeWithSeed(name string, extraArgs []string,
 	password []byte, statelessInit bool) (*HarnessNode, []string, []byte,
 	error) {
 
-	node, err := n.newNode(name, extraArgs, true, password)
+	return n.newNodeWithSeed(
+		name, extraArgs, password, nil, statelessInit, n.embeddedEtcd,
+	)
+}
+
+func (n *NetworkHarness) newNodeWithSeed(name string, extraArgs []string,
+	password, entropy []byte, statelessInit, embeddedEtcd bool) (
+	*HarnessNode, []string, []byte, error) {
+
+	node, err := n.newNode(
+		name, extraArgs, true, password, embeddedEtcd, true,
+	)
 	if err != nil {
 		return nil, nil, nil, err
 	}
@@ -296,6 +367,7 @@ func (n *NetworkHarness) NewNodeWithSeed(name string, extraArgs []string,
 	// same password as the internal wallet.
 	genSeedReq := &lnrpc.GenSeedRequest{
 		AezeedPassphrase: password,
+		SeedEntropy:      entropy,
 	}
 
 	ctxt, cancel := context.WithTimeout(ctxb, DefaultTimeout)
@@ -345,7 +417,9 @@ func (n *NetworkHarness) RestoreNodeWithSeed(name string, extraArgs []string,
 	password []byte, mnemonic []string, recoveryWindow int32,
 	chanBackups *lnrpc.ChanBackupSnapshot) (*HarnessNode, error) {
 
-	node, err := n.newNode(name, extraArgs, true, password)
+	node, err := n.newNode(
+		name, extraArgs, true, password, n.embeddedEtcd, true,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -375,7 +449,8 @@ func (n *NetworkHarness) RestoreNodeWithSeed(name string, extraArgs []string,
 // can be used immediately. Otherwise, the node will require an additional
 // initialization phase where the wallet is either created or restored.
 func (n *NetworkHarness) newNode(name string, extraArgs []string, hasSeed bool,
-	password []byte) (*HarnessNode, error) {
+	password []byte, embeddedEtcd, wait bool) (
+	*HarnessNode, error) {
 
 	node, err := newNode(NodeConfig{
 		Name:              name,
@@ -386,7 +461,7 @@ func (n *NetworkHarness) newNode(name string, extraArgs []string, hasSeed bool,
 		NetParams:         n.netParams,
 		ExtraArgs:         extraArgs,
 		FeeURL:            n.feeService.url,
-		Etcd:              n.useEtcd,
+		Etcd:              embeddedEtcd,
 	})
 	if err != nil {
 		return nil, err
@@ -398,7 +473,8 @@ func (n *NetworkHarness) newNode(name string, extraArgs []string, hasSeed bool,
 	n.activeNodes[node.NodeID] = node
 	n.mtx.Unlock()
 
-	if err := node.start(n.lndBinary, n.lndErrorChan); err != nil {
+	err = node.start(n.lndBinary, n.lndErrorChan, wait)
+	if err != nil {
 		return nil, err
 	}
 
@@ -684,7 +760,7 @@ func (n *NetworkHarness) RestartNodeNoUnlock(node *HarnessNode,
 		}
 	}
 
-	return node.start(n.lndBinary, n.lndErrorChan)
+	return node.start(n.lndBinary, n.lndErrorChan, true)
 }
 
 // SuspendNode stops the given node and returns a callback that can be used to
@@ -695,7 +771,7 @@ func (n *NetworkHarness) SuspendNode(node *HarnessNode) (func() error, error) {
 	}
 
 	restart := func() error {
-		return node.start(n.lndBinary, n.lndErrorChan)
+		return node.start(n.lndBinary, n.lndErrorChan, true)
 	}
 
 	return restart, nil

lntest/itest/lnd_etcd_failover_test.go
@@ -1,0 +1,143 @@
+// +build kvdb_etcd
+
+package itest
+
+import (
+	"context"
+	"io/ioutil"
+	"time"
+
+	"github.com/btcsuite/btcutil"
+	"github.com/lightningnetwork/lnd/channeldb/kvdb"
+	"github.com/lightningnetwork/lnd/cluster"
+	"github.com/lightningnetwork/lnd/lncfg"
+	"github.com/lightningnetwork/lnd/lnrpc"
+	"github.com/lightningnetwork/lnd/lnrpc/routerrpc"
+	"github.com/lightningnetwork/lnd/lntest"
+)
+
+func assertLeader(ht *harnessTest, observer cluster.LeaderElector,
+	expected string) {
+
+	leader, err := observer.Leader(context.Background())
+	if err != nil {
+		ht.Fatalf("Unable to query leader: %v", err)
+	}
+
+	if leader != expected {
+		ht.Fatalf("Leader should be '%v', got: '%v'", expected, leader)
+	}
+}
+
+func testEtcdFailover(net *lntest.NetworkHarness, ht *harnessTest) {
+	ctxb := context.Background()
+
+	tmpDir, err := ioutil.TempDir("", "etcd")
+	etcdCfg, cleanup, err := kvdb.StartEtcdTestBackend(
+		tmpDir, uint16(lntest.NextAvailablePort()),
+		uint16(lntest.NextAvailablePort()),
+	)
+	if err != nil {
+		ht.Fatalf("Failed to start etcd instance: %v", err)
+	}
+	defer cleanup()
+
+	observer, err := cluster.MakeLeaderElector(
+		ctxb, cluster.EtcdLeaderElector, "observer",
+		lncfg.DefaultEtcdElectionPrefix, etcdCfg,
+	)
+	if err != nil {
+		ht.Fatalf("Cannot start election observer")
+	}
+
+	password := []byte("the quick brown fox jumps the lazy dog")
+	entropy := [16]byte{1, 2, 3}
+	stateless := false
+	cluster := true
+
+	carol1, _, _, err := net.NewNodeWithSeedEtcd(
+		"Carol-1", etcdCfg, password, entropy[:], stateless, cluster,
+	)
+	if err != nil {
+		ht.Fatalf("unable to start Carol-1: %v", err)
+	}
+
+	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
+	info1, err := carol1.GetInfo(ctxt, &lnrpc.GetInfoRequest{})
+
+	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
+	if err := net.ConnectNodes(ctxt, carol1, net.Alice); err != nil {
+		ht.Fatalf("unable to connect Carol to Alice: %v", err)
+	}
+
+	// Open a channel with 100k satoshis between Carol and Alice with Alice
+	// being the sole funder of the channel.
+	chanAmt := btcutil.Amount(100000)
+	ctxt, _ = context.WithTimeout(ctxb, channelOpenTimeout)
+	_ = openChannelAndAssert(
+		ctxt, ht, net, net.Alice, carol1,
+		lntest.OpenChannelParams{
+			Amt: chanAmt,
+		},
+	)
+
+	// At this point Carol-1 is the elected leader, while Carol-2 will wait
+	// to become the leader when Carol-1 stops.
+	carol2, err := net.NewNodeEtcd(
+		"Carol-2", etcdCfg, password, cluster, false,
+	)
+	if err != nil {
+		ht.Fatalf("Unable to start Carol-2: %v", err)
+	}
+
+	assertLeader(ht, observer, "Carol-1")
+
+	amt := btcutil.Amount(1000)
+	payReqs, _, _, err := createPayReqs(carol1, amt, 2)
+	if err != nil {
+		ht.Fatalf("Carol-2 is unable to create payment requests: %v",
+			err)
+	}
+	sendAndAssertSuccess(ctxb, ht, net.Alice, &routerrpc.SendPaymentRequest{
+		PaymentRequest: payReqs[0],
+		TimeoutSeconds: 60,
+		FeeLimitSat:    noFeeLimitMsat,
+	})
+
+	// Shut down Carol-1 and wait for Carol-2 to become the leader.
+	shutdownAndAssert(net, ht, carol1)
+	err = carol2.WaitUntilLeader(30 * time.Second)
+	if err != nil {
+		ht.Fatalf("Waiting for Carol-2 to become the leader failed: %v",
+			err)
+	}
+
+	assertLeader(ht, observer, "Carol-2")
+
+	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
+	err = carol2.Unlock(ctxt, &lnrpc.UnlockWalletRequest{
+		WalletPassword: password,
+	})
+	if err != nil {
+		ht.Fatalf("Unlocking Carol-2 was not successful: %v", err)
+	}
+
+	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
+
+	// Make sure Carol-1 and Carol-2 have the same identity.
+	info2, err := carol2.GetInfo(ctxt, &lnrpc.GetInfoRequest{})
+	if info1.IdentityPubkey != info2.IdentityPubkey {
+		ht.Fatalf("Carol-1 and Carol-2 must have the same identity: "+
+			"%v vs %v", info1.IdentityPubkey, info2.IdentityPubkey)
+	}
+
+	// Now let Alice pay the second invoice but this time we expect Carol-2
+	// to receive the payment.
+	sendAndAssertSuccess(ctxb, ht, net.Alice, &routerrpc.SendPaymentRequest{
+		PaymentRequest: payReqs[1],
+		TimeoutSeconds: 60,
+		FeeLimitSat:    noFeeLimitMsat,
+	})
+
+	shutdownAndAssert(net, ht, carol2)
+}

lntest/itest/lnd_no_etcd_dummy_failover_test.go
@@ -1,0 +1,11 @@
+// +build !kvdb_etcd
+
+package itest
+
+import (
+	"github.com/lightningnetwork/lnd/lntest"
+)
+
+// testEtcdFailover is an empty itest when LND is not compiled with etcd
+// support.
+func testEtcdFailover(net *lntest.NetworkHarness, ht *harnessTest) {}

decred/dcrlnd

lntest/harness.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	"github.com/decred/dcrlnd"
+	"github.com/decred/dcrlnd/channeldb/kvdb/etcd"
 	"github.com/decred/dcrlnd/input"
 	"github.com/decred/dcrlnd/lnrpc"
 	"github.com/decred/dcrlnd/lntest/wait"
@@ -67,9 +68,9 @@ type NetworkHarness struct {
 	Alice *HarnessNode
 	Bob   *HarnessNode
 
-	// useEtcd is set to true if new nodes are to be created with an
+	// embeddedEtcd is set to true if new nodes are to be created with an
 	// embedded etcd backend instead of just bolt.
-	useEtcd bool
+	embeddedEtcd bool
 
 	// Channel for transmitting stderr output from failed lightning node
 	// to main process.
@@ -89,7 +90,7 @@ type NetworkHarness struct {
 // current repo. This will save developers from having to manually `go install`
 // within the repo each time before changes
 func NewNetworkHarness(r *rpctest.Harness, b BackendConfig, lndBinary string,
-	useEtcd bool) (*NetworkHarness, error) {
+	embeddedEtcd bool) (*NetworkHarness, error) {
 
 	feeService := startFeeService()
 
@@ -103,7 +104,7 @@ func NewNetworkHarness(r *rpctest.Harness, b BackendConfig, lndBinary string,
 		feeService:   feeService,
 		quit:         make(chan struct{}),
 		lndBinary:    lndBinary,
-		useEtcd:      useEtcd,
+		embeddedEtcd: embeddedEtcd,
 	}
 	return &n, nil
 }
@@ -323,11 +324,70 @@ func (n *NetworkHarness) Stop() {
 	n.feeService.stop()
 }
 
+// extraArgsEtcd returns extra args for configuring LND to use an external etcd
+// database (for remote channel DB and wallet DB).
+func extraArgsEtcd(etcdCfg *etcd.Config, name string, cluster bool) []string {
+	extraArgs := []string{
+		"--db.backend=etcd",
+		fmt.Sprintf("--db.etcd.host=%v", etcdCfg.Host),
+		fmt.Sprintf("--db.etcd.user=%v", etcdCfg.User),
+		fmt.Sprintf("--db.etcd.pass=%v", etcdCfg.Pass),
+		fmt.Sprintf("--db.etcd.namespace=%v", etcdCfg.Namespace),
+	}
+
+	if etcdCfg.InsecureSkipVerify {
+		extraArgs = append(extraArgs, "--db.etcd.insecure_skip_verify")
+
+	}
+
+	if cluster {
+		extraArgs = append(extraArgs, "--cluster.enable-leader-election")
+		extraArgs = append(
+			extraArgs, fmt.Sprintf("--cluster.id=%v", name),
+		)
+	}
+
+	return extraArgs
+}
+
+// NewNodeWithSeedEtcd starts a new node with seed that'll use an external
+// etcd database as its (remote) channel and wallet DB. The passsed cluster
+// flag indicates that we'd like the node to join the cluster leader election.
+func (n *NetworkHarness) NewNodeWithSeedEtcd(name string, etcdCfg *etcd.Config,
+	password []byte, entropy []byte, statelessInit, cluster bool) (
+	*HarnessNode, []string, []byte, error) {
+
+	// We don't want to use the embedded etcd instance.
+	const embeddedEtcd = false
+
+	extraArgs := extraArgsEtcd(etcdCfg, name, cluster)
+	return n.newNodeWithSeed(
+		name, extraArgs, password, entropy, statelessInit, embeddedEtcd,
+	)
+}
+
+// NewNodeWithSeedEtcd starts a new node with seed that'll use an external
+// etcd database as its (remote) channel and wallet DB. The passsed cluster
+// flag indicates that we'd like the node to join the cluster leader election.
+// If the wait flag is false then we won't wait until RPC is available (this is
+// useful when the node is not expected to become the leader right away).
+func (n *NetworkHarness) NewNodeEtcd(name string, etcdCfg *etcd.Config,
+	password []byte, cluster, wait bool) (*HarnessNode, error) {
+
+	// We don't want to use the embedded etcd instance.
+	const embeddedEtcd = false
+
+	extraArgs := extraArgsEtcd(etcdCfg, name, cluster)
+	return n.newNode(name, extraArgs, true, password, embeddedEtcd, wait)
+}
+
 // NewNode fully initializes a returns a new HarnessNode bound to the
 // current instance of the network harness. The created node is running, but
 // not yet connected to other nodes within the network.
-func (n *NetworkHarness) NewNode(name string, extraArgs []string) (*HarnessNode, error) {
-	return n.newNode(name, extraArgs, false, nil)
+func (n *NetworkHarness) NewNode(name string, extraArgs []string) (*HarnessNode,
+	error) {
+
+	return n.newNode(name, extraArgs, false, nil, n.embeddedEtcd, true)
 }
 
 // NewNodeWithSeed fully initializes a new HarnessNode after creating a fresh
@@ -338,7 +398,18 @@ func (n *NetworkHarness) NewNodeWithSeed(name string, extraArgs []string,
 	password []byte, statelessInit bool) (*HarnessNode, []string, []byte,
 	error) {
 
-	node, err := n.newNode(name, extraArgs, true, password)
+	return n.newNodeWithSeed(
+		name, extraArgs, password, nil, statelessInit, n.embeddedEtcd,
+	)
+}
+
+func (n *NetworkHarness) newNodeWithSeed(name string, extraArgs []string,
+	password, entropy []byte, statelessInit, embeddedEtcd bool) (
+	*HarnessNode, []string, []byte, error) {
+
+	node, err := n.newNode(
+		name, extraArgs, true, password, embeddedEtcd, true,
+	)
 	if err != nil {
 		return nil, nil, nil, err
 	}
@@ -349,6 +420,7 @@ func (n *NetworkHarness) NewNodeWithSeed(name string, extraArgs []string,
 	// same password as the internal wallet.
 	genSeedReq := &lnrpc.GenSeedRequest{
 		AezeedPassphrase: password,
+		SeedEntropy:      entropy,
 	}
 
 	ctxt, cancel := context.WithTimeout(ctxb, DefaultTimeout)
@@ -398,7 +470,9 @@ func (n *NetworkHarness) RestoreNodeWithSeed(name string, extraArgs []string,
 	password []byte, mnemonic []string, recoveryWindow int32,
 	chanBackups *lnrpc.ChanBackupSnapshot) (*HarnessNode, error) {
 
-	node, err := n.newNode(name, extraArgs, true, password)
+	node, err := n.newNode(
+		name, extraArgs, true, password, n.embeddedEtcd, true,
+	)
 	if err != nil {
 		return nil, err
 	}
@@ -428,7 +502,8 @@ func (n *NetworkHarness) RestoreNodeWithSeed(name string, extraArgs []string,
 // can be used immediately. Otherwise, the node will require an additional
 // initialization phase where the wallet is either created or restored.
 func (n *NetworkHarness) newNode(name string, extraArgs []string, hasSeed bool,
-	password []byte) (*HarnessNode, error) {
+	password []byte, embeddedEtcd, wait bool) (
+	*HarnessNode, error) {
 
 	node, err := newNode(NodeConfig{
 		Name:              name,
@@ -441,7 +516,7 @@ func (n *NetworkHarness) newNode(name string, extraArgs []string, hasSeed bool,
 		RemoteWallet:      useRemoteWallet(),
 		DcrwNode:          useDcrwNode(),
 		FeeURL:            n.feeService.url,
-		Etcd:              n.useEtcd,
+		Etcd:              embeddedEtcd,
 	})
 	if err != nil {
 		return nil, err
@@ -453,7 +528,8 @@ func (n *NetworkHarness) newNode(name string, extraArgs []string, hasSeed bool,
 	n.activeNodes[node.NodeID] = node
 	n.mtx.Unlock()
 
-	if err := node.start(n.lndBinary, n.lndErrorChan); err != nil {
+	err = node.start(n.lndBinary, n.lndErrorChan, wait)
+	if err != nil {
 		return nil, fmt.Errorf("unable to start new node: %v", err)
 	}
 
@@ -739,7 +815,7 @@ func (n *NetworkHarness) RestartNodeNoUnlock(node *HarnessNode,
 		}
 	}
 
-	return node.start(n.lndBinary, n.lndErrorChan)
+	return node.start(n.lndBinary, n.lndErrorChan, true)
 }
 
 // SuspendNode stops the given node and returns a callback that can be used to
@@ -750,7 +826,7 @@ func (n *NetworkHarness) SuspendNode(node *HarnessNode) (func() error, error) {
 	}
 
 	restart := func() error {
-		return node.start(n.lndBinary, n.lndErrorChan)
+		return node.start(n.lndBinary, n.lndErrorChan, true)
 	}
 
 	return restart, nil

lntest/itest/lnd_etcd_failover_test.go
@@ -1,0 +1,144 @@
+//go:build kvdb_etcd
+// +build kvdb_etcd
+
+package itest
+
+import (
+	"context"
+	"io/ioutil"
+	"time"
+
+	"github.com/decred/dcrd/dcrutil/v4"
+	"github.com/decred/dcrlnd/channeldb/kvdb"
+	"github.com/decred/dcrlnd/cluster"
+	"github.com/decred/dcrlnd/lncfg"
+	"github.com/decred/dcrlnd/lnrpc"
+	"github.com/decred/dcrlnd/lnrpc/routerrpc"
+	"github.com/decred/dcrlnd/lntest"
+)
+
+func assertLeader(ht *harnessTest, observer cluster.LeaderElector,
+	expected string) {
+
+	leader, err := observer.Leader(context.Background())
+	if err != nil {
+		ht.Fatalf("Unable to query leader: %v", err)
+	}
+
+	if leader != expected {
+		ht.Fatalf("Leader should be '%v', got: '%v'", expected, leader)
+	}
+}
+
+func testEtcdFailover(net *lntest.NetworkHarness, ht *harnessTest) {
+	ctxb := context.Background()
+
+	tmpDir, err := ioutil.TempDir("", "etcd")
+	etcdCfg, cleanup, err := kvdb.StartEtcdTestBackend(
+		tmpDir, uint16(lntest.NextAvailablePort()),
+		uint16(lntest.NextAvailablePort()),
+	)
+	if err != nil {
+		ht.Fatalf("Failed to start etcd instance: %v", err)
+	}
+	defer cleanup()
+
+	observer, err := cluster.MakeLeaderElector(
+		ctxb, cluster.EtcdLeaderElector, "observer",
+		lncfg.DefaultEtcdElectionPrefix, etcdCfg,
+	)
+	if err != nil {
+		ht.Fatalf("Cannot start election observer")
+	}
+
+	password := []byte("the quick brown fox jumps the lazy dog")
+	entropy := [16]byte{1, 2, 3}
+	stateless := false
+	cluster := true
+
+	carol1, _, _, err := net.NewNodeWithSeedEtcd(
+		"Carol-1", etcdCfg, password, entropy[:], stateless, cluster,
+	)
+	if err != nil {
+		ht.Fatalf("unable to start Carol-1: %v", err)
+	}
+
+	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
+	info1, err := carol1.GetInfo(ctxt, &lnrpc.GetInfoRequest{})
+
+	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
+	if err := net.ConnectNodes(ctxt, carol1, net.Alice); err != nil {
+		ht.Fatalf("unable to connect Carol to Alice: %v", err)
+	}
+
+	// Open a channel with 100k satoshis between Carol and Alice with Alice
+	// being the sole funder of the channel.
+	chanAmt := dcrutil.Amount(100000)
+	ctxt, _ = context.WithTimeout(ctxb, channelOpenTimeout)
+	_ = openChannelAndAssert(
+		ctxt, ht, net, net.Alice, carol1,
+		lntest.OpenChannelParams{
+			Amt: chanAmt,
+		},
+	)
+
+	// At this point Carol-1 is the elected leader, while Carol-2 will wait
+	// to become the leader when Carol-1 stops.
+	carol2, err := net.NewNodeEtcd(
+		"Carol-2", etcdCfg, password, cluster, false,
+	)
+	if err != nil {
+		ht.Fatalf("Unable to start Carol-2: %v", err)
+	}
+
+	assertLeader(ht, observer, "Carol-1")
+
+	amt := dcrutil.Amount(1000)
+	payReqs, _, _, err := createPayReqs(carol1, amt, 2)
+	if err != nil {
+		ht.Fatalf("Carol-2 is unable to create payment requests: %v",
+			err)
+	}
+	sendAndAssertSuccess(ctxb, ht, net.Alice, &routerrpc.SendPaymentRequest{
+		PaymentRequest: payReqs[0],
+		TimeoutSeconds: 60,
+		FeeLimitAtoms:  noFeeLimitMAtoms,
+	})
+
+	// Shut down Carol-1 and wait for Carol-2 to become the leader.
+	shutdownAndAssert(net, ht, carol1)
+	err = carol2.WaitUntilLeader(30 * time.Second)
+	if err != nil {
+		ht.Fatalf("Waiting for Carol-2 to become the leader failed: %v",
+			err)
+	}
+
+	assertLeader(ht, observer, "Carol-2")
+
+	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
+	err = carol2.Unlock(ctxt, &lnrpc.UnlockWalletRequest{
+		WalletPassword: password,
+	})
+	if err != nil {
+		ht.Fatalf("Unlocking Carol-2 was not successful: %v", err)
+	}
+
+	ctxt, _ = context.WithTimeout(ctxb, defaultTimeout)
+
+	// Make sure Carol-1 and Carol-2 have the same identity.
+	info2, err := carol2.GetInfo(ctxt, &lnrpc.GetInfoRequest{})
+	if info1.IdentityPubkey != info2.IdentityPubkey {
+		ht.Fatalf("Carol-1 and Carol-2 must have the same identity: "+
+			"%v vs %v", info1.IdentityPubkey, info2.IdentityPubkey)
+	}
+
+	// Now let Alice pay the second invoice but this time we expect Carol-2
+	// to receive the payment.
+	sendAndAssertSuccess(ctxb, ht, net.Alice, &routerrpc.SendPaymentRequest{
+		PaymentRequest: payReqs[1],
+		TimeoutSeconds: 60,
+		FeeLimitAtoms:  noFeeLimitMAtoms,
+	})
+
+	shutdownAndAssert(net, ht, carol2)
+}

lntest/itest/lnd_no_etcd_dummy_failover_test.go
@@ -1,0 +1,12 @@
+//go:build !kvdb_etcd
+// +build !kvdb_etcd
+
+package itest
+
+import (
+	"github.com/decred/dcrlnd/lntest"
+)
+
+// testEtcdFailover is an empty itest when LND is not compiled with etcd
+// support.
+func testEtcdFailover(net *lntest.NetworkHarness, ht *harnessTest) {}