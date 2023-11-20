lightningnetwork/lnd

lncli addinvoice: allow specifying msat with --amt_msat
* pass amt and amt_msat to rpc, letting server give an error if both
are present

* take amt from an extra argument if neither amt nor amt_ms are
present

implementation analogous to 298f35cdfbcfbf171fb7de645e90bdf6a368d6c8,
which is for addholdinvoice.

cmd/lncli/cmd_invoice.go
@@ -38,6 +38,10 @@ var addInvoiceCommand = cli.Command{
 			Name:  "amt",
 			Usage: "the amt of satoshis in this invoice",
 		},
+		cli.Int64Flag{
+			Name:  "amt_msat",
+			Usage: "the amt of millisatoshis in this invoice",
+		},
 		cli.StringFlag{
 			Name: "description_hash",
 			Usage: "SHA-256 hash of the description of the payment. " +
@@ -72,6 +76,7 @@ func addInvoice(ctx *cli.Context) error {
 		preimage []byte
 		descHash []byte
 		amt      int64
+		amtMsat  int64
 		err      error
 	)
 
@@ -80,10 +85,9 @@ func addInvoice(ctx *cli.Context) error {
 
 	args := ctx.Args()
 
-	switch {
-	case ctx.IsSet("amt"):
-		amt = ctx.Int64("amt")
-	case args.Present():
+	amt = ctx.Int64("amt")
+	amtMsat = ctx.Int64("amt_msat")
+	if !ctx.IsSet("amt") && !ctx.IsSet("amt_msat") && args.Present() {
 		amt, err = strconv.ParseInt(args.First(), 10, 64)
 		args = args.Tail()
 		if err != nil {
@@ -111,6 +115,7 @@ func addInvoice(ctx *cli.Context) error {
 		Memo:            ctx.String("memo"),
 		RPreimage:       preimage,
 		Value:           amt,
+		ValueMsat:       amtMsat,
 		DescriptionHash: descHash,
 		FallbackAddr:    ctx.String("fallback_addr"),
 		Expiry:          ctx.Int64("expiry"),

decred/dcrlnd

cmd/dcrlncli/cmd_invoice.go
@@ -37,6 +37,10 @@ var addInvoiceCommand = cli.Command{
 			Name:  "amt",
 			Usage: "The amt of atoms in this invoice",
 		},
+		cli.Int64Flag{
+			Name:  "amt_msat",
+			Usage: "the amt of millisatoshis in this invoice",
+		},
 		cli.StringFlag{
 			Name: "description_hash",
 			Usage: "SHA-256 hash of the description of the payment. " +
@@ -74,10 +78,11 @@ var addInvoiceCommand = cli.Command{
 
 func addInvoice(ctx *cli.Context) error {
 	var (
-		preimage []byte
-		descHash []byte
-		amt      int64
-		err      error
+		preimage  []byte
+		descHash  []byte
+		amt       int64
+		amtMAtoms int64
+		err       error
 	)
 	ctxc := getContext()
 	client, cleanUp := getClient(ctx)
@@ -85,10 +90,9 @@ func addInvoice(ctx *cli.Context) error {
 
 	args := ctx.Args()
 
-	switch {
-	case ctx.IsSet("amt"):
-		amt = ctx.Int64("amt")
-	case args.Present():
+	amt = ctx.Int64("amt")
+	amtMAtoms = ctx.Int64("amt_msat")
+	if !ctx.IsSet("amt") && !ctx.IsSet("amt_msat") && args.Present() {
 		amt, err = strconv.ParseInt(args.First(), 10, 64)
 		args = args.Tail()
 		if err != nil {
@@ -116,6 +120,7 @@ func addInvoice(ctx *cli.Context) error {
 		Memo:                ctx.String("memo"),
 		RPreimage:           preimage,
 		Value:               amt,
+		ValueMAtoms:         amtMAtoms,
 		DescriptionHash:     descHash,
 		FallbackAddr:        ctx.String("fallback_addr"),
 		Expiry:              ctx.Int64("expiry"),