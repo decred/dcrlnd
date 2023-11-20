lightningnetwork/lnd

multi: introduce config-level DustThreshold for defining threshold

config.go
@@ -355,6 +355,8 @@ type Config struct {
 
 	GcCanceledInvoicesOnTheFly bool `long:"gc-canceled-invoices-on-the-fly" description:"If true, we'll delete newly canceled invoices on the fly."`
 
+	DustThreshold uint64 `long:"dust-threshold" description:"Sets the dust sum threshold in satoshis for a channel after which dust HTLC's will be failed."`
+
 	Invoices *lncfg.Invoices `group:"invoices" namespace:"invoices"`
 
 	Routing *lncfg.Routing `group:"routing" namespace:"routing"`
@@ -550,6 +552,7 @@ func DefaultConfig() Config {
 		MaxOutgoingCltvExpiry:   htlcswitch.DefaultMaxOutgoingCltvExpiry,
 		MaxChannelFeeAllocation: htlcswitch.DefaultMaxLinkFeeAllocation,
 		MaxCommitFeeRateAnchors: lnwallet.DefaultAnchorsCommitMaxFeeRateSatPerVByte,
+		DustThreshold:           uint64(htlcswitch.DefaultDustThreshold.ToSatoshis()),
 		LogWriter:               build.NewRotatingLogWriter(),
 		DB:                      lncfg.DefaultDB(),
 		Cluster:                 lncfg.DefaultCluster(),

htlcswitch/switch.go
@@ -77,9 +77,9 @@ var (
 	// threshold.
 	errDustThresholdExceeded = errors.New("dust threshold exceeded")
 
-	// defaultDustThreshold is the default threshold after which we'll fail
-	// payments if they are dust. This is currently set to 500k sats.
-	defaultDustThreshold = lnwire.MilliSatoshi(500_000_000)
+	// DefaultDustThreshold is the default threshold after which we'll fail
+	// payments if they are dust. This is currently set to 500m msats.
+	DefaultDustThreshold = lnwire.MilliSatoshi(500_000_000)
 )
 
 // plexPacket encapsulates switch packet and adds error channel to receive
@@ -187,6 +187,10 @@ type Config struct {
 	// will expiry this long after the Adds are added to a mailbox via
 	// AddPacket.
 	HTLCExpiry time.Duration
+
+	// DustThreshold is the threshold in milli-satoshis after which we'll
+	// fail incoming or outgoing dust payments for a particular channel.
+	DustThreshold lnwire.MilliSatoshi
 }
 
 // Switch is the central messaging bus for all incoming/outgoing HTLCs.
@@ -2375,7 +2379,7 @@ func (s *Switch) evaluateDustThreshold(link ChannelLink,
 		}
 
 		// Finally check against the defined dust threshold.
-		if localSum > defaultDustThreshold {
+		if localSum > s.cfg.DustThreshold {
 			return true
 		}
 	}
@@ -2393,7 +2397,7 @@ func (s *Switch) evaluateDustThreshold(link ChannelLink,
 		}
 
 		// Finally check against the defined dust threshold.
-		if remoteSum > defaultDustThreshold {
+		if remoteSum > s.cfg.DustThreshold {
 			return true
 		}
 	}

server.go
@@ -490,6 +490,9 @@ func newServer(cfg *Config, listenAddrs []net.Addr,
 
 	s.htlcNotifier = htlcswitch.NewHtlcNotifier(time.Now)
 
+	thresholdSats := btcutil.Amount(cfg.DustThreshold)
+	thresholdMSats := lnwire.NewMSatFromSatoshis(thresholdSats)
+
 	s.htlcSwitch, err = htlcswitch.New(htlcswitch.Config{
 		DB: dbs.chanStateDB,
 		LocalChannelClose: func(pubKey []byte,
@@ -519,6 +522,7 @@ func newServer(cfg *Config, listenAddrs []net.Addr,
 		RejectHTLC:             cfg.RejectHTLC,
 		Clock:                  clock.NewDefaultClock(),
 		HTLCExpiry:             htlcswitch.DefaultHTLCExpiry,
+		DustThreshold:          thresholdMSats,
 	}, uint32(currentHeight))
 	if err != nil {
 		return nil, err

decred/dcrlnd

config.go
@@ -340,6 +340,8 @@ type Config struct {
 
 	GcCanceledInvoicesOnTheFly bool `long:"gc-canceled-invoices-on-the-fly" description:"If true, we'll delete newly canceled invoices on the fly."`
 
+	DustThreshold uint64 `long:"dust-threshold" description:"Sets the dust sum threshold in satoshis for a channel after which dust HTLC's will be failed."`
+
 	Invoices *lncfg.Invoices `group:"invoices" namespace:"invoices"`
 
 	Routing *lncfg.Routing `group:"routing" namespace:"routing"`
@@ -506,6 +508,7 @@ func DefaultConfig() Config {
 		MaxOutgoingCltvExpiry:   htlcswitch.DefaultMaxOutgoingCltvExpiry,
 		MaxChannelFeeAllocation: htlcswitch.DefaultMaxLinkFeeAllocation,
 		MaxCommitFeeRateAnchors: lnwallet.DefaultAnchorsCommitMaxFeeRateAtomsPerByte,
+		DustThreshold:           uint64(htlcswitch.DefaultDustThreshold.ToAtoms()),
 		LogWriter:               build.NewRotatingLogWriter(),
 		DB:                      lncfg.DefaultDB(),
 		Cluster:                 lncfg.DefaultCluster(),

htlcswitch/switch.go
@@ -77,9 +77,9 @@ var (
 	// threshold.
 	errDustThresholdExceeded = errors.New("dust threshold exceeded")
 
-	// defaultDustThreshold is the default threshold after which we'll fail
+	// DefaultDustThreshold is the default threshold after which we'll fail
 	// payments if they are dust. This is currently set to 500k atoms.
-	defaultDustThreshold = lnwire.MilliAtom(500_000_000)
+	DefaultDustThreshold = lnwire.MilliAtom(500_000_000)
 )
 
 // plexPacket encapsulates switch packet and adds error channel to receive
@@ -201,6 +201,10 @@ type Config struct {
 	// will expiry this long after the Adds are added to a mailbox via
 	// AddPacket.
 	HTLCExpiry time.Duration
+
+	// DustThreshold is the threshold in milli-satoshis after which we'll
+	// fail incoming or outgoing dust payments for a particular channel.
+	DustThreshold lnwire.MilliAtom
 }
 
 // Switch is the central messaging bus for all incoming/outgoing HTLCs.
@@ -2374,7 +2378,7 @@ func (s *Switch) evaluateDustThreshold(link ChannelLink,
 		}
 
 		// Finally check against the defined dust threshold.
-		if localSum > defaultDustThreshold {
+		if localSum > s.cfg.DustThreshold {
 			return true
 		}
 	}
@@ -2392,7 +2396,7 @@ func (s *Switch) evaluateDustThreshold(link ChannelLink,
 		}
 
 		// Finally check against the defined dust threshold.
-		if remoteSum > defaultDustThreshold {
+		if remoteSum > s.cfg.DustThreshold {
 			return true
 		}
 	}

server.go
@@ -494,6 +494,9 @@ func newServer(cfg *Config, listenAddrs []net.Addr,
 
 	s.htlcNotifier = htlcswitch.NewHtlcNotifier(time.Now)
 
+	thresholdSats := dcrutil.Amount(cfg.DustThreshold)
+	thresholdMSats := lnwire.NewMAtomsFromAtoms(thresholdSats)
+
 	s.htlcSwitch, err = htlcswitch.New(htlcswitch.Config{
 		DB: remoteChanDB,
 		LocalChannelClose: func(pubKey []byte,
@@ -523,6 +526,7 @@ func newServer(cfg *Config, listenAddrs []net.Addr,
 		RejectHTLC:             cfg.RejectHTLC,
 		Clock:                  clock.NewDefaultClock(),
 		HTLCExpiry:             htlcswitch.DefaultHTLCExpiry,
+		DustThreshold:          thresholdMSats,
 	}, uint32(currentHeight))
 	if err != nil {
 		return nil, err