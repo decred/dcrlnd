lightningnetwork/lnd

mod+kvdb+channeldb: use btcwallet new DB interface
Depends on btcsuite/btcwallet#757.
Pulls in the updated version of btcwallet and walletdb that have the DB
interface enhanced by their own View() and Update() methods with the
reset callback/closure supported out of the box. That way the global
package-level View() and Update() functions now become pure redirects.

lnwallet/test/test_interface.go
@@ -3201,7 +3201,7 @@ func TestLightningWallet(t *testing.T, targetBackEnd string) {
 	testCfg := chainntnfs.CacheConfig{
 		QueryDisable: false,
 	}
-	hintCache, err := chainntnfs.NewHeightHintCache(testCfg, db)
+	hintCache, err := chainntnfs.NewHeightHintCache(testCfg, db.Backend)
 	if err != nil {
 		t.Fatalf("unable to create height hint cache: %v", err)
 	}

decred/dcrlnd

lnwallet/test/test_interface.go
@@ -3314,11 +3314,10 @@ func TestLightningWallet(t *testing.T, driverName, backEnd string) {
 	hintCacheCfg := chainntnfs.CacheConfig{
 		QueryDisable: false,
 	}
-	hintCache, err := chainntnfs.NewHeightHintCache(hintCacheCfg, db)
+	hintCache, err := chainntnfs.NewHeightHintCache(hintCacheCfg, db.Backend)
 	require.NoError(t, err)
 	chainNotifier, err := dcrdnotify.New(
-		&rpcConfig, netParams, hintCache, hintCache,
-		nil,
+		&rpcConfig, netParams, hintCache, hintCache, nil,
 	)
 	require.NoError(t, err)
 	if err := chainNotifier.Start(); err != nil {