lightningnetwork/lnd

multi: move breach arbiter and utxo nursery into contractcourt package
In this commit, we take an initial step towards converting the existing
breach arbiter and utxo nursery logic into contract resolvers by moving
the files as is, into the `contractcourt` pacakge.

This commit is primarily move only, though we had to massage some
interfaces and config names along the way to make things compile and the
tests run properly.

contractcourt/nursery_store_test.go
@@ -1,13 +1,14 @@
+//go:build !rpctest
 // +build !rpctest
 
-package lnd
+package contractcourt
 
 import (
 	"reflect"
 	"testing"
 
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
-	"github.com/lightningnetwork/lnd/chainreg"
 	"github.com/lightningnetwork/lnd/channeldb"
 )
 
@@ -22,6 +23,8 @@ type incubateTest struct {
 // incubateTests holds the test vectors used to test the state transitions of
 // outputs stored in the nursery store.
 var incubateTests []incubateTest
+
+var chainHash chainhash.Hash
 
 // initIncubateTests instantiates the test vectors during package init, which
 // properly captures the sign descriptors and public keys.
@@ -55,7 +58,7 @@ func TestNurseryStoreInit(t *testing.T) {
 	}
 	defer cleanUp()
 
-	ns, err := newNurseryStore(&chainreg.BitcoinTestnetGenesis, cdb)
+	ns, err := NewNurseryStore(&chainHash, cdb)
 	if err != nil {
 		t.Fatalf("unable to open nursery store: %v", err)
 	}
@@ -75,7 +78,7 @@ func TestNurseryStoreIncubate(t *testing.T) {
 	}
 	defer cleanUp()
 
-	ns, err := newNurseryStore(&chainreg.BitcoinTestnetGenesis, cdb)
+	ns, err := NewNurseryStore(&chainHash, cdb)
 	if err != nil {
 		t.Fatalf("unable to open nursery store: %v", err)
 	}
@@ -316,7 +319,7 @@ func TestNurseryStoreGraduate(t *testing.T) {
 	}
 	defer cleanUp()
 
-	ns, err := newNurseryStore(&chainreg.BitcoinTestnetGenesis, cdb)
+	ns, err := NewNurseryStore(&chainHash, cdb)
 	if err != nil {
 		t.Fatalf("unable to open nursery store: %v", err)
 	}
@@ -363,7 +366,7 @@ func TestNurseryStoreGraduate(t *testing.T) {
 
 // assertNumChanOutputs checks that the channel bucket has the expected number
 // of outputs.
-func assertNumChanOutputs(t *testing.T, ns NurseryStore,
+func assertNumChanOutputs(t *testing.T, ns NurseryStorer,
 	chanPoint *wire.OutPoint, expectedNum int) {
 
 	var count int
@@ -389,7 +392,7 @@ func assertNumChanOutputs(t *testing.T, ns NurseryStore,
 
 // assertNumPreschools loads all preschool outputs and verifies their count
 // matches the expected number.
-func assertNumPreschools(t *testing.T, ns NurseryStore, expected int) {
+func assertNumPreschools(t *testing.T, ns NurseryStorer, expected int) {
 	psclOutputs, err := ns.FetchPreschools()
 	if err != nil {
 		t.Fatalf("unable to retrieve preschool outputs: %v", err)
@@ -403,7 +406,7 @@ func assertNumPreschools(t *testing.T, ns NurseryStore, expected int) {
 
 // assertNumChannels checks that the nursery has a given number of active
 // channels.
-func assertNumChannels(t *testing.T, ns NurseryStore, expected int) {
+func assertNumChannels(t *testing.T, ns NurseryStorer, expected int) {
 	channels, err := ns.ListChannels()
 	if err != nil {
 		t.Fatalf("unable to fetch channels from nursery store: %v",
@@ -418,7 +421,7 @@ func assertNumChannels(t *testing.T, ns NurseryStore, expected int) {
 
 // assertHeightIsPurged checks that the finalized transaction, kindergarten, and
 // htlc outputs at a particular height are all nil.
-func assertHeightIsPurged(t *testing.T, ns NurseryStore,
+func assertHeightIsPurged(t *testing.T, ns NurseryStorer,
 	height uint32) {
 
 	kndrOutputs, cribOutputs, err := ns.FetchClass(height)
@@ -438,7 +441,7 @@ func assertHeightIsPurged(t *testing.T, ns NurseryStore,
 
 // assertCribAtExpiryHeight loads the class at the given height, and verifies
 // that the given htlc output is one of the crib outputs.
-func assertCribAtExpiryHeight(t *testing.T, ns NurseryStore,
+func assertCribAtExpiryHeight(t *testing.T, ns NurseryStorer,
 	htlcOutput *babyOutput) {
 
 	expiryHeight := htlcOutput.expiry
@@ -460,7 +463,7 @@ func assertCribAtExpiryHeight(t *testing.T, ns NurseryStore,
 
 // assertCribNotAtExpiryHeight loads the class at the given height, and verifies
 // that the given htlc output is not one of the crib outputs.
-func assertCribNotAtExpiryHeight(t *testing.T, ns NurseryStore,
+func assertCribNotAtExpiryHeight(t *testing.T, ns NurseryStorer,
 	htlcOutput *babyOutput) {
 
 	expiryHeight := htlcOutput.expiry
@@ -481,7 +484,7 @@ func assertCribNotAtExpiryHeight(t *testing.T, ns NurseryStore,
 // assertKndrAtMaturityHeight loads the class at the provided height and
 // verifies that the provided kid output is one of the kindergarten outputs
 // returned.
-func assertKndrAtMaturityHeight(t *testing.T, ns NurseryStore,
+func assertKndrAtMaturityHeight(t *testing.T, ns NurseryStorer,
 	kndrOutput *kidOutput) {
 
 	maturityHeight := kndrOutput.ConfHeight() +
@@ -505,7 +508,7 @@ func assertKndrAtMaturityHeight(t *testing.T, ns NurseryStore,
 // assertKndrNotAtMaturityHeight loads the class at the provided height and
 // verifies that the provided kid output is not one of the kindergarten outputs
 // returned.
-func assertKndrNotAtMaturityHeight(t *testing.T, ns NurseryStore,
+func assertKndrNotAtMaturityHeight(t *testing.T, ns NurseryStorer,
 	kndrOutput *kidOutput) {
 
 	maturityHeight := kndrOutput.ConfHeight() +
@@ -527,7 +530,7 @@ func assertKndrNotAtMaturityHeight(t *testing.T, ns NurseryStore,
 
 // assertChannelMaturity queries the nursery store for the maturity of the given
 // channel, failing if the result does not match the expectedMaturity.
-func assertChannelMaturity(t *testing.T, ns NurseryStore,
+func assertChannelMaturity(t *testing.T, ns NurseryStorer,
 	chanPoint *wire.OutPoint, expectedMaturity bool) {
 
 	isMature, err := ns.IsMatureChannel(chanPoint)
@@ -543,7 +546,7 @@ func assertChannelMaturity(t *testing.T, ns NurseryStore,
 
 // assertCanRemoveChannel tries to remove a channel from the nursery store,
 // failing if the result does match expected canRemove.
-func assertCanRemoveChannel(t *testing.T, ns NurseryStore,
+func assertCanRemoveChannel(t *testing.T, ns NurseryStorer,
 	chanPoint *wire.OutPoint, canRemove bool) {
 
 	err := ns.RemoveChannel(chanPoint)

contractcourt/utxonursery_test.go
@@ -1,6 +1,7 @@
+//go:build !rpctest
 // +build !rpctest
 
-package lnd
+package contractcourt
 
 import (
 	"bytes"
@@ -397,14 +398,14 @@ func TestBabyOutputSerialization(t *testing.T) {
 }
 
 type nurseryTestContext struct {
-	nursery     *utxoNursery
+	nursery     *UtxoNursery
 	notifier    *sweep.MockNotifier
 	chainIO     *mock.ChainIO
 	publishChan chan wire.MsgTx
 	store       *nurseryStoreInterceptor
 	restart     func() bool
 	receiveTx   func() wire.MsgTx
-	sweeper     *mockSweeper
+	sweeper     *mockSweeperFull
 	timeoutChan chan chan time.Time
 	t           *testing.T
 	dbCleanup   func()
@@ -422,7 +423,7 @@ func createNurseryTestContext(t *testing.T,
 		t.Fatalf("unable to open channeldb: %v", err)
 	}
 
-	store, err := newNurseryStore(&chainhash.Hash{}, cdb)
+	store, err := NewNurseryStore(&chainhash.Hash{}, cdb)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -435,7 +436,7 @@ func createNurseryTestContext(t *testing.T,
 
 	publishChan := make(chan wire.MsgTx, 1)
 	publishFunc := func(tx *wire.MsgTx, source string) error {
-		utxnLog.Tracef("Publishing tx %v by %v", tx.TxHash(), source)
+		log.Tracef("Publishing tx %v by %v", tx.TxHash(), source)
 		publishChan <- *tx
 		return nil
 	}
@@ -446,7 +447,7 @@ func createNurseryTestContext(t *testing.T,
 		BestHeight: 0,
 	}
 
-	sweeper := newMockSweeper(t)
+	sweeper := newMockSweeperFull(t)
 
 	nurseryCfg := NurseryConfig{
 		Notifier: notifier,
@@ -468,7 +469,7 @@ func createNurseryTestContext(t *testing.T,
 		},
 	}
 
-	nursery := newUtxoNursery(&nurseryCfg)
+	nursery := NewUtxoNursery(&nurseryCfg)
 	nursery.Start()
 
 	ctx := &nurseryTestContext{
@@ -487,7 +488,7 @@ func createNurseryTestContext(t *testing.T,
 		var tx wire.MsgTx
 		select {
 		case tx = <-ctx.publishChan:
-			utxnLog.Debugf("Published tx %v", tx.TxHash())
+			log.Debugf("Published tx %v", tx.TxHash())
 			return tx
 		case <-time.After(defaultTestTimeout):
 			pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
@@ -499,16 +500,16 @@ func createNurseryTestContext(t *testing.T,
 
 	ctx.restart = func() bool {
 		return checkStartStop(func() {
-			utxnLog.Tracef("Restart sweeper and nursery")
+			log.Tracef("Restart sweeper and nursery")
 			// Simulate lnd restart.
 			ctx.nursery.Stop()
 
 			// Restart sweeper.
-			ctx.sweeper = newMockSweeper(t)
+			ctx.sweeper = newMockSweeperFull(t)
 
 			/// Restart nursery.
 			nurseryCfg.SweepInput = ctx.sweeper.sweepInput
-			ctx.nursery = newUtxoNursery(&nurseryCfg)
+			ctx.nursery = NewUtxoNursery(&nurseryCfg)
 			ctx.nursery.Start()
 
 		})
@@ -627,7 +628,7 @@ func createOutgoingRes(onLocalCommitment bool) *lnwallet.OutgoingHtlcResolution 
 	return &outgoingRes
 }
 
-func incubateTestOutput(t *testing.T, nursery *utxoNursery,
+func incubateTestOutput(t *testing.T, nursery *UtxoNursery,
 	onLocalCommitment bool) *lnwallet.OutgoingHtlcResolution {
 
 	outgoingRes := createOutgoingRes(onLocalCommitment)
@@ -653,7 +654,7 @@ func incubateTestOutput(t *testing.T, nursery *utxoNursery,
 	return outgoingRes
 }
 
-func assertNurseryReport(t *testing.T, nursery *utxoNursery,
+func assertNurseryReport(t *testing.T, nursery *UtxoNursery,
 	expectedNofHtlcs int, expectedStage uint32,
 	expectedLimboBalance btcutil.Amount) {
 	report, err := nursery.NurseryReport(&testChanPoint)
@@ -661,27 +662,27 @@ func assertNurseryReport(t *testing.T, nursery *utxoNursery,
 		t.Fatal(err)
 	}
 
-	if len(report.htlcs) != expectedNofHtlcs {
+	if len(report.Htlcs) != expectedNofHtlcs {
 		t.Fatalf("expected %v outputs to be reported, but report "+
-			"only contains %v", expectedNofHtlcs, len(report.htlcs))
+			"only contains %v", expectedNofHtlcs, len(report.Htlcs))
 	}
 
 	if expectedNofHtlcs != 0 {
-		htlcReport := report.htlcs[0]
-		if htlcReport.stage != expectedStage {
+		htlcReport := report.Htlcs[0]
+		if htlcReport.Stage != expectedStage {
 			t.Fatalf("expected htlc be advanced to stage %v, but "+
 				"it is reported in stage %v",
-				expectedStage, htlcReport.stage)
+				expectedStage, htlcReport.Stage)
 		}
 	}
 
-	if report.limboBalance != expectedLimboBalance {
+	if report.LimboBalance != expectedLimboBalance {
 		t.Fatalf("expected limbo balance to be %v, but it is %v instead",
-			expectedLimboBalance, report.limboBalance)
+			expectedLimboBalance, report.LimboBalance)
 	}
 }
 
-func assertNurseryReportUnavailable(t *testing.T, nursery *utxoNursery) {
+func assertNurseryReportUnavailable(t *testing.T, nursery *UtxoNursery) {
 	_, err := nursery.NurseryReport(&testChanPoint)
 	if err != ErrContractNotFound {
 		t.Fatal("expected report to be unavailable")
@@ -711,7 +712,7 @@ func testRestartLoop(t *testing.T, test func(*testing.T,
 
 				return true
 			}
-			utxnLog.Debugf("Skipping restart point %v",
+			log.Debugf("Skipping restart point %v",
 				currentStartStopIdx)
 			return false
 		}
@@ -863,7 +864,7 @@ func testSweep(t *testing.T, ctx *nurseryTestContext,
 }
 
 type nurseryStoreInterceptor struct {
-	ns NurseryStore
+	ns NurseryStorer
 
 	// TODO(joostjager): put more useful info through these channels.
 	cribToKinderChan      chan struct{}
@@ -872,7 +873,7 @@ type nurseryStoreInterceptor struct {
 	preschoolToKinderChan chan struct{}
 }
 
-func newNurseryStoreInterceptor(ns NurseryStore) *nurseryStoreInterceptor {
+func newNurseryStoreInterceptor(ns NurseryStorer) *nurseryStoreInterceptor {
 	return &nurseryStoreInterceptor{
 		ns:                    ns,
 		cribToKinderChan:      make(chan struct{}),
@@ -950,7 +951,7 @@ func (i *nurseryStoreInterceptor) RemoveChannel(chanPoint *wire.OutPoint) error 
 	return i.ns.RemoveChannel(chanPoint)
 }
 
-type mockSweeper struct {
+type mockSweeperFull struct {
 	lock sync.Mutex
 
 	resultChans map[wire.OutPoint]chan sweep.Result
@@ -959,18 +960,18 @@ type mockSweeper struct {
 	sweepChan chan input.Input
 }
 
-func newMockSweeper(t *testing.T) *mockSweeper {
-	return &mockSweeper{
+func newMockSweeperFull(t *testing.T) *mockSweeperFull {
+	return &mockSweeperFull{
 		resultChans: make(map[wire.OutPoint]chan sweep.Result),
 		sweepChan:   make(chan input.Input, 1),
 		t:           t,
 	}
 }
 
-func (s *mockSweeper) sweepInput(input input.Input,
+func (s *mockSweeperFull) sweepInput(input input.Input,
 	_ sweep.Params) (chan sweep.Result, error) {
 
-	utxnLog.Debugf("mockSweeper sweepInput called for %v", *input.OutPoint())
+	log.Debugf("mockSweeper sweepInput called for %v", *input.OutPoint())
 
 	select {
 	case s.sweepChan <- input:
@@ -987,7 +988,7 @@ func (s *mockSweeper) sweepInput(input input.Input,
 	return c, nil
 }
 
-func (s *mockSweeper) expectSweep() {
+func (s *mockSweeperFull) expectSweep() {
 	s.t.Helper()
 
 	select {
@@ -997,7 +998,7 @@ func (s *mockSweeper) expectSweep() {
 	}
 }
 
-func (s *mockSweeper) sweepAll() {
+func (s *mockSweeperFull) sweepAll() {
 	s.t.Helper()
 
 	s.lock.Lock()
@@ -1006,7 +1007,7 @@ func (s *mockSweeper) sweepAll() {
 	s.lock.Unlock()
 
 	for o, c := range currentChans {
-		utxnLog.Debugf("mockSweeper signal swept for %v", o)
+		log.Debugf("mockSweeper signal swept for %v", o)
 
 		select {
 		case c <- sweep.Result{}:

decred/dcrlnd

Note(decred): This was written manually instead of ported from upstream
to ensure the local changes were picked up.

contractcourt/nursery_store_test.go
@@ -1,11 +1,11 @@
-package dcrlnd
+package contractcourt
 
 import (
 	"reflect"
 	"testing"
 
+	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/wire"
-	"github.com/decred/dcrlnd/chainreg"
 	"github.com/decred/dcrlnd/channeldb"
 )
 
@@ -20,6 +20,8 @@ type incubateTest struct {
 // incubateTests holds the test vectors used to test the state transitions of
 // outputs stored in the nursery store.
 var incubateTests []incubateTest
+
+var chainHash chainhash.Hash
 
 // initIncubateTests instantiates the test vectors during package init, which
 // properly captures the sign descriptors and public keys.
@@ -53,7 +55,7 @@ func TestNurseryStoreInit(t *testing.T) {
 	}
 	defer cleanUp()
 
-	ns, err := newNurseryStore(&chainreg.DecredTestNetParams.GenesisHash, cdb)
+	ns, err := NewNurseryStore(&chainHash, cdb)
 	if err != nil {
 		t.Fatalf("unable to open nursery store: %v", err)
 	}
@@ -73,7 +75,7 @@ func TestNurseryStoreIncubate(t *testing.T) {
 	}
 	defer cleanUp()
 
-	ns, err := newNurseryStore(&chainreg.DecredTestNetParams.GenesisHash, cdb)
+	ns, err := NewNurseryStore(&chainHash, cdb)
 	if err != nil {
 		t.Fatalf("unable to open nursery store: %v", err)
 	}
@@ -314,7 +316,7 @@ func TestNurseryStoreGraduate(t *testing.T) {
 	}
 	defer cleanUp()
 
-	ns, err := newNurseryStore(&chainreg.DecredTestNetParams.GenesisHash, cdb)
+	ns, err := NewNurseryStore(&chainHash, cdb)
 	if err != nil {
 		t.Fatalf("unable to open nursery store: %v", err)
 	}
@@ -361,7 +363,7 @@ func TestNurseryStoreGraduate(t *testing.T) {
 
 // assertNumChanOutputs checks that the channel bucket has the expected number
 // of outputs.
-func assertNumChanOutputs(t *testing.T, ns NurseryStore,
+func assertNumChanOutputs(t *testing.T, ns NurseryStorer,
 	chanPoint *wire.OutPoint, expectedNum int) {
 
 	var count int
@@ -387,7 +389,7 @@ func assertNumChanOutputs(t *testing.T, ns NurseryStore,
 
 // assertNumPreschools loads all preschool outputs and verifies their count
 // matches the expected number.
-func assertNumPreschools(t *testing.T, ns NurseryStore, expected int) {
+func assertNumPreschools(t *testing.T, ns NurseryStorer, expected int) {
 	psclOutputs, err := ns.FetchPreschools()
 	if err != nil {
 		t.Fatalf("unable to retrieve preschool outputs: %v", err)
@@ -401,7 +403,7 @@ func assertNumPreschools(t *testing.T, ns NurseryStore, expected int) {
 
 // assertNumChannels checks that the nursery has a given number of active
 // channels.
-func assertNumChannels(t *testing.T, ns NurseryStore, expected int) {
+func assertNumChannels(t *testing.T, ns NurseryStorer, expected int) {
 	channels, err := ns.ListChannels()
 	if err != nil {
 		t.Fatalf("unable to fetch channels from nursery store: %v",
@@ -416,7 +418,7 @@ func assertNumChannels(t *testing.T, ns NurseryStore, expected int) {
 
 // assertHeightIsPurged checks that the finalized transaction, kindergarten, and
 // htlc outputs at a particular height are all nil.
-func assertHeightIsPurged(t *testing.T, ns NurseryStore,
+func assertHeightIsPurged(t *testing.T, ns NurseryStorer,
 	height uint32) {
 
 	kndrOutputs, cribOutputs, err := ns.FetchClass(height)
@@ -436,7 +438,7 @@ func assertHeightIsPurged(t *testing.T, ns NurseryStore,
 
 // assertCribAtExpiryHeight loads the class at the given height, and verifies
 // that the given htlc output is one of the crib outputs.
-func assertCribAtExpiryHeight(t *testing.T, ns NurseryStore,
+func assertCribAtExpiryHeight(t *testing.T, ns NurseryStorer,
 	htlcOutput *babyOutput) {
 
 	expiryHeight := htlcOutput.expiry
@@ -458,7 +460,7 @@ func assertCribAtExpiryHeight(t *testing.T, ns NurseryStore,
 
 // assertCribNotAtExpiryHeight loads the class at the given height, and verifies
 // that the given htlc output is not one of the crib outputs.
-func assertCribNotAtExpiryHeight(t *testing.T, ns NurseryStore,
+func assertCribNotAtExpiryHeight(t *testing.T, ns NurseryStorer,
 	htlcOutput *babyOutput) {
 
 	expiryHeight := htlcOutput.expiry
@@ -479,7 +481,7 @@ func assertCribNotAtExpiryHeight(t *testing.T, ns NurseryStore,
 // assertKndrAtMaturityHeight loads the class at the provided height and
 // verifies that the provided kid output is one of the kindergarten outputs
 // returned.
-func assertKndrAtMaturityHeight(t *testing.T, ns NurseryStore,
+func assertKndrAtMaturityHeight(t *testing.T, ns NurseryStorer,
 	kndrOutput *kidOutput) {
 
 	maturityHeight := kndrOutput.ConfHeight() +
@@ -503,7 +505,7 @@ func assertKndrAtMaturityHeight(t *testing.T, ns NurseryStore,
 // assertKndrNotAtMaturityHeight loads the class at the provided height and
 // verifies that the provided kid output is not one of the kindergarten outputs
 // returned.
-func assertKndrNotAtMaturityHeight(t *testing.T, ns NurseryStore,
+func assertKndrNotAtMaturityHeight(t *testing.T, ns NurseryStorer,
 	kndrOutput *kidOutput) {
 
 	maturityHeight := kndrOutput.ConfHeight() +
@@ -525,7 +527,7 @@ func assertKndrNotAtMaturityHeight(t *testing.T, ns NurseryStore,
 
 // assertChannelMaturity queries the nursery store for the maturity of the given
 // channel, failing if the result does not match the expectedMaturity.
-func assertChannelMaturity(t *testing.T, ns NurseryStore,
+func assertChannelMaturity(t *testing.T, ns NurseryStorer,
 	chanPoint *wire.OutPoint, expectedMaturity bool) {
 
 	isMature, err := ns.IsMatureChannel(chanPoint)
@@ -541,7 +543,7 @@ func assertChannelMaturity(t *testing.T, ns NurseryStore,
 
 // assertCanRemoveChannel tries to remove a channel from the nursery store,
 // failing if the result does match expected canRemove.
-func assertCanRemoveChannel(t *testing.T, ns NurseryStore,
+func assertCanRemoveChannel(t *testing.T, ns NurseryStorer,
 	chanPoint *wire.OutPoint, canRemove bool) {
 
 	err := ns.RemoveChannel(chanPoint)

contractcourt/utxonursery_test.go
@@ -1,7 +1,7 @@
 //go:build !rpctest
 // +build !rpctest
 
-package dcrlnd
+package contractcourt
 
 import (
 	"bytes"
@@ -402,14 +402,14 @@ func TestBabyOutputSerialization(t *testing.T) {
 }
 
 type nurseryTestContext struct {
-	nursery     *utxoNursery
+	nursery     *UtxoNursery
 	notifier    *sweep.MockNotifier
 	chainIO     *mock.ChainIO
 	publishChan chan wire.MsgTx
 	store       *nurseryStoreInterceptor
 	restart     func() bool
 	receiveTx   func() wire.MsgTx
-	sweeper     *mockSweeper
+	sweeper     *mockSweeperFull
 	timeoutChan chan chan time.Time
 	t           *testing.T
 	dbCleanup   func()
@@ -427,7 +427,7 @@ func createNurseryTestContext(t *testing.T,
 		t.Fatalf("unable to open channeldb: %v", err)
 	}
 
-	store, err := newNurseryStore(&chainhash.Hash{}, cdb)
+	store, err := NewNurseryStore(&chainhash.Hash{}, cdb)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -440,7 +440,7 @@ func createNurseryTestContext(t *testing.T,
 
 	publishChan := make(chan wire.MsgTx, 1)
 	publishFunc := func(tx *wire.MsgTx, source string) error {
-		utxnLog.Tracef("Publishing tx %v by %v", tx.TxHash(), source)
+		log.Tracef("Publishing tx %v by %v", tx.TxHash(), source)
 		publishChan <- *tx
 		return nil
 	}
@@ -451,7 +451,7 @@ func createNurseryTestContext(t *testing.T,
 		BestHeight: 0,
 	}
 
-	sweeper := newMockSweeper(t)
+	sweeper := newMockSweeperFull(t)
 
 	nurseryCfg := NurseryConfig{
 		Notifier: notifier,
@@ -473,7 +473,7 @@ func createNurseryTestContext(t *testing.T,
 		},
 	}
 
-	nursery := newUtxoNursery(&nurseryCfg)
+	nursery := NewUtxoNursery(&nurseryCfg)
 	nursery.Start()
 
 	ctx := &nurseryTestContext{
@@ -492,7 +492,7 @@ func createNurseryTestContext(t *testing.T,
 		var tx wire.MsgTx
 		select {
 		case tx = <-ctx.publishChan:
-			utxnLog.Debugf("Published tx %v", tx.TxHash())
+			log.Debugf("Published tx %v", tx.TxHash())
 			return tx
 		case <-time.After(defaultTestTimeout):
 			pprof.Lookup("goroutine").WriteTo(os.Stdout, 1)
@@ -504,16 +504,16 @@ func createNurseryTestContext(t *testing.T,
 
 	ctx.restart = func() bool {
 		return checkStartStop(func() {
-			utxnLog.Tracef("Restart sweeper and nursery")
+			log.Tracef("Restart sweeper and nursery")
 			// Simulate lnd restart.
 			ctx.nursery.Stop()
 
 			// Restart sweeper.
-			ctx.sweeper = newMockSweeper(t)
+			ctx.sweeper = newMockSweeperFull(t)
 
 			/// Restart nursery.
 			nurseryCfg.SweepInput = ctx.sweeper.sweepInput
-			ctx.nursery = newUtxoNursery(&nurseryCfg)
+			ctx.nursery = NewUtxoNursery(&nurseryCfg)
 			ctx.nursery.Start()
 
 		})
@@ -632,7 +632,7 @@ func createOutgoingRes(onLocalCommitment bool) *lnwallet.OutgoingHtlcResolution 
 	return &outgoingRes
 }
 
-func incubateTestOutput(t *testing.T, nursery *utxoNursery,
+func incubateTestOutput(t *testing.T, nursery *UtxoNursery,
 	onLocalCommitment bool) *lnwallet.OutgoingHtlcResolution {
 
 	outgoingRes := createOutgoingRes(onLocalCommitment)
@@ -658,7 +658,7 @@ func incubateTestOutput(t *testing.T, nursery *utxoNursery,
 	return outgoingRes
 }
 
-func assertNurseryReport(t *testing.T, nursery *utxoNursery,
+func assertNurseryReport(t *testing.T, nursery *UtxoNursery,
 	expectedNofHtlcs int, expectedStage uint32,
 	expectedLimboBalance dcrutil.Amount) {
 	report, err := nursery.NurseryReport(&testChanPoint)
@@ -666,27 +666,27 @@ func assertNurseryReport(t *testing.T, nursery *utxoNursery,
 		t.Fatal(err)
 	}
 
-	if len(report.htlcs) != expectedNofHtlcs {
+	if len(report.Htlcs) != expectedNofHtlcs {
 		t.Fatalf("expected %v outputs to be reported, but report "+
-			"only contains %v", expectedNofHtlcs, len(report.htlcs))
+			"only contains %v", expectedNofHtlcs, len(report.Htlcs))
 	}
 
 	if expectedNofHtlcs != 0 {
-		htlcReport := report.htlcs[0]
-		if htlcReport.stage != expectedStage {
+		htlcReport := report.Htlcs[0]
+		if htlcReport.Stage != expectedStage {
 			t.Fatalf("expected htlc be advanced to stage %v, but "+
 				"it is reported in stage %v",
-				expectedStage, htlcReport.stage)
+				expectedStage, htlcReport.Stage)
 		}
 	}
 
-	if report.limboBalance != expectedLimboBalance {
+	if report.LimboBalance != expectedLimboBalance {
 		t.Fatalf("expected limbo balance to be %v, but it is %v instead",
-			expectedLimboBalance, report.limboBalance)
+			expectedLimboBalance, report.LimboBalance)
 	}
 }
 
-func assertNurseryReportUnavailable(t *testing.T, nursery *utxoNursery) {
+func assertNurseryReportUnavailable(t *testing.T, nursery *UtxoNursery) {
 	_, err := nursery.NurseryReport(&testChanPoint)
 	if err != ErrContractNotFound {
 		t.Fatal("expected report to be unavailable")
@@ -716,7 +716,7 @@ func testRestartLoop(t *testing.T, test func(*testing.T,
 
 				return true
 			}
-			utxnLog.Debugf("Skipping restart point %v",
+			log.Debugf("Skipping restart point %v",
 				currentStartStopIdx)
 			return false
 		}
@@ -868,7 +868,7 @@ func testSweep(t *testing.T, ctx *nurseryTestContext,
 }
 
 type nurseryStoreInterceptor struct {
-	ns NurseryStore
+	ns NurseryStorer
 
 	// TODO(joostjager): put more useful info through these channels.
 	cribToKinderChan      chan struct{}
@@ -877,7 +877,7 @@ type nurseryStoreInterceptor struct {
 	preschoolToKinderChan chan struct{}
 }
 
-func newNurseryStoreInterceptor(ns NurseryStore) *nurseryStoreInterceptor {
+func newNurseryStoreInterceptor(ns NurseryStorer) *nurseryStoreInterceptor {
 	return &nurseryStoreInterceptor{
 		ns:                    ns,
 		cribToKinderChan:      make(chan struct{}),
@@ -955,7 +955,7 @@ func (i *nurseryStoreInterceptor) RemoveChannel(chanPoint *wire.OutPoint) error 
 	return i.ns.RemoveChannel(chanPoint)
 }
 
-type mockSweeper struct {
+type mockSweeperFull struct {
 	lock sync.Mutex
 
 	resultChans map[wire.OutPoint]chan sweep.Result
@@ -964,18 +964,18 @@ type mockSweeper struct {
 	sweepChan chan input.Input
 }
 
-func newMockSweeper(t *testing.T) *mockSweeper {
-	return &mockSweeper{
+func newMockSweeperFull(t *testing.T) *mockSweeperFull {
+	return &mockSweeperFull{
 		resultChans: make(map[wire.OutPoint]chan sweep.Result),
 		sweepChan:   make(chan input.Input, 1),
 		t:           t,
 	}
 }
 
-func (s *mockSweeper) sweepInput(input input.Input,
+func (s *mockSweeperFull) sweepInput(input input.Input,
 	_ sweep.Params) (chan sweep.Result, error) {
 
-	utxnLog.Debugf("mockSweeper sweepInput called for %v", *input.OutPoint())
+	log.Debugf("mockSweeperFull sweepInput called for %v", *input.OutPoint())
 
 	select {
 	case s.sweepChan <- input:
@@ -992,7 +992,7 @@ func (s *mockSweeper) sweepInput(input input.Input,
 	return c, nil
 }
 
-func (s *mockSweeper) expectSweep() {
+func (s *mockSweeperFull) expectSweep() {
 	s.t.Helper()
 
 	select {
@@ -1002,7 +1002,7 @@ func (s *mockSweeper) expectSweep() {
 	}
 }
 
-func (s *mockSweeper) sweepAll() {
+func (s *mockSweeperFull) sweepAll() {
 	s.t.Helper()
 
 	s.lock.Lock()
@@ -1011,7 +1011,7 @@ func (s *mockSweeper) sweepAll() {
 	s.lock.Unlock()
 
 	for o, c := range currentChans {
-		utxnLog.Debugf("mockSweeper signal swept for %v", o)
+		log.Debugf("mockSweeperFull signal swept for %v", o)
 
 		select {
 		case c <- sweep.Result{}: