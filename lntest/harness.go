lightningnetwork/lnd

lntest: count unconfirmed balance as well
It looks like in some cases (apparently mostly on Windows) a previous
sub test sometimes leaves some unconfirmed balance in the wallet. That
balance is confirmed in the next test when new coins are sent to the
wallet.

lntest/harness.go
@@ -1615,7 +1615,9 @@ func (n *NetworkHarness) sendCoins(amt btcutil.Amount, target *HarnessNode,
 		return err
 	}
 
-	expectedBalance := btcutil.Amount(initialBalance.ConfirmedBalance) + amt
+	fullInitialBalance := initialBalance.ConfirmedBalance +
+		initialBalance.UnconfirmedBalance
+	expectedBalance := btcutil.Amount(fullInitialBalance) + amt
 	return target.WaitForBalance(expectedBalance, true)
 }
 

decred/dcrlnd

lntest/harness.go
@@ -1696,7 +1696,9 @@ func (n *NetworkHarness) sendCoins(amt dcrutil.Amount, target *HarnessNode,
 	}
 
 	// Ensure the balance is as expected.
-	expectedBalance := dcrutil.Amount(initialBalance.ConfirmedBalance) + amt
+	fullInitialBalance := initialBalance.ConfirmedBalance +
+		initialBalance.UnconfirmedBalance
+	expectedBalance := dcrutil.Amount(fullInitialBalance) + amt
 	return target.WaitForBalance(expectedBalance, true)
 }
 