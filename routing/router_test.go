lightningnetwork/lnd

routing: add wait.NoError to TestBlockDifferenceFix assertion
This fixes a flake I've seen in the wild lately:
```
--- FAIL: TestBlockDifferenceFix (0.01s)
    router_test.go:4335: height should have been updated to 5, instead got 4
FAIL
FAIL	github.com/lightningnetwork/lnd/routing	3.865s
FAIL
```

We wrap things in an assertion loop to ensure that timing quirks don't
cause the test to fail sporadically.

routing/router_test.go
@@ -23,6 +23,7 @@ import (
 	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/htlcswitch"
 	lnmock "github.com/lightningnetwork/lnd/lntest/mock"
+	"github.com/lightningnetwork/lnd/lntest/wait"
 	"github.com/lightningnetwork/lnd/lntypes"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/record"
@@ -4274,6 +4275,7 @@ func TestBlockDifferenceFix(t *testing.T) {
 	t.Parallel()
 
 	initialBlockHeight := uint32(0)
+
 	// Starting height here is set to 0, which is behind where we want to be.
 	ctx, cleanup := createTestCtxSingleNode(t, initialBlockHeight)
 	defer cleanup()
@@ -4330,9 +4332,17 @@ func TestBlockDifferenceFix(t *testing.T) {
 		<-ctx.chainView.notifyBlockAck
 	}
 
-	// Then router height should be updated to the latest block.
-	if atomic.LoadUint32(&ctx.router.bestHeight) != newBlockHeight {
-		t.Fatalf("height should have been updated to %v, instead got "+
-			"%v", newBlockHeight, ctx.router.bestHeight)
+	err := wait.NoError(func() error {
+		// Then router height should be updated to the latest block.
+		if atomic.LoadUint32(&ctx.router.bestHeight) != newBlockHeight {
+			return fmt.Errorf("height should have been updated "+
+				"to %v, instead got %v", newBlockHeight,
+				ctx.router.bestHeight)
+		}
+
+		return nil
+	}, testTimeout)
+	if err != nil {
+		t.Fatalf("block height wasn't updated: %v", err)
 	}
 }

decred/dcrlnd

routing/router_test.go
@@ -24,6 +24,7 @@ import (
 	"github.com/decred/dcrlnd/clock"
 	"github.com/decred/dcrlnd/htlcswitch"
 	lnmock "github.com/decred/dcrlnd/lntest/mock"
+	"github.com/decred/dcrlnd/lntest/wait"
 	"github.com/decred/dcrlnd/lntypes"
 	"github.com/decred/dcrlnd/lnwire"
 	"github.com/decred/dcrlnd/record"
@@ -4275,6 +4276,7 @@ func TestBlockDifferenceFix(t *testing.T) {
 	t.Parallel()
 
 	initialBlockHeight := uint32(0)
+
 	// Starting height here is set to 0, which is behind where we want to be.
 	ctx, cleanup := createTestCtxSingleNode(t, initialBlockHeight)
 	defer cleanup()
@@ -4331,9 +4333,17 @@ func TestBlockDifferenceFix(t *testing.T) {
 		<-ctx.chainView.notifyBlockAck
 	}
 
-	// Then router height should be updated to the latest block.
-	if atomic.LoadUint32(&ctx.router.bestHeight) != newBlockHeight {
-		t.Fatalf("height should have been updated to %v, instead got "+
-			"%v", newBlockHeight, ctx.router.bestHeight)
+	err := wait.NoError(func() error {
+		// Then router height should be updated to the latest block.
+		if atomic.LoadUint32(&ctx.router.bestHeight) != newBlockHeight {
+			return fmt.Errorf("height should have been updated "+
+				"to %v, instead got %v", newBlockHeight,
+				ctx.router.bestHeight)
+		}
+
+		return nil
+	}, testTimeout)
+	if err != nil {
+		t.Fatalf("block height wasn't updated: %v", err)
 	}
 }