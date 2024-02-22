lightningnetwork/lnd

multi: refactor SignMessage to specify hashing

funding/manager_test.go
@@ -362,7 +362,7 @@ func createTestFundingManager(t *testing.T, privKey *btcec.PrivateKey,
 		Notifier:     chainNotifier,
 		FeeEstimator: estimator,
 		SignMessage: func(_ keychain.KeyLocator,
-			_ []byte) (*btcec.Signature, error) {
+			_ []byte, _ bool) (*btcec.Signature, error) {
 
 			return testSig, nil
 		},
@@ -510,7 +510,7 @@ func recreateAliceFundingManager(t *testing.T, alice *testNode) {
 		Notifier:     oldCfg.Notifier,
 		FeeEstimator: oldCfg.FeeEstimator,
 		SignMessage: func(_ keychain.KeyLocator,
-			_ []byte) (*btcec.Signature, error) {
+			_ []byte, _ bool) (*btcec.Signature, error) {
 
 			return testSig, nil
 		},

lntest/mock/signer.go
@@ -116,7 +116,7 @@ func (s *SingleSigner) ComputeInputScript(tx *wire.MsgTx,
 // SignMessage takes a public key and a message and only signs the message
 // with the stored private key if the public key matches the private key.
 func (s *SingleSigner) SignMessage(keyLoc keychain.KeyLocator,
-	msg []byte) (*btcec.Signature, error) {
+	msg []byte, doubleHash bool) (*btcec.Signature, error) {
 
 	mockKeyLoc := s.KeyLoc
 	if s.KeyLoc.IsEmpty() {
@@ -127,7 +127,12 @@ func (s *SingleSigner) SignMessage(keyLoc keychain.KeyLocator,
 		return nil, fmt.Errorf("unknown public key")
 	}
 
-	digest := chainhash.DoubleHashB(msg)
+	var digest []byte
+	if doubleHash {
+		digest = chainhash.DoubleHashB(msg)
+	} else {
+		digest = chainhash.HashB(msg)
+	}
 	sign, err := s.Privkey.Sign(digest)
 	if err != nil {
 		return nil, fmt.Errorf("can't sign the message: %v", err)

lnwallet/interface.go
@@ -442,9 +442,9 @@ type MessageSigner interface {
 	// SignMessage attempts to sign a target message with the private key
 	// described in the key locator. If the target private key is unable to
 	// be found, then an error will be returned. The actual digest signed is
-	// the double SHA-256 of the passed message.
-	SignMessage(keyLoc keychain.KeyLocator, msg []byte) (*btcec.Signature,
-		error)
+	// the single or double SHA-256 of the passed message.
+	SignMessage(keyLoc keychain.KeyLocator, msg []byte,
+		doubleHash bool) (*btcec.Signature, error)
 }
 
 // WalletDriver represents a "driver" for a particular concrete

netann/channel_update_test.go
@@ -18,7 +18,7 @@ type mockSigner struct {
 }
 
 func (m *mockSigner) SignMessage(_ keychain.KeyLocator,
-	_ []byte) (*btcec.Signature, error) {
+	_ []byte, _ bool) (*btcec.Signature, error) {
 
 	if m.err != nil {
 		return nil, m.err

netann/node_signer.go
@@ -26,7 +26,7 @@ func NewNodeSigner(keySigner keychain.SingleKeyMessageSigner) *NodeSigner {
 // resident node's private key described in the key locator. If the target key
 // locator is _not_ the node's private key, then an error will be returned.
 func (n *NodeSigner) SignMessage(keyLoc keychain.KeyLocator,
-	msg []byte) (*btcec.Signature, error) {
+	msg []byte, doubleHash bool) (*btcec.Signature, error) {
 
 	// If this isn't our identity public key, then we'll exit early with an
 	// error as we can't sign with this key.
@@ -35,7 +35,7 @@ func (n *NodeSigner) SignMessage(keyLoc keychain.KeyLocator,
 	}
 
 	// Otherwise, we'll sign the double-sha256 of the target message.
-	sig, err := n.keySigner.SignMessage(msg, true)
+	sig, err := n.keySigner.SignMessage(msg, doubleHash)
 	if err != nil {
 		return nil, fmt.Errorf("can't sign the message: %v", err)
 	}

decred/dcrlnd

funding/manager_test.go
@@ -398,7 +398,7 @@ func createTestFundingManager(t *testing.T, privKey *secp256k1.PrivateKey,
 		Notifier:     chainNotifier,
 		FeeEstimator: estimator,
 		SignMessage: func(_ keychain.KeyLocator,
-			_ []byte) (*ecdsa.Signature, error) {
+			_ []byte, _ bool) (*ecdsa.Signature, error) {
 
 			return testSig, nil
 		},
@@ -546,7 +546,7 @@ func recreateAliceFundingManager(t *testing.T, alice *testNode) {
 		Notifier:     oldCfg.Notifier,
 		FeeEstimator: oldCfg.FeeEstimator,
 		SignMessage: func(_ keychain.KeyLocator,
-			_ []byte) (*ecdsa.Signature, error) {
+			_ []byte, _ bool) (*ecdsa.Signature, error) {
 
 			return testSig, nil
 		},

lntest/mock/signer.go
@@ -114,7 +114,7 @@ func (s *SingleSigner) ComputeInputScript(tx *wire.MsgTx,
 // SignMessage takes a public key and a message and only signs the message
 // with the stored private key if the public key matches the private key.
 func (s *SingleSigner) SignMessage(keyLoc keychain.KeyLocator,
-	msg []byte) (*ecdsa.Signature, error) {
+	msg []byte, doubleHash bool) (*ecdsa.Signature, error) {
 
 	mockKeyLoc := s.KeyLoc
 	if s.KeyLoc.IsEmpty() {
@@ -125,7 +125,13 @@ func (s *SingleSigner) SignMessage(keyLoc keychain.KeyLocator,
 		return nil, fmt.Errorf("unknown public key")
 	}
 
-	digest := chainhash.HashB(msg)
+	var digest []byte
+	if doubleHash {
+		digest1 := chainhash.HashB(msg)
+		digest = chainhash.HashB(digest1)
+	} else {
+		digest = chainhash.HashB(msg)
+	}
 	sign := ecdsa.Sign(s.Privkey, digest)
 	return sign, nil
 }

lnwallet/interface.go
@@ -453,8 +453,8 @@ type MessageSigner interface {
 	// described in the key locator. If the target private key is unable to
 	// be found, then an error will be returned. The actual digest signed is
 	// the chainhash of the passed message.
-	SignMessage(keyLoc keychain.KeyLocator, msg []byte) (*ecdsa.Signature,
-		error)
+	SignMessage(keyLoc keychain.KeyLocator, msg []byte,
+		doubleHash bool) (*ecdsa.Signature, error)
 }
 
 // WalletDriver represents a "driver" for a particular concrete

netann/channel_update_test.go
@@ -19,7 +19,7 @@ type mockSigner struct {
 }
 
 func (m *mockSigner) SignMessage(_ keychain.KeyLocator,
-	_ []byte) (*ecdsa.Signature, error) {
+	_ []byte, _ bool) (*ecdsa.Signature, error) {
 
 	if m.err != nil {
 		return nil, m.err

netann/node_signer.go
@@ -26,7 +26,7 @@ func NewNodeSigner(keySigner keychain.SingleKeyMessageSigner) *NodeSigner {
 // resident node's private key described in the key locator. If the target key
 // locator is _not_ the node's private key, then an error will be returned.
 func (n *NodeSigner) SignMessage(keyLoc keychain.KeyLocator,
-	msg []byte) (*ecdsa.Signature, error) {
+	msg []byte, doubleHash bool) (*ecdsa.Signature, error) {
 
 	// If this isn't our identity public key, then we'll exit early with an
 	// error as we can't sign with this key.
@@ -35,7 +35,7 @@ func (n *NodeSigner) SignMessage(keyLoc keychain.KeyLocator,
 	}
 
 	// Otherwise, we'll sign the chainhash of the target message.
-	sig, err := n.keySigner.SignMessage(msg, false)
+	sig, err := n.keySigner.SignMessage(msg, doubleHash)
 	if err != nil {
 		return nil, fmt.Errorf("can't sign the message: %v", err)
 	}