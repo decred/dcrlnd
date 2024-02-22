joostjager/lnd

kvdb/test: generalize etcd tests

kvdb/etcd/db_test.go
@@ -38,8 +38,8 @@ func TestCopy(t *testing.T) {
 	require.Nil(t, err)
 
 	expected := map[string]string{
-		bkey("apple"):        bval("apple"),
-		vkey("key", "apple"): "val",
+		Bkey("apple"):        Bval("apple"),
+		Vkey("key", "apple"): "val",
 	}
 	require.Equal(t, expected, f.Dump())
 }

kvdb/etcd/readwrite_tx_test.go
@@ -10,70 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTxManualCommit(t *testing.T) {
-	t.Parallel()
-
-	f := NewEtcdTestFixture(t)
-	defer f.Cleanup()
-
-	db, err := newEtcdBackend(context.TODO(), f.BackendConfig())
-	require.NoError(t, err)
-
-	tx, err := db.BeginReadWriteTx()
-	require.NoError(t, err)
-	require.NotNil(t, tx)
-
-	committed := false
-
-	tx.OnCommit(func() {
-		committed = true
-	})
-
-	apple, err := tx.CreateTopLevelBucket([]byte("apple"))
-	require.NoError(t, err)
-	require.NotNil(t, apple)
-	require.NoError(t, apple.Put([]byte("testKey"), []byte("testVal")))
-
-	banana, err := tx.CreateTopLevelBucket([]byte("banana"))
-	require.NoError(t, err)
-	require.NotNil(t, banana)
-	require.NoError(t, banana.Put([]byte("testKey"), []byte("testVal")))
-	require.NoError(t, tx.DeleteTopLevelBucket([]byte("banana")))
-
-	require.NoError(t, tx.Commit())
-	require.True(t, committed)
-
-	expected := map[string]string{
-		bkey("apple"):            bval("apple"),
-		vkey("testKey", "apple"): "testVal",
-	}
-	require.Equal(t, expected, f.Dump())
-}
-
-func TestTxRollback(t *testing.T) {
-	t.Parallel()
-
-	f := NewEtcdTestFixture(t)
-	defer f.Cleanup()
-
-	db, err := newEtcdBackend(context.TODO(), f.BackendConfig())
-	require.NoError(t, err)
-
-	tx, err := db.BeginReadWriteTx()
-	require.Nil(t, err)
-	require.NotNil(t, tx)
-
-	apple, err := tx.CreateTopLevelBucket([]byte("apple"))
-	require.Nil(t, err)
-	require.NotNil(t, apple)
-
-	require.NoError(t, apple.Put([]byte("testKey"), []byte("testVal")))
-
-	require.NoError(t, tx.Rollback())
-	require.Error(t, walletdb.ErrTxClosed, tx.Commit())
-	require.Equal(t, map[string]string{}, f.Dump())
-}
-
 func TestChangeDuringManualTx(t *testing.T) {
 	t.Parallel()
 
@@ -94,12 +30,12 @@ func TestChangeDuringManualTx(t *testing.T) {
 	require.NoError(t, apple.Put([]byte("testKey"), []byte("testVal")))
 
 	// Try overwriting the bucket key.
-	f.Put(bkey("apple"), "banana")
+	f.Put(Bkey("apple"), "banana")
 
 	// TODO: translate error
 	require.NotNil(t, tx.Commit())
 	require.Equal(t, map[string]string{
-		bkey("apple"): "banana",
+		Bkey("apple"): "banana",
 	}, f.Dump())
 }
 
@@ -122,8 +58,8 @@ func TestChangeDuringUpdate(t *testing.T) {
 		require.NoError(t, apple.Put([]byte("key"), []byte("value")))
 
 		if count == 0 {
-			f.Put(vkey("key", "apple"), "new_value")
-			f.Put(vkey("key2", "apple"), "value2")
+			f.Put(Vkey("key", "apple"), "new_value")
+			f.Put(Vkey("key2", "apple"), "value2")
 		}
 
 		cursor := apple.ReadCursor()
@@ -149,9 +85,9 @@ func TestChangeDuringUpdate(t *testing.T) {
 	require.Equal(t, count, 2)
 
 	expected := map[string]string{
-		bkey("apple"):         bval("apple"),
-		vkey("key", "apple"):  "value",
-		vkey("key2", "apple"): "value2",
+		Bkey("apple"):         Bval("apple"),
+		Vkey("key", "apple"):  "value",
+		Vkey("key2", "apple"): "value2",
 	}
 	require.Equal(t, expected, f.Dump())
 }

decred/dcrlnd

kvdb/etcd/db_test.go
@@ -39,8 +39,8 @@ func TestCopy(t *testing.T) {
 	require.Nil(t, err)
 
 	expected := map[string]string{
-		bkey("apple"):        bval("apple"),
-		vkey("key", "apple"): "val",
+		BucketKey("apple"):        BucketVal("apple"),
+		ValueKey("key", "apple"): "val",
 	}
 	require.Equal(t, expected, f.Dump())
 }

kvdb/etcd/readwrite_tx_test.go
@@ -11,70 +11,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTxManualCommit(t *testing.T) {
-	t.Parallel()
-
-	f := NewEtcdTestFixture(t)
-	defer f.Cleanup()
-
-	db, err := newEtcdBackend(context.TODO(), f.BackendConfig())
-	require.NoError(t, err)
-
-	tx, err := db.BeginReadWriteTx()
-	require.NoError(t, err)
-	require.NotNil(t, tx)
-
-	committed := false
-
-	tx.OnCommit(func() {
-		committed = true
-	})
-
-	apple, err := tx.CreateTopLevelBucket([]byte("apple"))
-	require.NoError(t, err)
-	require.NotNil(t, apple)
-	require.NoError(t, apple.Put([]byte("testKey"), []byte("testVal")))
-
-	banana, err := tx.CreateTopLevelBucket([]byte("banana"))
-	require.NoError(t, err)
-	require.NotNil(t, banana)
-	require.NoError(t, banana.Put([]byte("testKey"), []byte("testVal")))
-	require.NoError(t, tx.DeleteTopLevelBucket([]byte("banana")))
-
-	require.NoError(t, tx.Commit())
-	require.True(t, committed)
-
-	expected := map[string]string{
-		bkey("apple"):            bval("apple"),
-		vkey("testKey", "apple"): "testVal",
-	}
-	require.Equal(t, expected, f.Dump())
-}
-
-func TestTxRollback(t *testing.T) {
-	t.Parallel()
-
-	f := NewEtcdTestFixture(t)
-	defer f.Cleanup()
-
-	db, err := newEtcdBackend(context.TODO(), f.BackendConfig())
-	require.NoError(t, err)
-
-	tx, err := db.BeginReadWriteTx()
-	require.Nil(t, err)
-	require.NotNil(t, tx)
-
-	apple, err := tx.CreateTopLevelBucket([]byte("apple"))
-	require.Nil(t, err)
-	require.NotNil(t, apple)
-
-	require.NoError(t, apple.Put([]byte("testKey"), []byte("testVal")))
-
-	require.NoError(t, tx.Rollback())
-	require.Error(t, walletdb.ErrTxClosed, tx.Commit())
-	require.Equal(t, map[string]string{}, f.Dump())
-}
-
 func TestChangeDuringManualTx(t *testing.T) {
 	t.Parallel()
 
@@ -95,12 +31,12 @@ func TestChangeDuringManualTx(t *testing.T) {
 	require.NoError(t, apple.Put([]byte("testKey"), []byte("testVal")))
 
 	// Try overwriting the bucket key.
-	f.Put(bkey("apple"), "banana")
+	f.Put(BucketKey("apple"), "banana")
 
 	// TODO: translate error
 	require.NotNil(t, tx.Commit())
 	require.Equal(t, map[string]string{
-		bkey("apple"): "banana",
+		BucketKey("apple"): "banana",
 	}, f.Dump())
 }
 
@@ -123,8 +59,8 @@ func TestChangeDuringUpdate(t *testing.T) {
 		require.NoError(t, apple.Put([]byte("key"), []byte("value")))
 
 		if count == 0 {
-			f.Put(vkey("key", "apple"), "new_value")
-			f.Put(vkey("key2", "apple"), "value2")
+			f.Put(ValueKey("key", "apple"), "new_value")
+			f.Put(ValueKey("key2", "apple"), "value2")
 		}
 
 		cursor := apple.ReadCursor()
@@ -150,9 +86,9 @@ func TestChangeDuringUpdate(t *testing.T) {
 	require.Equal(t, count, 2)
 
 	expected := map[string]string{
-		bkey("apple"):         bval("apple"),
-		vkey("key", "apple"):  "value",
-		vkey("key2", "apple"): "value2",
+		BucketKey("apple"):         BucketVal("apple"),
+		ValueKey("key", "apple"):  "value",
+		ValueKey("key2", "apple"): "value2",
 	}
 	require.Equal(t, expected, f.Dump())
 }