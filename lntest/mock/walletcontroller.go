lightningnetwork/lnd

multi: fix formatting

lntest/mock/walletcontroller.go
@@ -54,14 +54,15 @@ func (w *WalletController) FetchInputInfo(
 }
 
 // ConfirmedBalance currently returns dummy values.
-func (w *WalletController) ConfirmedBalance(confs int32,
-	_ string) (btcutil.Amount, error) {
+func (w *WalletController) ConfirmedBalance(int32, string) (btcutil.Amount,
+	error) {
+
 	return 0, nil
 }
 
 // NewAddress is called to get new addresses for delivery, change etc.
-func (w *WalletController) NewAddress(addrType lnwallet.AddressType,
-	change bool, _ string) (btcutil.Address, error) {
+func (w *WalletController) NewAddress(lnwallet.AddressType, bool,
+	string) (btcutil.Address, error) {
 
 	addr, _ := btcutil.NewAddressPubKey(
 		w.RootKey.PubKey().SerializeCompressed(), &chaincfg.MainNetParams,
@@ -70,19 +71,21 @@ func (w *WalletController) NewAddress(addrType lnwallet.AddressType,
 }
 
 // LastUnusedAddress currently returns dummy values.
-func (w *WalletController) LastUnusedAddress(addrType lnwallet.AddressType,
-	_ string) (btcutil.Address, error) {
+func (w *WalletController) LastUnusedAddress(lnwallet.AddressType,
+	string) (btcutil.Address, error) {
+
 	return nil, nil
 }
 
 // IsOurAddress currently returns a dummy value.
-func (w *WalletController) IsOurAddress(a btcutil.Address) bool {
+func (w *WalletController) IsOurAddress(btcutil.Address) bool {
 	return false
 }
 
 // ListAccounts currently returns a dummy value.
-func (w *WalletController) ListAccounts(_ string,
-	_ *waddrmgr.KeyScope) ([]*waddrmgr.AccountProperties, error) {
+func (w *WalletController) ListAccounts(string,
+	*waddrmgr.KeyScope) ([]*waddrmgr.AccountProperties, error) {
+
 	return nil, nil
 }
 
@@ -90,33 +93,35 @@ func (w *WalletController) ListAccounts(_ string,
 func (w *WalletController) ImportAccount(string, *hdkeychain.ExtendedKey,
 	uint32, *waddrmgr.AddressType, bool) (*waddrmgr.AccountProperties,
 	[]btcutil.Address, []btcutil.Address, error) {
+
 	return nil, nil, nil, nil
 }
 
 // ImportPublicKey currently returns a dummy value.
 func (w *WalletController) ImportPublicKey(*btcec.PublicKey,
 	waddrmgr.AddressType) error {
+
 	return nil
 }
 
 // SendOutputs currently returns dummy values.
-func (w *WalletController) SendOutputs(outputs []*wire.TxOut,
-	_ chainfee.SatPerKWeight, _ int32, _ string) (*wire.MsgTx, error) {
+func (w *WalletController) SendOutputs([]*wire.TxOut,
+	chainfee.SatPerKWeight, int32, string) (*wire.MsgTx, error) {
 
 	return nil, nil
 }
 
 // CreateSimpleTx currently returns dummy values.
-func (w *WalletController) CreateSimpleTx(outputs []*wire.TxOut,
-	_ chainfee.SatPerKWeight, _ int32, _ bool) (*txauthor.AuthoredTx, error) {
+func (w *WalletController) CreateSimpleTx([]*wire.TxOut,
+	chainfee.SatPerKWeight, int32, bool) (*txauthor.AuthoredTx, error) {
 
 	return nil, nil
 }
 
 // ListUnspentWitness is called by the wallet when doing coin selection. We just
 // need one unspent for the funding transaction.
-func (w *WalletController) ListUnspentWitness(minConfs,
-	maxConfs int32, _ string) ([]*lnwallet.Utxo, error) {
+func (w *WalletController) ListUnspentWitness(int32, int32,
+	string) ([]*lnwallet.Utxo, error) {
 
 	// If the mock already has a list of utxos, return it.
 	if w.Utxos != nil {
@@ -140,8 +145,8 @@ func (w *WalletController) ListUnspentWitness(minConfs,
 }
 
 // ListTransactionDetails currently returns dummy values.
-func (w *WalletController) ListTransactionDetails(_,
-	_ int32, _ string) ([]*lnwallet.TransactionDetail, error) {
+func (w *WalletController) ListTransactionDetails(int32, int32,
+	string) ([]*lnwallet.TransactionDetail, error) {
 
 	return nil, nil
 }
@@ -169,8 +174,8 @@ func (w *WalletController) ListLeasedOutputs() ([]*wtxmgr.LockedOutput, error) {
 }
 
 // FundPsbt currently does nothing.
-func (w *WalletController) FundPsbt(_ *psbt.Packet, _ int32,
-	_ chainfee.SatPerKWeight, _ string) (int32, error) {
+func (w *WalletController) FundPsbt(*psbt.Packet, int32, chainfee.SatPerKWeight,
+	string) (int32, error) {
 
 	return 0, nil
 }
@@ -187,8 +192,8 @@ func (w *WalletController) PublishTransaction(tx *wire.MsgTx, _ string) error {
 }
 
 // LabelTransaction currently does nothing.
-func (w *WalletController) LabelTransaction(_ chainhash.Hash, _ string,
-	_ bool) error {
+func (w *WalletController) LabelTransaction(chainhash.Hash, string,
+	bool) error {
 
 	return nil
 }

lnwallet/interface.go
@@ -168,7 +168,8 @@ type WalletController interface {
 	// NOTE: Only witness outputs should be included in the computation of
 	// the total spendable balance of the wallet. We require this as only
 	// witness inputs can be used for funding channels.
-	ConfirmedBalance(confs int32, accountFilter string) (btcutil.Amount, error)
+	ConfirmedBalance(confs int32, accountFilter string) (btcutil.Amount,
+		error)
 
 	// NewAddress returns the next external or internal address for the
 	// wallet dictated by the value of the `change` parameter. If change is
@@ -197,7 +198,8 @@ type WalletController interface {
 	// ListAccounts retrieves all accounts belonging to the wallet by
 	// default. A name and key scope filter can be provided to filter
 	// through all of the wallet accounts and return only those matching.
-	ListAccounts(string, *waddrmgr.KeyScope) ([]*waddrmgr.AccountProperties, error)
+	ListAccounts(string, *waddrmgr.KeyScope) ([]*waddrmgr.AccountProperties,
+		error)
 
 	// ImportAccount imports an account backed by an account extended public
 	// key. The master key fingerprint denotes the fingerprint of the root
@@ -228,7 +230,8 @@ type WalletController interface {
 	// in the case of legacy versions (xpub, tpub), an address type must be
 	// specified as we intend to not support importing BIP-44 keys into the
 	// wallet using the legacy pay-to-pubkey-hash (P2PKH) scheme.
-	ImportPublicKey(pubKey *btcec.PublicKey, addrType waddrmgr.AddressType) error
+	ImportPublicKey(pubKey *btcec.PublicKey,
+		addrType waddrmgr.AddressType) error
 
 	// SendOutputs funds, signs, and broadcasts a Bitcoin transaction paying
 	// out to the specified outputs. In the case the wallet has insufficient
@@ -237,8 +240,8 @@ type WalletController interface {
 	// be used when crafting the transaction.
 	//
 	// NOTE: This method requires the global coin selection lock to be held.
-	SendOutputs(outputs []*wire.TxOut,
-		feeRate chainfee.SatPerKWeight, minConfs int32, label string) (*wire.MsgTx, error)
+	SendOutputs(outputs []*wire.TxOut, feeRate chainfee.SatPerKWeight,
+		minConfs int32, label string) (*wire.MsgTx, error)
 
 	// CreateSimpleTx creates a Bitcoin transaction paying to the specified
 	// outputs. The transaction is not broadcasted to the network. In the

decred/dcrlnd

lntest/mock/walletcontroller.go
@@ -54,13 +54,15 @@ func (w *WalletController) FetchInputInfo(
 }
 
 // ConfirmedBalance currently returns dummy values.
-func (w *WalletController) ConfirmedBalance(confs int32, account string) (dcrutil.Amount, error) {
+func (w *WalletController) ConfirmedBalance(int32, string) (dcrutil.Amount,
+	error) {
+
 	return 0, nil
 }
 
 // NewAddress is called to get new addresses for delivery, change etc.
-func (w *WalletController) NewAddress(addrType lnwallet.AddressType,
-	change bool, account string) (stdaddr.Address, error) {
+func (w *WalletController) NewAddress(lnwallet.AddressType, bool,
+	string) (stdaddr.Address, error) {
 
 	addr, _ := stdaddr.NewAddressPubKeyEcdsaSecp256k1V0(
 		w.RootKey.PubKey(), chaincfg.RegNetParams(),
@@ -69,26 +71,28 @@ func (w *WalletController) NewAddress(addrType lnwallet.AddressType,
 }
 
 // LastUnusedAddress currently returns dummy values.
-func (w *WalletController) LastUnusedAddress(addrType lnwallet.AddressType,
-	account string) (
-	stdaddr.Address, error) {
+func (w *WalletController) LastUnusedAddress(lnwallet.AddressType,
+	string) (stdaddr.Address, error) {
+
 	return nil, nil
 }
 
 // IsOurAddress currently returns a dummy value.
-func (w *WalletController) IsOurAddress(a stdaddr.Address) bool {
+func (w *WalletController) IsOurAddress(stdaddr.Address) bool {
 	return false
 }
 
-// ListAccountscurrently returns a dummy value.
-func (w *WalletController) ListAccounts(string) ([]wallet.AccountProperties, error) {
+// ListAccounts currently returns a dummy value.
+func (w *WalletController) ListAccounts(string) (
+	[]wallet.AccountProperties, error) {
+
 	return nil, nil
 }
 
-// ImportAccountcurrently returns a dummy value.
-func (w *WalletController) ImportAccount(name string, accountPubKey *hdkeychain.ExtendedKey,
-	dryRun bool) (*wallet.AccountProperties, []stdaddr.Address,
-	[]stdaddr.Address, error) {
+// ImportAccount currently returns a dummy value.
+func (w *WalletController) ImportAccount(string, *hdkeychain.ExtendedKey,
+	bool) (*wallet.AccountProperties,
+	[]stdaddr.Address, []stdaddr.Address, error) {
 
 	return nil, nil, nil, nil
 }
@@ -99,23 +103,23 @@ func (w *WalletController) ImportPublicKey(pubKey *secp256k1.PublicKey) error {
 }
 
 // SendOutputs currently returns dummy values.
-func (w *WalletController) SendOutputs(outputs []*wire.TxOut,
-	_ chainfee.AtomPerKByte, _ int32, _, _ string) (*wire.MsgTx, error) {
+func (w *WalletController) SendOutputs([]*wire.TxOut,
+	chainfee.AtomPerKByte, int32, string, string) (*wire.MsgTx, error) {
 
 	return nil, nil
 }
 
 // CreateSimpleTx currently returns dummy values.
-func (w *WalletController) CreateSimpleTx(outputs []*wire.TxOut,
-	_ chainfee.AtomPerKByte, _ int32, _ bool) (*txauthor.AuthoredTx, error) {
+func (w *WalletController) CreateSimpleTx([]*wire.TxOut,
+	chainfee.AtomPerKByte, int32, bool) (*txauthor.AuthoredTx, error) {
 
 	return nil, nil
 }
 
 // ListUnspentWitness is called by the wallet when doing coin selection. We just
 // need one unspent for the funding transaction.
-func (w *WalletController) ListUnspentWitness(minConfs,
-	maxConfs int32, _ string) ([]*lnwallet.Utxo, error) {
+func (w *WalletController) ListUnspentWitness(int32, int32,
+	string) ([]*lnwallet.Utxo, error) {
 
 	// If the mock already has a list of utxos, return it.
 	if w.Utxos != nil {
@@ -139,8 +143,8 @@ func (w *WalletController) ListUnspentWitness(minConfs,
 }
 
 // ListTransactionDetails currently returns dummy values.
-func (w *WalletController) ListTransactionDetails(_,
-	_ int32, _ string) ([]*lnwallet.TransactionDetail, error) {
+func (w *WalletController) ListTransactionDetails(int32, int32,
+	string) ([]*lnwallet.TransactionDetail, error) {
 
 	return nil, nil
 }
@@ -167,8 +171,8 @@ func (w *WalletController) ListLeasedOutputs() ([]*lnwallet.LockedOutput, error)
 }
 
 // FundPsbt currently does nothing.
-func (w *WalletController) FundPsbt(_ *psbt.Packet, _ int32,
-	_ chainfee.AtomPerKByte, _ string) (int32, error) {
+func (w *WalletController) FundPsbt(*psbt.Packet, int32, chainfee.AtomPerKByte,
+	string) (int32, error) {
 
 	return 0, nil
 }
@@ -185,8 +189,8 @@ func (w *WalletController) PublishTransaction(tx *wire.MsgTx, _ string) error {
 }
 
 // LabelTransaction currently does nothing.
-func (w *WalletController) LabelTransaction(_ chainhash.Hash, _ string,
-	_ bool) error {
+func (w *WalletController) LabelTransaction(chainhash.Hash, string,
+	bool) error {
 
 	return nil
 }

lnwallet/interface.go
@@ -182,7 +182,8 @@ type WalletController interface {
 	// NOTE: Only witness outputs should be included in the computation of
 	// the total spendable balance of the wallet. We require this as only
 	// witness inputs can be used for funding channels.
-	ConfirmedBalance(confs int32, accountFilter string) (dcrutil.Amount, error)
+	ConfirmedBalance(confs int32, accountFilter string) (dcrutil.Amount,
+		error)
 
 	// NewAddress returns the next external or internal address for the
 	// wallet dictated by the value of the `change` parameter. If change is
@@ -232,9 +233,8 @@ type WalletController interface {
 	// be used when crafting the transaction.
 	//
 	// NOTE: This method requires the global coin selection lock to be held.
-	SendOutputs(outputs []*wire.TxOut,
-		feeRate chainfee.AtomPerKByte, minConfs int32, label string,
-		fromAccount string) (*wire.MsgTx, error)
+	SendOutputs(outputs []*wire.TxOut, feeRate chainfee.AtomPerKByte,
+		minConfs int32, label, fromAccount string) (*wire.MsgTx, error)
 
 	// CreateSimpleTx creates a Bitcoin transaction paying to the specified
 	// outputs. The transaction is not broadcasted to the network. In the