lightningnetwork/lnd

itest: manage context timeout in utils.go
This commit finishes moving the context management into utils.go.

lntest/itest/lnd_funding_test.go
@@ -491,8 +491,6 @@ func testExternalFundingChanPoint(net *lntest.NetworkHarness, t *harnessTest) {
 // testFundingPersistence mirrors testBasicChannelFunding, but adds restarts
 // and checks for the state of channels with unconfirmed funding transactions.
 func testChannelFundingPersistence(net *lntest.NetworkHarness, t *harnessTest) {
-	ctxb := context.Background()
-
 	chanAmt := funding.MaxBtcFundingAmount
 	pushAmt := btcutil.Amount(0)
 
@@ -570,8 +568,7 @@ func testChannelFundingPersistence(net *lntest.NetworkHarness, t *harnessTest) {
 	// Assert that our wallet has our opening transaction with a label
 	// that does not have a channel ID set yet, because we have not
 	// reached our required confirmations.
-	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	tx := findTxAtHeight(ctxt, t, height, fundingTxStr, net.Alice)
+	tx := findTxAtHeight(t, height, fundingTxStr, net.Alice)
 
 	// At this stage, we expect the transaction to be labelled, but not with
 	// our channel ID because our transaction has not yet confirmed.
@@ -600,7 +597,7 @@ func testChannelFundingPersistence(net *lntest.NetworkHarness, t *harnessTest) {
 	}
 
 	// Re-lookup our transaction in the block that it confirmed in.
-	tx = findTxAtHeight(ctxt, t, height, fundingTxStr, net.Alice)
+	tx = findTxAtHeight(t, height, fundingTxStr, net.Alice)
 
 	// Create an additional check for our channel assertion that will
 	// check that our label is as expected.

lntest/itest/utils.go
@@ -190,8 +190,11 @@ func createPayReqs(node *lntest.HarnessNode, paymentAmt btcutil.Amount,
 
 // getChanInfo is a helper method for getting channel info for a node's sole
 // channel.
-func getChanInfo(ctx context.Context, node *lntest.HarnessNode) (
-	*lnrpc.Channel, error) {
+func getChanInfo(node *lntest.HarnessNode) (*lnrpc.Channel, error) {
+
+	ctxb := context.Background()
+	ctx, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
 
 	req := &lnrpc.ListChannelsRequest{}
 	channelInfo, err := node.ListChannels(ctx, req)
@@ -326,8 +329,12 @@ func calculateMaxHtlc(chanCap btcutil.Amount) uint64 {
 
 // waitForNodeBlockHeight queries the node for its current block height until
 // it reaches the passed height.
-func waitForNodeBlockHeight(ctx context.Context, node *lntest.HarnessNode,
-	height int32) error {
+func waitForNodeBlockHeight(node *lntest.HarnessNode, height int32) error {
+
+	ctxb := context.Background()
+	ctx, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+
 	var predErr error
 	err := wait.Predicate(func() bool {
 		ctxt, _ := context.WithTimeout(ctx, defaultTimeout)
@@ -465,8 +472,12 @@ func subscribeChannelNotifications(ctxb context.Context, t *harnessTest,
 // findTxAtHeight gets all of the transactions that a node's wallet has a record
 // of at the target height, and finds and returns the tx with the target txid,
 // failing if it is not found.
-func findTxAtHeight(ctx context.Context, t *harnessTest, height int32,
+func findTxAtHeight(t *harnessTest, height int32,
 	target string, node *lntest.HarnessNode) *lnrpc.Transaction {
+
+	ctxb := context.Background()
+	ctx, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
 
 	txns, err := node.LightningClient.GetTransactions(
 		ctx, &lnrpc.GetTransactionsRequest{

decred/dcrlnd

lntest/itest/lnd_funding_test.go
@@ -516,8 +516,6 @@ func testExternalFundingChanPoint(net *lntest.NetworkHarness, t *harnessTest) {
 // testFundingPersistence mirrors testBasicChannelFunding, but adds restarts
 // and checks for the state of channels with unconfirmed funding transactions.
 func testChannelFundingPersistence(net *lntest.NetworkHarness, t *harnessTest) {
-	ctxb := context.Background()
-
 	chanAmt := defaultChanAmt
 	pushAmt := dcrutil.Amount(0)
 
@@ -595,8 +593,7 @@ func testChannelFundingPersistence(net *lntest.NetworkHarness, t *harnessTest) {
 	// Assert that our wallet has our opening transaction with a label
 	// that does not have a channel ID set yet, because we have not
 	// reached our required confirmations.
-	ctxt, _ := context.WithTimeout(ctxb, defaultTimeout)
-	tx := findTxAtHeight(ctxt, t, height, fundingTxStr, net.Alice)
+	tx := findTxAtHeight(t, height, fundingTxStr, net.Alice)
 
 	// At this stage, we expect the transaction to be labelled, but not with
 	// our channel ID because our transaction has not yet confirmed.
@@ -626,7 +623,7 @@ func testChannelFundingPersistence(net *lntest.NetworkHarness, t *harnessTest) {
 	}
 
 	// Re-lookup our transaction in the block that it confirmed in.
-	_ = findTxAtHeight(ctxt, t, height, fundingTxStr, net.Alice)
+	_ = findTxAtHeight(t, height, fundingTxStr, net.Alice)
 
 	// Create an additional check for our channel assertion that will
 	// check that our label is as expected.

lntest/itest/utils.go
@@ -195,8 +195,11 @@ func createPayReqs(node *lntest.HarnessNode, paymentAmt dcrutil.Amount,
 
 // getChanInfo is a helper method for getting channel info for a node's sole
 // channel.
-func getChanInfo(ctx context.Context, node *lntest.HarnessNode) (
-	*lnrpc.Channel, error) {
+func getChanInfo(node *lntest.HarnessNode) (*lnrpc.Channel, error) {
+
+	ctxb := context.Background()
+	ctx, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
 
 	req := &lnrpc.ListChannelsRequest{}
 	channelInfo, err := node.ListChannels(ctx, req)
@@ -331,8 +334,12 @@ func calculateMaxHtlc(chanCap dcrutil.Amount) uint64 {
 
 // waitForNodeBlockHeight queries the node for its current block height until
 // it reaches the passed height.
-func waitForNodeBlockHeight(ctx context.Context, node *lntest.HarnessNode,
-	height int64) error {
+func waitForNodeBlockHeight(node *lntest.HarnessNode, height int64) error {
+
+	ctxb := context.Background()
+	ctx, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
+
 	var predErr error
 	err := wait.Predicate(func() bool {
 		ctxt, _ := context.WithTimeout(ctx, defaultTimeout)
@@ -474,8 +481,12 @@ func subscribeChannelNotifications(ctxb context.Context, t *harnessTest,
 // findTxAtHeight gets all of the transactions that a node's wallet has a record
 // of at the target height, and finds and returns the tx with the target txid,
 // failing if it is not found.
-func findTxAtHeight(ctx context.Context, t *harnessTest, height int64,
+func findTxAtHeight(t *harnessTest, height int64,
 	target string, node *lntest.HarnessNode) *lnrpc.Transaction {
+
+	ctxb := context.Background()
+	ctx, cancel := context.WithTimeout(ctxb, defaultTimeout)
+	defer cancel()
 
 	txns, err := node.LightningClient.GetTransactions(
 		ctx, &lnrpc.GetTransactionsRequest{