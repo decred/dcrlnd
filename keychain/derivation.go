lightningnetwork/lnd

multi: refactor SignDigest into SignMessage
To make it possible to use a remote signrpc server as a signer for our
wallet, we need to change our main interface to sign the message instead
of the message's digest. Otherwise we'd need to alter the
signrpc.SignMessage RPC to accept a digest instead of only the message
which has security implications.

keychain/derivation.go
@@ -191,10 +191,10 @@ type SecretKeyRing interface {
 // DigestSignerRing is an interface that abstracts away basic low-level ECDSA
 // signing on keys within a key ring.
 type DigestSignerRing interface {
-	// SignDigest signs the given SHA256 message digest with the private key
-	// described in the key descriptor.
-	SignDigest(keyDesc KeyDescriptor, digest [32]byte) (*btcec.Signature,
-		error)
+	// SignMessage signs the given message, single or double SHA256 hashing
+	// it first, with the private key described in the key descriptor.
+	SignMessage(keyDesc KeyDescriptor, message []byte,
+		doubleHash bool) (*btcec.Signature, error)
 
 	// SignDigestCompact signs the given SHA256 message digest with the
 	// private key described in the key descriptor and returns the signature
@@ -209,9 +209,9 @@ type SingleKeyDigestSigner interface {
 	// PubKey returns the public key of the wrapped private key.
 	PubKey() *btcec.PublicKey
 
-	// SignDigest signs the given SHA256 message digest with the wrapped
-	// private key.
-	SignDigest(digest [32]byte) (*btcec.Signature, error)
+	// SignMessage signs the given message, single or double SHA256 hashing
+	// it first, with the wrapped private key.
+	SignMessage(message []byte, doubleHash bool) (*btcec.Signature, error)
 
 	// SignDigestCompact signs the given SHA256 message digest with the
 	// wrapped private key and returns the signature in the compact, public

keychain/signer.go
@@ -1,6 +1,9 @@
 package keychain
 
-import "github.com/btcsuite/btcd/btcec"
+import (
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+)
 
 func NewPubKeyDigestSigner(keyDesc KeyDescriptor,
 	signer DigestSignerRing) *PubKeyDigestSigner {
@@ -20,10 +23,10 @@ func (p *PubKeyDigestSigner) PubKey() *btcec.PublicKey {
 	return p.keyDesc.PubKey
 }
 
-func (p *PubKeyDigestSigner) SignDigest(digest [32]byte) (*btcec.Signature,
-	error) {
+func (p *PubKeyDigestSigner) SignMessage(message []byte,
+	doubleHash bool) (*btcec.Signature, error) {
 
-	return p.digestSigner.SignDigest(p.keyDesc, digest)
+	return p.digestSigner.SignMessage(p.keyDesc, message, doubleHash)
 }
 
 func (p *PubKeyDigestSigner) SignDigestCompact(digest [32]byte) ([]byte,
@@ -40,10 +43,16 @@ func (p *PrivKeyDigestSigner) PubKey() *btcec.PublicKey {
 	return p.PrivKey.PubKey()
 }
 
-func (p *PrivKeyDigestSigner) SignDigest(digest [32]byte) (*btcec.Signature,
-	error) {
+func (p *PrivKeyDigestSigner) SignMessage(msg []byte,
+	doubleHash bool) (*btcec.Signature, error) {
 
-	return p.PrivKey.Sign(digest[:])
+	var digest []byte
+	if doubleHash {
+		digest = chainhash.DoubleHashB(msg)
+	} else {
+		digest = chainhash.HashB(msg)
+	}
+	return p.PrivKey.Sign(digest)
 }
 
 func (p *PrivKeyDigestSigner) SignDigestCompact(digest [32]byte) ([]byte,

lntest/mock/secretkeyring.go
@@ -2,7 +2,7 @@ package mock
 
 import (
 	"github.com/btcsuite/btcd/btcec"
-
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/lightningnetwork/lnd/keychain"
 )
 
@@ -41,11 +41,17 @@ func (s *SecretKeyRing) ECDH(_ keychain.KeyDescriptor, pubKey *btcec.PublicKey) 
 	return [32]byte{}, nil
 }
 
-// SignDigest signs the passed digest and ignores the KeyDescriptor.
-func (s *SecretKeyRing) SignDigest(_ keychain.KeyDescriptor,
-	digest [32]byte) (*btcec.Signature, error) {
+// SignMessage signs the passed message and ignores the KeyDescriptor.
+func (s *SecretKeyRing) SignMessage(_ keychain.KeyDescriptor,
+	msg []byte, doubleHash bool) (*btcec.Signature, error) {
 
-	return s.RootKey.Sign(digest[:])
+	var digest []byte
+	if doubleHash {
+		digest = chainhash.DoubleHashB(msg)
+	} else {
+		digest = chainhash.HashB(msg)
+	}
+	return s.RootKey.Sign(digest)
 }
 
 // SignDigestCompact signs the passed digest.

decred/dcrlnd

keychain/derivation.go
@@ -205,10 +205,10 @@ type SecretKeyRing interface {
 // DigestSignerRing is an interface that abstracts away basic low-level ECDSA
 // signing on keys within a key ring.
 type DigestSignerRing interface {
-	// SignDigest signs the given SHA256 message digest with the private key
-	// described in the key descriptor.
-	SignDigest(keyDesc KeyDescriptor, digest [32]byte) (*ecdsa.Signature,
-		error)
+	// SignMessage signs the given message, single or double SHA256 hashing
+	// it first, with the private key described in the key descriptor.
+	SignMessage(keyDesc KeyDescriptor, message []byte,
+		doubleHash bool) (*ecdsa.Signature, error)
 
 	// SignDigestCompact signs the given SHA256 message digest with the
 	// private key described in the key descriptor and returns the signature
@@ -223,9 +223,9 @@ type SingleKeyDigestSigner interface {
 	// PubKey returns the public key of the wrapped private key.
 	PubKey() *secp256k1.PublicKey
 
-	// SignDigest signs the given SHA256 message digest with the wrapped
-	// private key.
-	SignDigest(digest [32]byte) (*ecdsa.Signature, error)
+	// SignMessage signs the given message, single or double SHA256 hashing
+	// it first, with the wrapped private key.
+	SignMessage(message []byte, doubleHash bool) (*ecdsa.Signature, error)
 
 	// SignDigestCompact signs the given SHA256 message digest with the
 	// wrapped private key and returns the signature in the compact, public

keychain/signer.go
@@ -1,6 +1,7 @@
 package keychain
 
 import (
+	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4/ecdsa"
 )
@@ -23,10 +24,10 @@ func (p *PubKeyDigestSigner) PubKey() *secp256k1.PublicKey {
 	return p.keyDesc.PubKey
 }
 
-func (p *PubKeyDigestSigner) SignDigest(digest [32]byte) (*ecdsa.Signature,
-	error) {
+func (p *PubKeyDigestSigner) SignMessage(message []byte,
+	doubleHash bool) (*ecdsa.Signature, error) {
 
-	return p.digestSigner.SignDigest(p.keyDesc, digest)
+	return p.digestSigner.SignMessage(p.keyDesc, message, doubleHash)
 }
 
 func (p *PubKeyDigestSigner) SignDigestCompact(digest [32]byte) ([]byte,
@@ -43,10 +44,17 @@ func (p *PrivKeyDigestSigner) PubKey() *secp256k1.PublicKey {
 	return p.PrivKey.PubKey()
 }
 
-func (p *PrivKeyDigestSigner) SignDigest(digest [32]byte) (*ecdsa.Signature,
-	error) {
+func (p *PrivKeyDigestSigner) SignMessage(msg []byte,
+	doubleHash bool) (*ecdsa.Signature, error) {
 
-	return ecdsa.Sign(p.PrivKey, digest[:]), nil
+	var digest []byte
+	if doubleHash {
+		digest1 := chainhash.HashB(msg)
+		digest = chainhash.HashB(digest1)
+	} else {
+		digest = chainhash.HashB(msg)
+	}
+	return ecdsa.Sign(p.PrivKey, digest), nil
 }
 
 func (p *PrivKeyDigestSigner) SignDigestCompact(digest [32]byte) ([]byte,

lntest/mock/secretkeyring.go
@@ -1,6 +1,7 @@
 package mock
 
 import (
+	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 	"github.com/decred/dcrd/dcrec/secp256k1/v4/ecdsa"
 
@@ -42,11 +43,18 @@ func (s *SecretKeyRing) ECDH(_ keychain.KeyDescriptor, pubKey *secp256k1.PublicK
 	return [32]byte{}, nil
 }
 
-// SignDigest signs the passed digest and ignores the KeyDescriptor.
-func (s *SecretKeyRing) SignDigest(_ keychain.KeyDescriptor,
-	digest [32]byte) (*ecdsa.Signature, error) {
+// SignMessage signs the passed message and ignores the KeyDescriptor.
+func (s *SecretKeyRing) SignMessage(_ keychain.KeyDescriptor,
+	msg []byte, doubleHash bool) (*ecdsa.Signature, error) {
 
-	return ecdsa.Sign(s.RootKey, digest[:]), nil
+	var digest []byte
+	if doubleHash {
+		digest1 := chainhash.HashB(msg)
+		digest = chainhash.HashB(digest1)
+	} else {
+		digest = chainhash.HashB(msg)
+	}
+	return ecdsa.Sign(s.RootKey, digest), nil
 }
 
 // SignDigestCompact signs the passed digest.